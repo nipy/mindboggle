74c2821e1a2286229e777a09d757f87be9047d17
Arno Klein <arno@binarybottle.com>
2011-08-12 19:32:14 -0400

    removed third-party software and hbm evaluation study, renamed folders
    Former-commit-id: a042582320b95ae82e81a7303ad1608aa0e4c797

--- registration/test/build_LPBA40_template.sh
+++
@@ -1,4 +0,0 @@
-# Run inside the LPBA40 brain-extracted folder:
-# -c 0 (non-parallel, 1 for sge)
-
-bash buildtemplateparallel.sh -d 3 -m 30x100x10 -t GR -s CC -c 0 -j 4 -o LPBA40 -z InitialTemplate.nii.gz  *.nii.gz
