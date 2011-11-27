59324c798e442256cd7d2cb83dd54735b0781062
Forrest Sheng Bao <forrest.bao@gmail.com>
2011-11-26 20:33:37 -0600

    delete src files that are no longer in use
    Former-commit-id: 2afaa94dea8b55d116788b3c168899e7dd1d2eba

--- extract/fundi/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-time python extract.py -T -S ../$1/lh.inflated ../$1/lh.curv ../$1/lh.pial
-# python extract.py -T -S ../sub01912/rh.inflated ../sub01912/rh.curv ../sub01912/rh.pial
-
