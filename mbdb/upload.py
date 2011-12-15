c43f9f136805d7a4d63b8fad838ce35fd2f05937
Nolan Nichols <nolan.nichols@gmail.com>
2011-12-15 00:47:27 -0800

    demo upload script available
    Former-commit-id: d74225822c9317338b35bda080506b1e1c4c8be6

--- mbdb/upload.py
+++ mbdb/upload.py
@@ -11,10 +11,13 @@ import csv, sys
 def set_db_url(server=None):
     """
     set_db_url is used to configure the default server for all mbdb.base domain objects
+
+    http://50.18.175.117:8182/graphs
     """
     NodeMB.resource = Resource(db_url=server)
     RelationshipMB.resource = Resource(db_url=server)
     print "db_url set to", server
+
 
 def read_stats(file):
     """
@@ -22,7 +25,6 @@ def read_stats(file):
 
     input: a text file and attempts to guess how it is delimited
     output: a list of tuples where each tuple is a row
-
     """
     filename = file
     with open(filename, 'rb') as f:
@@ -33,7 +35,7 @@ def read_stats(file):
         print "Reading %s ..." % header
         try:
             stats = list()
-            stats.append(f.name)
+            # stats.append(f.name)
             for row in reader:
                 stripped = [float(value) for value in row if len(value) != 0]
                 stats.append(tuple(stripped))
@@ -41,33 +43,41 @@ def read_stats(file):
         except csv.Error, e:
             sys.exit('file %s, line %d: %s' % (filename, reader.line_num, e))
 
+
 def create_db(db_name):
     db = Database(name=db_name)
     return db
 
+
 def create_project(project_name, db):
-    project = Project(name = project_name)
+    project = Project(name=project_name)
     ContatinedIn(project, db)
     return project
 
+
 def create_subject(subject_name, project):
     subject = Subject(name=subject_name)
-    ContatinedIn(subject,project)
+    ContatinedIn(subject, project)
     return subject
+
 
 def create_fundus(fundus_name, subject):
     fundus = Fundus(name=fundus_name)
-    ContatinedIn(fundus,subject)
+    ContatinedIn(fundus, subject)
     return fundus
 
-def set_fundus_stats(fundus, curvature, convexity, depth, thickness, length):
-    fundus.curvature = curvature
-    fundus.convexity = convexity
-    fundus.depth = depth
-    fundus.thickness = thickness
-    fundus.length = length
-    fundus.save()
-    return fundus
+
+def set_fundus_stats(subject, stats):
+    for featureVector in range(len(stats)):
+        featureVectorName = subject.name + '-' + str(featureVector)
+        fundus = create_fundus(featureVectorName, subject)
+        fundus.curvature = stats[featureVector][0]
+        fundus.convexity = stats[featureVector][1]
+        fundus.depth = stats[featureVector][2]
+        fundus.thickness = stats[featureVector][3]
+        fundus.length = stats[featureVector][4]
+        fundus.save()
+        print stats[featureVector]
 
 
 
