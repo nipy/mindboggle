74847d583c926a96c451a2bae534f28d2c606507
binarybottle <arno@binarybottle.com>
2012-02-20 21:07:56 -0500

    updated laplace beltrami code
    Former-commit-id: edb7ccf90f80d14892e57177e5b51ce4afd753af

--- measure/laplace_beltrami/laplace_beltrami.py
+++ measure/laplace_beltrami/laplace_beltrami.py
@@ -1,7 +1,7 @@
 """ Laplace Beltrami Operator
 --------------------------------------------------------
-Parameters: VTK: file location
-					File containing nodes and meshes on which LBO will be performed
+Parameters: file_name: file location
+					VTK File containing nodes and meshes on which LBO will be performed
 			which: string (Choice of which graph Laplacian.)
 					basic - non-normalized Laplacian (Lap = D - W)
 					norm1 - normalized Laplacian (Lap = ddmi_sq * L * ddmi_sq) - recovers definition
@@ -10,7 +10,7 @@ Parameters: VTK: file location
 					random_walk - random walk Laplacian (Lap = inv(D) * W) 
 			kernel: function (Choice of kernel to compute edge weights)
 					rbf_kernel - Gaussian kernel with parameter sigma
-					cotangent_kernel - typical weight calculation for laplace_beltrami_Operator
+					cotangent_kernel - typical weight calculation for Laplace_Beltrami_Operator
 					inverse_distance - weight is the inverse of the disance between two nodes
 			sigma:	float
 					parameter for use in gaussian kernel
@@ -18,7 +18,9 @@ Parameters: VTK: file location
 					number of eigenvalues/vectors to compute
 			export: boolean
 					Would you like to write the eigenvalues to a file for independent analysis?
-
+			return_header: boolean
+					Would you like the header of the vtk file, extracted by pyvtk?
+					
 Returns:	Nodes: numpy array
 			
 			Meshes: numpy array 
@@ -27,80 +29,79 @@ Returns:	Nodes: numpy array
 			
 			Laplacian: scipy sparse matrix in csr format
 			
-			evals: array of eigenvalues
+			evals: numpy array of eigenvalues
 			
-			evecs: array of eigenvectors
+			evecs: numpy array of eigenvectors
 			
-			vtk files
+			header: string
 ---------------------------------------------------------			
 """
 import numpy as np
-from scipy.sparse.linalg.eigen.arpack import *
+from scipy.sparse.linalg import eigsh
 from scipy.sparse import lil_matrix, csc_matrix
 import pickle
+from time import time
+import pyvtk
 
-from vtk_operations import *
+import vtk_operations as vo
 from compute_weights import *
 from graph_operations import *
 
-default_VTK = '/Users/eli/Documents/PythonFiles/Data/testdatalabels.txt'
-default_kernel = cotangent_kernel
+default_VTK = '/home/eli/PythonFiles/Data/testdatalabels.vtk'
 
 ################################################################## 
-### Main Function:
-def laplace_beltrami_operator(VTK=default_VTK, which='norm1', 
-							  kernel=default_kernel, sigma=10, components=50, export=False):
-	""" laplace_beltrami_operator."""
+################### Main Function: ##############################
+
+def Laplace_Beltrami_Operator(file_name=default_VTK, which='norm1', kernel=cotangent_kernel, sigma=10, components=200, export=True, return_header=False):
+	""" Laplace_Beltrami_Operator."""
 	
-	# Extract Nodes and Meshes from VTK file:
-	vtk = convert_vtk_to_matrix(VTK, return_files=True, labels=False, fundi=False, delete=False)
-	Nodes, Meshes, A = vtk['Nodes'], vtk['Meshes'], vtk['main_file']
+	##############################################################
+	##### Extract Nodes and Meshes from VTK file: 
 	
-	# Compute Weight Matrix:
+	Data = pyvtk.VtkData(file_name)
+	Nodes = np.asarray(Data.structure.points)
+	Meshes = np.asarray(Data.structure.polygons)
+	
+	##############################################################
+	##### Compute Weight Matrix:
+	
+	t0 = time()
 	aff_mat = compute_weights(Nodes, Meshes, kernel=kernel, add_to_graph=False, sigma=sigma)
-	
-	# Compute Graph Laplacian:
+	print("done in %0.3fs" % (time() - t0))
+
+	##############################################################
+	##### Compute Graph Laplacian:
+
+	t0 = time()
 	Laplacian = graph_laplacian(aff_mat, which=which)
+	print("done in %0.3fs" % (time() - t0))
+
+	##############################################################
+	##### Obtain Eigen Spectrum:
 	
-	# Obtain Eigen Spectrum:
 	print 'Finding the eigen spectrum...'
+	t0 = time()
 	Laplacian = Laplacian.tocsc()
 	if components >= Nodes.shape[0]:
 		components = Nodes.shape[0] - 1
-	evals, evecs = eigsh(Laplacian, components, which='SM') # does sigma=0 work?
+	evals, evecs = eigsh(Laplacian, components, sigma=0)
+	print("done in %0.3fs" % (time() - t0))
 	
 	if export:
-		file_name = VTK[:-4] + '.npy'
-		h = open(file_name, 'w')
+		file_name2 = file_name[:-4] + '.npy'
+		h = open(file_name2, 'w')
 		pickle.dump(evals, h)
 		h.close()
 
-	return Nodes, Meshes, aff_mat, Laplacian, evals, evecs
+	################################################################
+	##### Write New VTK File With Feidler Vector Overlayed On Brain:
 	
-	
-	
-## Piece of Old Code:
-	'''
-	# Linearize to visualize.
-	
-	ones = np.ones(Nodes.shape[0])
-	natural = np.cumsum(ones)
-	print 'Writing results to a file...'
+	# f = vo.write_all(file_name[:-4]+'_eigen.vtk', Nodes, Meshes, evecs[:,1], label_type='Eigenvector')
 
-	for i in xrange(1, components):
-		vector = np.argsort(evecs[:,i])
-		components = np.zeros(Nodes.shape[0])
-		components[vector] = natural
-		file_name = '/Users/eli/Desktop/Eigen' + str(i) + '.vtk'
-		h = open(file_name, 'w')
-		line_num = 0
-		total_lines_to_copy = 9 + Nodes.shape[0] + Meshes.shape[0] 
-		for line in A:
-			if line_num < total_lines_to_copy:
-				h.write(line)
-				line_num += 1
-		for i in xrange(len(evecs[:,i])):
-			h.write('{0}\n'.format(str(components[i])))
+	#################################################################
+	##### Return Results:
 	
-		h.close()
-	'''
+	if return_header:
+		return Nodes, Meshes, aff_mat, Laplacian, evals, evecs, Data.header
+	else:
+		return Nodes, Meshes, aff_mat, Laplacian, evals, evecs
