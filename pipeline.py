ad004055adcaf72bff7d1fecd91b2cd5b4d2fbc3
Arno Klein <arno@binarybottle.com>
2012-05-05 01:29:10 -0400

    stuck trying to connect multiple shape measure nodes to db write node...
    Former-commit-id: accbfbba79b987cbb9cbe65939353e7f8e6ac5b4

--- pipeline.py
+++ pipeline.py
@@ -5,10 +5,11 @@ This is Mindboggle's NiPype pipeline!
 
 Example usage:
 
-from pipeline import create_mindboggle_flow
-wf = create_mindboggle_flow()
-wf.inputs.feature_extractor.curvature_file = '/home/arno/Documents/Projects/mindboggle/data/ManualSurfandVolLabels/subjects/KKI2009-14/surf/lh.curv'
-wf.inputs.feature_extractor.surface_file = '/home/arno/Documents/Projects/mindboggle/data/ManualSurfandVolLabels/subjects/KKI2009-14/surf/lh.pial'
+from pipeline import create_mindboggle_workflow
+wf = create_mindboggle_workflow()
+subject_path = '../data/ManualSurfandVolLabels/subjects/KKI2009-14/'
+wf.inputs.feature_extractor.curvature_file = subjects_path + 'surf/lh.curv'
+wf.inputs.feature_extractor.surface_file = subjects_path + 'surf/lh.pial'
 wf.run() # doctest: +SKIP
 
 
@@ -29,7 +30,7 @@ def extract_features(surface_file, curvature_file):
     """
     from glob import glob
     import subprocess as sp
-    cmd = '/home/arno/Documents/Projects/mindboggle/mindboggle/extract/fundi/extract.py'
+    cmd = 'extract/fundi/extract.py'
     cmd = ['python', cmd, '%s'%curvature_file, '%s'%surface_file]
     proc = sp.Popen(cmd)
     o, e = proc.communicate()
@@ -89,18 +90,6 @@ def measure_LaplaceBeltrami(object):
         measurement = measure_LaplaceBeltrami(object)
     return measurement
 
-"""
-def measure_(object):
-    ""Measure
-
-    measure_()
-    ""
-    from measure.py import measure_
-    if type(object) is np.ndarray:
-        measurement = measure_(object)
-    return measurement
-"""
-
 def write_features_to_database(object):
     """Write to database
 
@@ -123,82 +112,67 @@ def write_measures_to_database(measurement):
     success = 'True'
     return success
 
-def create_mindboggle_flow():
+def create_mindboggle_workflow():
     """Create a Mindboggle workflow"""
 
     # Create an instance of a workflow and indicate that it should operate in the current directory:
-    flow = pe.Workflow(name='pipeline')
-    flow.base_dir = '.'
+    workflow = pe.Workflow(name='pipeline')
+    workflow.base_dir = '.'
 
     # Define nodes
     extract_features_node = pe.Node(util.Function(input_names = ['surface_file', 'curvature_file'],
                                               output_names=['feature_files'],
                                               function = extract_features),
-                                name='extract_features_node')
-    measure_position_node = pe.MapNode(util.Function(input_names = ['object'],
+                                name='ExtractFeatures')
+    measure_position_node = pe.Node(util.Function(input_names = ['object'],
                                               output_names=['position'],
                                               function = measure_position),
-                                       iterfield=['object'],
-                                name='measure_position_node')
-    measure_extent_node = pe.MapNode(util.Function(input_names = ['object'],
+                                name='MeasurePosition')
+    measure_extent_node = pe.Node(util.Function(input_names = ['object'],
                                               output_names=['extent'],
                                               function = measure_extent),
-                                       iterfield=['object'],
-                                name='measure_extent_node')
-    measure_curvature_node = pe.MapNode(util.Function(input_names = ['object'],
+                                name='MeasureExtent')
+    measure_curvature_node = pe.Node(util.Function(input_names = ['object'],
                                               output_names=['curvature'],
                                               function = measure_curvature),
-                                       iterfield=['object'],
-                                name='measure_curvature_node')
-    measure_LaplaceBeltrami_node = pe.MapNode(util.Function(input_names = ['object'],
+                                name='MeasureCurvature')
+    measure_LaplaceBeltrami_node = pe.Node(util.Function(input_names = ['object'],
                                               output_names=['LaplaceBeltrami'],
                                               function = measure_LaplaceBeltrami),
-                                       iterfield=['object'],
-                                name='measure_LaplaceBeltrami_node')
-    """
-    measure__node = pe.MapNode(util.Function(input_names = ['object'],
-                                              output_names=[''],
-                                              function = measure_),
-                                       iterfield=['object'],
-                                name='position_')
-    """
-    write_features_to_database_node = pe.MapNode(util.Function(input_names = ['object'],
+                                name='MeasureLaplaceBeltrami')
+    write_features_to_database_node = pe.Node(util.Function(input_names = ['object'],
                                               output_names=['success'],
                                               function = write_features_to_database),
-                                       iterfield=['object'],
-                                name='write_features_to_database_node')
-    write_measures_to_database_node = pe.MapNode(util.Function(input_names = ['measurement'],
+                                name='WriteFeaturesToDatabase')
+    write_measures_to_database_node = pe.Node(util.Function(input_names = ['measurement'],
                                               output_names=['success'],
                                               function = write_measures_to_database),
-                                       iterfield=['measurement'],
-                                name='write_measures_to_database_node')
+                                name='WriteMeasuresToDatabase')
 
 # Define connections
-    #flow.add_nodes([extract_features_node])
+    #workflow.add_nodes([extract_features_node])
+    workflow.connect(extract_features_node, 'feature_files', measure_position_node, 'object')
+    workflow.connect(extract_features_node, 'feature_files', measure_extent_node, 'object')
+    workflow.connect(extract_features_node, 'feature_files', measure_curvature_node, 'object')
+    workflow.connect(extract_features_node, 'feature_files', measure_LaplaceBeltrami_node, 'object')
 
-    flow.connect(extract_features_node, 'feature_files', write_features_to_database_node, 'object')
+    workflow.connect(extract_features_node, 'feature_files', write_features_to_database_node, 'object')
 
-    flow.connect(extract_features_node, 'feature_files', measure_position_node, 'object')
-    flow.connect(extract_features_node, 'feature_files', measure_extent_node, 'object')
-    flow.connect(extract_features_node, 'feature_files', measure_curvature_node, 'object')
-    flow.connect(extract_features_node, 'feature_files', measure_LaplaceBeltrami_node, 'object')
-    #flow.connect(extract_features_node, 'feature_files', measure__node, 'object')
-
+    workflow.connect(measure_position_node, 'position', write_measures_to_database_node, 'measurement')
+    #workflow.connect(measure_extent_node, 'extent', write_measures_to_database_node, 'measurement')
     """
-    flow.connect(measure_position_node, 'position', write_measures_to_database_node, 'measurement')
-    flow.connect(measure_extent_node, 'extent', write_measures_to_database_node, 'measurement')
-    flow.connect(measure_depth_node, 'depth', write_measures_to_database_node, 'measurement')
-    flow.connect(measure_curvature_node, 'curvature', write_measures_to_database_node, 'measurement')
-    flow.connect(measure_LaplaceBeltrami_node, 'LaplaceBeltrami', write_measures_to_database_node, 'measurement')
-    #flow.connect(measure__node, '', write_measures_to_database_node, 'measurement')
+    workflow.connect(measure_depth_node, 'measurement', write_measures_to_database_node, 'measurement')
+    workflow.connect(measure_curvature_node, 'measurement', write_measures_to_database_node, 'measurement')
+    workflow.connect(measure_LaplaceBeltrami_node, 'measurement', write_measures_to_database_node, 'measurement')
+    #workflow.connect(measure__node, '', write_measures_to_database_node, 'measurement')
     """
-
-    return flow
+    return workflow
 
 if __name__ == '__main__':
-    flow = create_mindboggle_flow()
-    flow.inputs.extract_features_node.curvature_file = '/home/arno/Documents/Projects/mindboggle/data/ManualSurfandVolLabels/subjects/KKI2009-14/surf/lh.curv'
-    flow.inputs.extract_features_node.surface_file = '/home/arno/Documents/Projects/mindboggle/data/ManualSurfandVolLabels/subjects/KKI2009-14/surf/lh.pial'
-    flow.write_graph()
-    #flow.run()
+    subject_path = '../data/ManualSurfandVolLabels/subjects/KKI2009-14/'
+    workflow = create_mindboggle_workflow()
+    workflow.inputs.ExtractFeatures.curvature_file = subject_path + 'surf/lh.curv'
+    workflow.inputs.ExtractFeatures.surface_file = subject_path + 'surf/lh.pial'
+    workflow.write_graph()
+    #workflow.run()
 
