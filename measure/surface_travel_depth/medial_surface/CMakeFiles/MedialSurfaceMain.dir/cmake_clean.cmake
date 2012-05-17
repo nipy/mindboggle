3e5d0892ff968ee06930dd0908d5fcc7288af365
Arno Klein <arno@binarybottle.com>
2012-05-17 12:13:35 -0400

    added compiled version of surface_travel_depth
    Former-commit-id: c89bc66dea9656872effa6f42a735a0c326432cc

---
+++ measure/surface_travel_depth/medial_surface/CMakeFiles/MedialSurfaceMain.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/MedialSurfaceMain.dir/MedialSurfaceMain.cpp.o"
+  "MedialSurfaceMain.pdb"
+  "MedialSurfaceMain"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/MedialSurfaceMain.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
