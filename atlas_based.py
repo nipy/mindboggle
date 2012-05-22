9985757aeb10cc5f7c3cd6f92eeff4bb89c954af
Arno Klein <arno@binarybottle.com>
2012-05-21 21:51:46 -0400

    atlas-based pipeline running without errors
    Former-commit-id: e11937ee9207c9b5605b37215ca5688792585a48

--- atlas_based.py
+++ atlas_based.py
@@ -1,12 +1,22 @@
 #!/usr/bin/python
 
 """
+Atlas-based functions for surface registration-based labeling:
+
+1. Surface map conversion (from FreeSurfer to VTK format)
+
+2. Multi-atlas registration
+Register surface to template with FreeSurfer's mris_register.
+Transform the labels from multiple atlases via a template 
+(using FreeSurfer's mri_surf2surf).
+
+3. Multi-atlas labeling
 For each brain hemisphere (left and right) in a given subject, 
 read in FreeSurfer *.annot files (multiple labelings) and output one VTK file 
 of majority vote labels, representing a "maximum probability" labeling.
-
 The main function is labeling() and calls: 
 load_labels, vote_labels, combine_labels, and read_annot (0-index labels).
+
 
 Authors:  
 Forrest Bao  .  forrest.bao@gmail.com
@@ -15,6 +25,137 @@ Arno Klein  .  arno@mindboggle.info  .  www.binarybottle.com
 (c) 2012  Mindbogglers (www.mindboggle.info), under Apache License Version 2.0
 
 """
+
+##############################################################################
+#   Surface map conversion
+##############################################################################
+
+def convert_to_vtk(fs_surface_files):
+    """
+    Measure
+
+    # import nipype.interfaces.freesurfer as fs
+    # mris = fs.MRIsConvert()
+    # mris.inputs(in_file = '', out_file = '', subjects_dir = '')
+    """
+    #import subprocess as sp
+    import os
+    surface_files = []
+    for fs_surface_file in fs_surface_files:
+        surface_file = fs_surface_file + '.vtk'
+        surface_files.append(surface_file)
+        args = ['mris_convert', fs_surface_file, surface_file]
+        print(' '.join(args)); os.system(' '.join(args))
+    #proc = sp.Popen(' '.join(args))
+    #o, e = proc.communicate()
+    #if proc.returncode > 0 :
+    #    raise Exception('\n'.join([args + ' failed', o, e]))
+    return surface_files
+
+##############################################################################
+#   Multi-atlas registration
+##############################################################################
+
+def register_template(subject_id, subjects_path, 
+                      template_name, templates_path, reg_name):
+    """
+    Register surface to template with FreeSurfer's mris_register
+
+    Example: bert
+             /Applications/freesurfer/subjects
+             ./templates_freesurfer
+             KKI_2.tif
+             sphere_to_template.reg
+    """
+    import os
+
+    for hemi in ['lh','rh']:
+        input_file = os.path.join(subjects_path, subject_id, 'surf', hemi + '.sphere')
+        output_file = os.path.join(subjects_path, subject_id, 'surf', hemi + '.' + reg_name)
+        template_file = os.path.join(templates_path, hemi + '.' + template_name)
+        args = ['mris_register -curv', input_file, template_file, output_file]
+        print(' '.join(args)); os.system(' '.join(args))
+    return reg_name
+
+def register_atlases(subject_id, subjects_path, atlas_list_file, 
+                     annot_name, reg_name):
+    """
+    Transform the labels from multiple atlases via a template
+    (using FreeSurfer's mri_surf2surf)
+    """
+    import os
+
+    # Get list of atlas subjects from a file
+    f = open(atlas_list_file)
+    atlas_list = f.readlines()
+    for atlas_line in atlas_list:
+        # For each atlas
+        atlas_name = atlas_line.strip("\n")
+        # For each hemisphere
+        for hemi in ['lh','rh']:        
+            annot_file = os.path.join(subjects_path, atlas_name, 'label',
+                                      hemi + '.' + annot_name) 
+            output_annot = hemi + '.' + atlas_name + '_to_' + \
+                           subject_id + '_' + annot_name
+            args = ['mri_surf2surf',
+                    '--hemi', hemi,
+                    '--srcsubject', atlas_name,
+                    '--trgsubject', subject_id,
+                    '--sval-annot', annot_file,
+                    '--tval', output_annot,
+                    '--srcsurfreg', reg_name,
+                    '--trgsurfreg', reg_name]
+            print(' '.join(args)); os.system(' '.join(args))
+    return annot_name
+
+"""
+def register_atlases(subject_id, subjects_path, atlas_list_file, 
+                     annot_name, reg_name):
+    ""
+    Transform the labels from multiple atlases via a template
+    using FreeSurfer's mri_surf2surf (wrapped in NiPype)
+
+    nipype.workflows.smri.freesurfer.utils.fs.SurfaceTransform
+    wraps command **mri_surf2surf**:
+    "Transform a surface file from one subject to another via a spherical registration.
+    Both the source and target subject must reside in your Subjects Directory,
+    and they must have been processed with recon-all, unless you are transforming
+    to one of the icosahedron meshes."
+    ""
+    import os
+    from nipype.interfaces.freesurfer import SurfaceTransform
+
+    sxfm = SurfaceTransform()
+    sxfm.inputs.target_subject = subject_id
+
+    # Get list of atlas subjects from a file
+    f = open(atlas_list_file)
+    atlas_list = f.readlines()
+    for atlas_line in atlas_list:
+        # For each atlas
+        atlas_name = atlas_line.strip("\n")
+        sxfm.inputs.source_subject = atlas_name
+        # For each hemisphere
+        for hemi in ['lh','rh']:        
+            sxfm.inputs.hemi = hemi
+            source_file = os.path.join(subjects_path, atlas_name, 'label',
+                                       hemi + '.' + annot_name) 
+            sxfm.inputs.source_file = source_file
+            # Specify annotation file
+            output_annot = os.path.join(subjects_path, subject_id, 'label',
+                                        hemi + '.' + atlas_name + '_to_' + \
+                                        subject_id + '_' + annot_name)
+            args = ['--tval', output_annot,
+                    '--srcsurfreg', reg_name,
+                    '--trgsurfreg', reg_name]
+            sxfm.inputs.args = ' '.join(args)
+            sxfm.run()
+    return atlas_list
+"""
+
+##############################################################################
+#   Multi-atlas labeling
+##############################################################################
 
 def combine_labels(label_index):
     """
@@ -139,24 +280,25 @@ def load_labels(annot_path, annot_name):
     print "Loading multiple annotations..."
     
     from os import listdir, path
-    Allfiles  = listdir(annot_path)
+
+    all_files  = listdir(annot_path)
     left_files, right_files = [], []
-    for file in Allfiles:
-        if file.find(annot_name) > -1:
-            if file[0] == 'l':
-                left_files.append(file)
-            elif file[0] == 'r':        
-                right_files.append(file)
+    for file1 in all_files:
+        if file1.find(annot_name) > -1 and file1.find('_to_') > -1:
+            if file1[0] == 'l':
+                left_files.append(file1)
+            elif file1[0] == 'r':        
+                right_files.append(file1)
             else:
                 print "Unable to match any file names."
-    
+
     left_labels, right_labels = [], []
-    for file in left_files:
-        Labels, ColorTable, Names = read_annot(path.join(annot_path, file))
+    for file1 in left_files:
+        Labels, ColorTable, Names = read_annot(path.join(annot_path, file1))
         left_labels.append(map(combine_labels,Labels))
 
-    for file in right_files:
-        Labels, ColorTable, Names = read_annot(path.join(annot_path, file))
+    for file1 in right_files:
+        Labels, ColorTable, Names = read_annot(path.join(annot_path, file1))
         right_labels.append(map(combine_labels,Labels))
     
     print "Multiple annotations loaded."
@@ -188,6 +330,7 @@ def vote_labels(left_labels, right_labels):
     
     print "Begin voting..."
     
+    n_labelings = len(left_labels)  # number of atlases used to label subject
     left_n_labels, right_n_labels = len(left_labels[0]), len(right_labels[0])
     left_assign = [-1 for i in xrange(left_n_labels)]  
                   # if no consistent vote, the label is -1 (vertex is unlabeled)
@@ -196,11 +339,11 @@ def vote_labels(left_labels, right_labels):
     if compute_diff_consensus:
         left_diff = [1 for i in xrange(left_n_labels)]   
         right_diff = [1 for i in xrange(right_n_labels)]
-        left_consensus = [21 for i in xrange(left_n_labels)]   
-        right_consensus = [21 for i in xrange(right_n_labels)]
+        left_consensus = [n_labelings for i in xrange(left_n_labels)]   
+        right_consensus = [n_labelings for i in xrange(right_n_labels)]
     
     for vertex in xrange(left_n_labels):
-        votes = Counter([left_labels[i][vertex] for i in xrange(21)])
+        votes = Counter([left_labels[i][vertex] for i in xrange(n_labelings)])
         
         left_assign[vertex] = votes.most_common(1)[0][0]
         if compute_diff_consensus:
@@ -208,7 +351,7 @@ def vote_labels(left_labels, right_labels):
             left_diff[vertex] = len(votes)
             
     for vertex in xrange(right_n_labels):
-        votes = Counter([right_labels[i][vertex] for i in xrange(21)])
+        votes = Counter([right_labels[i][vertex] for i in xrange(n_labelings)])
         
         right_assign[vertex] = votes.most_common(1)[0][0]
         if compute_diff_consensus:
@@ -226,11 +369,10 @@ def labeling(subject_id, subjects_path, annot_name):
     """
     Load VTK surfaces and write majority vote labels as VTK files, 
     according to multiple labelings (annot_name).
-    
     """
     from os import path
     import pyvtk
-    from multilabel import load_labels, vote_labels
+    from atlas_based import load_labels, vote_labels
 
     compute_diff_consensus = 0
     save_inflated = 0
@@ -272,17 +414,17 @@ def labeling(subject_id, subjects_path, annot_name):
 
     for i, input_file in enumerate(input_files):
         VTKReader = pyvtk.VtkData(path.join(subject_surf_path, input_file))
-        Vertexes =  VTKReader.structure.points
+        Vertices =  VTKReader.structure.points
         Faces =     VTKReader.structure.polygons
         
         if compute_diff_consensus:
-            pyvtk.VtkData(pyvtk.PolyData(points=Vertexes, polygons=Faces),\
+            pyvtk.VtkData(pyvtk.PolyData(points=Vertices, polygons=Faces),\
                   pyvtk.PointData(pyvtk.Scalars(dv[i][0], name='Assigned'),\
                                   pyvtk.Scalars(dv[i][1], name='Different'),\
                                   pyvtk.Scalars(dv[i][2], name='Common'))).\
                   tofile(path.join(annot_path, output_files[i]), 'ascii')
         else:
-            pyvtk.VtkData(pyvtk.PolyData(points=Vertexes, polygons=Faces),\
+            pyvtk.VtkData(pyvtk.PolyData(points=Vertices, polygons=Faces),\
                   pyvtk.PointData(pyvtk.Scalars(dv[i][0], name='Assigned'))).\
                   tofile(path.join(annot_path, output_files[i]), 'ascii')
     return annot_name  
