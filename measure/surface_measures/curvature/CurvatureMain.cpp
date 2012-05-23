05bc66a7e78b1d6ec73baea37f922a1a7e7a68be
Joachim Giard <giard@pc-giard.(none)>
2012-05-23 14:56:46 +0200

    modified curvature main class to add output files. Added a saturation in ComputePrincipalCurvatures
    Former-commit-id: d94d21b6daefd398044ea803f2c066512b521d70

--- measure/surface_measures/curvature/CurvatureMain.cpp
+++ measure/surface_measures/curvature/CurvatureMain.cpp
@@ -8,9 +8,28 @@ int main(int argc, char** argv)
 {
     time_t start= time(NULL);
 
+    if(argc < 3)
+    {
+        cout<<"Usage: CurvatureMain input MeanCurvatureOutput [GaussianCurvatureOutput] [MaximalCurvatureOutput] [MinimalCurvatureOutput]"<<endl;
+    }
+
     MeshAnalyser* ma = new MeshAnalyser(argv[1]);
     ma->ComputePrincipalCurvatures();
+
     ma->WriteIntoFile(argv[2], (char*)"curv");
+
+    if(argc > 3)
+    {
+        ma->WriteIntoFile(argv[3], (char*)"gCurv");
+    }
+    if(argc > 4)
+    {
+        ma->WriteIntoFile(argv[4], (char*)"curv1");
+    }
+    if(argc > 5)
+    {
+        ma->WriteIntoFile(argv[5], (char*)"curv2");
+    }
 
 
     cout<<"Elapsed time (meshTest): "<<time(NULL)-start<<" s"<<endl;
