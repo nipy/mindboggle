d2aece2c1a9a09c4d2007191c514a60dce3d57b8
Arno Klein <arno@binarybottle.com>
2012-06-04 17:07:28 -0400

    Move Eliezer's PyShape code to propagate/; remove spaces in filenames
    Former-commit-id: b3e5dc4793f24b7dc932281e1b9787a8e2fcb705

---
+++ propagate/kernels.py
@@ -0,0 +1,33 @@
+# Module containing various kernels
+
+import numpy as np
+from scipy.sparse import lil_matrix
+
+def rbf_kernel(x1, x2, sigma):
+	return np.exp(-np.linalg.norm(x1 - x2) ** 2 / (2 * sigma ** 2))
+
+def cotangent_kernel(Nodes, Meshes):
+	num_nodes = Nodes.shape[0]
+	W = lil_matrix((num_nodes, num_nodes))
+	print 'Constructing sparse affinity matrix...'
+	for c in Meshes:		
+		# Obtain vertices which comprise face
+		v0, v1, v2 = Nodes[c[0]], Nodes[c[1]], Nodes[c[2]]
+		
+		# Obtain cotangents of angles
+		cot0 = np.dot(v1-v0, v2-v0) / np.linalg.norm(np.cross(v1-v0, v2-v0))
+		cot1 = np.dot(v2-v1, v0-v1) / np.linalg.norm(np.cross(v2-v1, v0-v1))
+		cot2 = np.dot(v0-v2, v1-v2) / np.linalg.norm(np.cross(v0-v2, v1-v2))
+		
+		# Update weight matrix accordingly
+		W[c[1], c[2]] += cot0		
+		W[c[2], c[1]] += cot0
+		W[c[0], c[2]] += cot1
+		W[c[2], c[0]] += cot1
+		W[c[0], c[1]] += cot2
+		W[c[1], c[0]] += cot2
+		
+	return W
+	
+def inverse_distance(x1, x2, epsilon):
+	return 1.0/(np.linalg.norm(x1 - x2) + epsilon)
