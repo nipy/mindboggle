a33919eb322ee4e78a0a34b8b490b3e528764401
Arno Klein <arno@binarybottle.com>
2012-06-22 14:54:55 -0400

    Add debug_skip options to skip to code under construction.
    Former-commit-id: 14cba08d9782e52d22004f090136ab71143b91a0

--- mindboggle/utils/plot_label.py
+++ mindboggle/utils/plot_label.py
@@ -1,10 +1,22 @@
+#!/usr/bin/python
+"""
+Visualize .label labeled surface mesh data.
 
-# Comment out the top line (below the header) with the number of rows,
-# so that an appropriately sized numpy array is loaded.
+Comment out the top line (below the header) of the .label file
+containing the number of rows, to load an appropriately sized numpy array.
 
-label_file = '/home/arno/Documents/Projects/mindboggle/results/workingdir/Mindboggle_workflow/Atlas_workflow/_hemi_lh_subject_test/Write_label_files/mapflow/_Write_label_files30/lh.insula.label'
+(c) 2012  Mindbogglers (www.mindboggle.info), under Apache License Version 2.0
+"""
 
+import sys
 import numpy as np
+from matplotlib import pyplot as plt
+
+if len(sys.argv) < 2:
+    sys.exit('Usage: %s label-file-name' %sys.argv[0])
+    #label_file = '/projects/mindboggle/results/workingdir/Mindboggle_workflow/Atlas_workflow/_hemi_lh_subject_test/Write_label_files/mapflow/_Write_label_files30/lh.insula.label'
+else:
+    label_file = sys.argv[1]
 
 a = np.loadtxt(label_file)
 x = a[:,1]
