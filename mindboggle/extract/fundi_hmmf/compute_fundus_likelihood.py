0fd30e0b9d1a340b64487ba1e8841102939317ce
Arno Klein <arno@binarybottle.com>
2012-06-27 11:32:31 -0400

    Translate Yrjo Hame's HMMF fundus extraction code from Matlab. First attempt on all but connect_the_dots.py, which is next.
    Former-commit-id: 5aff83830cf1ab026bd22e0492b40e73933c11d4

--- mindboggle/extract/fundi_hmmf/compute_fundus_likelihood.py
+++ mindboggle/extract/fundi_hmmf/compute_fundus_likelihood.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 """
-Compute distance measures.
+Compute fundus likelihood values.
+
+Input
+
 
 Authors:
 Yrjo Hame  .  yrjo.hame@gmail.com  (original Matlab code)
@@ -15,50 +18,78 @@ import numpy as np
 #=================================
 # Compute fundus likelihood values
 #=================================
-def compute_fundus_likelihood(mean_curvatures, depths, sulci, ind):
+def compute_fundus_likelihood(curvatures, depths, sulci, sulcus_index):
     """
     Compute fundus likelihood values.
+
+    Inputs:
+    ------
+    curvatures: mean curvature values [#vertices x 1] numpy array
+    depths: depth values [#vertices x 1] numpy array
+    sulci: sulcus values [#vertices x 1] numpy array
+    sulcus_index: sulcus number [int]
+
+    Parameters:
+    ----------
+    threshold1
+    threshold2
+    threshold3
+    high_map_value
+    increment1
+    increment2
+
+    Output:
+    ------
+    L: fundus likelihood values [#vertices x 1] numpy array
+
     """
-    threshold1 = .6
-    threshold2 = .05
-    threshold3 = .3
-    high_map_value = .9
-    depth_search_increment1 = 0.01
-    depth_search_increment2 = 0.0001
+    threshold1 = 0.6
+    threshold2 = 0.05
+    threshold3 = 0.3
+    high_map_value = 0.9
+    increment1 = 0.01
+    increment2 = 0.0001
 
-    sulci(sulci != ind) = 0
+    slope_factor = np.log((1/high_map_value)-1)
 
-    # Switch curvature values to opposite
-    mean_curvatures = (-1 * mean_curvatures)
+    # Take the opposite of the curvature values
+    curvatures = (-1 * curvatures)
 
-    depth_search = 0
-    mass_left = 1
+    # Retain only sulcus numbers for the sulcus corresponding to sulcus_index
+    sulci[sulci != sulcus_index] = 0
+    Igt0 = sulci > 0
+    # Find sulcus depths and curvature values
+    len_sulcus = sum(Igt0)
+    sulcus_depths = depths[Igt0]
+    sulcus_curvatures = curvatures[Igt0]
 
     # Find depth value where less than threshold1 are larger
+    mass_left = 1
+    depth_search = 0
     while(mass_left > threshold1):
-        depth_search = depth_search + depth_search_increment1
-        mass_left = sum(depths(sulci > 0) > depth_search) / sum(sulci > 0)
-    half_depth = depth_search
+        depth_search = depth_search + increment1
+        mass_left = sum(sulcus_depths > depth_search) / len_sulcus
+    depth_found = depth_search
 
     # Find depth value where less than threshold2 are larger
     while(mass_left > threshold2):
-        depth_search = depth_search + depth_search_increment1
-        mass_left = sum(depths(sulci > 0) > depth_search) / sum(sulci > 0)
-    slope_depth = (-1/(depth_search - half_depth)) * log((1/high_map_value)-1)
+        depth_search = depth_search + increment1
+        mass_left = sum(sulcus_depths > depth_search) / len_sulcus
+    slope_depth = -slope_factor / (depth_search - depth_found)
 
-    # Find slope similarly for curvature values
+    # Find slope for curvature values
     mass_left = 1
     depth_search = 0
     while(mass_left > threshold3):
-        depth_search = depth_search + depth_search_increment2
-        mass_left = sum(mean_curvatures(sulci > 0) > depth_search) / sum(sulci > 0)
-    slope_mean_curvatures = -1/depth_search * log((1/high_map_value) - 1)
+        depth_search = depth_search + increment2
+        mass_left = sum(sulcus_curvatures > depth_search) / len_sulcus
+    slope_curvatures = -slope_factor / depth_search
 
     # Map curvature and depth values with sigmoidal functions to range [0,1]
-    st2 = 1 ./ (1 + exp(-slope_mean_curvatures * mean_curvatures))
-    st3 = 1 ./ (1 + exp(-slope_depth * (depths - half_depth)))
+    st2 = 1 / (1 + np.exp(-slope_curvatures * curvatures))
+    st3 = 1 / (1 + np.exp(-slope_depth * (depths - depth_found)))
 
-    L = st2 .* st3
-    L(sulci ==0) = 0
+    L = st2 * st3  # element-wise multiplication
+    L[sulci == 0] = 0
 
     return L
