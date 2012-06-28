1cf3c2816e0d94359f8c4a60c4e13238fbe4acab
Arno Klein <arno@binarybottle.com>
2012-06-28 10:55:00 -0400

    Continue to translate and document connect_the_dots()
    Former-commit-id: 92066ddb3ed48fbbc07b120879887c3098335d88

--- mindboggle/extract/fundi_hmmf/connect_the_dots.py
+++ mindboggle/extract/fundi_hmmf/connect_the_dots.py
@@ -11,8 +11,7 @@ Arno Klein  .  arno@mindboggle.info  .  www.binarybottle.com
 """
 
 import numpy as np
-
-from simple_point_test import simple_point_test
+from find_neighbors import find_neighbors
 
 #--------------------
 # Compute probability
@@ -20,6 +19,18 @@ from simple_point_test import simple_point_test
 def prob(wl, li, q, qneighs, wt_neighbors):
     """
     Compute probability
+    Inputs:
+    ------
+    wl: ????
+    li: ????
+    q: ????
+    qneighs: ????
+    wt_neighbors: ????
+
+    Output:
+    ------
+    p: probability????
+
     """
     p = -1 * (q * np.sqrt((wl-li)**2) + (wt_neighbors * sum((q - qneighs)**2)))
     return p
@@ -27,93 +38,101 @@ def prob(wl, li, q, qneighs, wt_neighbors):
 #-----------------------
 # Test for simple points
 #-----------------------
-def simple_point_test(faces, ind, values):
+def simple_point_test(faces, index, values, thr=0.5):
     """
-    Find anchor points
-
-    Assign maximum likelihood vertices as "anchor points"
-    for use in constructing fundus curves.
-    Ensure tht anchor points are not close to one another.
+    Test to see if vertex is a "simple point".
 
     Inputs:
     ------
-    vertices: [#vertices x 3] numpy array
-    L: fundus likelihood values [#vertices x 1] numpy array
-    min_distances: [#vertices x 1] numpy array
-    distance_threshold: distance threshold
-    max_distance: maximum distance
+    faces: [#faces x 3] numpy array
+    index: index of vertex
+    values: likelihood values: [#vertices x 1] numpy array
+    thr: likelihood threshold
 
     Output:
     ------
-    P: anchor points [#vertices x 1] numpy array
+    sp: simple point decision (0 or 1)
+
+    Calls:
+    -----
+    find_neighbors()
 
     """
-    import numpy as np
-    from find_neighbors import find_neigbhors
 
-    thr = 0.5
-    neighs = find_neighbors(faces,ind)
+    max_set_size = 20
+
+    # Find neighbors to the input vertex and
+    # count the number of "inside" and "outside" neighbors
+    # (n_sets for neighbors greater than the likelihood threshold, thr,
+    #  n_outside for neighbors less than or equal to thr)
+    neighs = find_neighbors(faces, index)
     neighvals = values(neighs)
-    nOutside = sum(neighvals <= thr)
-    nInside = sum(neighvals > thr)
+    n_sets = sum(neighvals > thr)
+    n_outside = sum(neighvals <= thr)
 
-    if (nOutside == 0 || nInside == 0)
+    # If the number of inside or outside neighbors is zero,
+    # than the vertex IS NOT a simple point
+    if n_outside * n_sets == 0:
         sp = 0
-        return
-
-    if (nOutside == 1)
+    # Or if either the number of inside or outside neighbors is one,
+    # than the vertex IS a simple point
+    elif n_outside == 1 or n_sets == 1:
         sp = 1
-        return
-
-    if (nInside == 1)
-        sp = 1
-        return
-
-    neighsInside = neighs[neighvals > thr]
-    nSets = len(neighsInside)
-
-    sets = np.zeros(nSets,20)
-    setLabels = np.transpose(range(1,nSets+1))
-
-    values(ind) = -1
-
-    for i in range(nSets):
-        currNeighs = find_neighbors(faces,neighsInside[i])
-        currNeighs = currNeighs(values(currNeighs)>thr)
-        sets(i,1:size(currNeighs,1)) = np.transpose(currNeighs)
-        sets(i,size(currNeighs,1) + 1) = neighsInside[i]
-
-    change = 1
-    while (change > 0):
-        change = 0
-        for i in range(nSets):
-            for j in range(nSets):
-               if (j>i && setLabels[i] != setLabels[j]):
-                    iInds = np.unique(sets(i,:))
-                    iInds = iInds(iInds > 0)
-                    iSize = size(iInds,2)
-
-                    jInds = np.unique(sets(j,:))
-                    jInds = jInds(jInds > 0)
-                    jSize = size(jInds,2)
-
-                    totInds = unique([iInds jInds])
-                    totSize = size(totInds,2)
-
-                    if (totSize < iSize + jSize):
-
-                        minLabel = min(setLabels[j],setLabels[i])
-
-                        setLabels[i] = minLabel
-                        setLabels[j] = minLabel
-
-                        change = 1
-
-    numberOfSeparateSets = size(unique(setLabels),1)
-    if (numberOfSeparateSets < 2):
-        sp = 1
+    # Otherwise, ????
     else:
-        sp = 0
+        neighs_inside = neighs[neighvals > thr]
+
+        # Initialize sets numpy array [#neighbors>thr x max_set_size]
+        sets = np.zeros((n_sets, max_set_size))
+        set_labels = range(1, n_sets + 1)
+
+        # Reset vertex value
+        values[index] = -1
+
+        # For each neighbor exceeding the threshold,
+        # find its neighbors that also exceed the threshold,
+        # then store these neighbors' values in a row of the sets array
+        for i in range(n_sets):
+            current_neighs = find_neighbors(faces, neighs_inside[i])
+            current_neighs = current_neighs[values[current_neighs] > thr]
+            len_current = len(current_neighs)
+            if len_current > max_set_size - 1:
+                len_current = max_set_size - 1
+                current_neighs = current_neighs[range(len_current)]
+            sets[i, range(len_current)] = current_neighs
+            sets[i, len_current] = neighs_inside[i]
+
+        # ????
+        change = 1
+        while (change > 0):
+            change = 0
+            for i in range(n_sets - 1):
+                for j in range(i + 1, n_sets):
+                   if set_labels[i] != set_labels[j]:
+                        iInds = np.unique(sets[i, :])
+                        iInds = iInds[iInds > 0]
+                        iSize = np.shape(iInds)[1]
+
+                        jInds = np.unique(sets[j, :])
+                        jInds = jInds(jInds > 0)
+                        jSize = np.shape(jInds)[1]
+
+                        totInds = np.unique([iInds jInds])
+                        totSize = np.shape(totInds)[1]
+
+                        if (totSize < iSize + jSize):
+
+                            min_label = min([set_labels[j], set_labels[i]])
+                            set_labels[i] = min_label
+                            set_labels[j] = min_label
+
+                            change = 1
+
+        n_separate_sets = np.shape(np.unique(set_labels))[0]
+        if (n_separate_sets < 2):
+            sp = 1
+        else:
+            sp = 0
 
     return sp
 
@@ -151,6 +170,7 @@ def connect_the_dots(L, L_init, faces, dots, neighbors, indices):
     Calls:
     -----
     prob()
+    simple_point_test()
 
     """
 
@@ -214,44 +234,42 @@ def connect_the_dots(L, L_init, faces, dots, neighbors, indices):
                         neighs = neighbors[indices == i, :]
                         neighs = np.unique(neighs[neighs > 0])
 
-                        # Compute the probability for this point ????
-                        q = max([C[i] - step_size, 0])
-                        prob_down = prob(wl, L[i], q, C[neighs], wt_neighbors)
-                        
                         # Compute a decrement...????
-                        decr = mult * (prob_down - probs[i]) / step_size
+                        # based on the probability for this point ????
+                        q = max([C[i] - step_size, 0])
+                        prob_decr = prob(wl, L[i], q, C[neighs], wt_neighbors)
+                        decr = mult * (prob_decr - probs[i]) / step_size
 
-                        # Test to update the connectivity value
-                        # (default is not to update)
-                        update = 0
+                        # Test to update the connectivity value:
                         # Update the connectivity value
                         # if it is far from L_threshold
                         if np.abs(C[i] - L_threshold) > np.abs(decr):
                             update = 1
-                        # Otherwise...
-                        elif decr > 0:
-                            # If the dot value exceeds L_threshold
-                            # do not update the connectivity value
-                            if dots[i] > L_threshold:
-                                update = 0
-                            # Or if the dot value is within L_threshold
-                            # and if the vertex is a "simple point"
-                            # then update the connectivity value
-                            else:
-                                Cnew_copy = Cnew.copy()
-                                Cnew_copy[i] = C[i] - decr
-                                update = simple_point_test(faces, i, Cnew_copy)
+                        # Otherwise, if the decrement is positive,
+                        # the dot value is less than or equal to L_threshold,
+                        # and the vertex is a "simple point,"
+                        # then update the connectivity value
+                        elif decr > 0 and dots[i] <= L_threshold:
+                            Cnew_copy = Cnew.copy()
+                            Cnew_copy[i] = C[i] - decr
+                            update = simple_point_test(faces, i, Cnew_copy, L_threshold)
+                        # Or if the decrement is negative,
+                        # and the vertex is a "simple point,"
+                        # then update the connectivity value
                         elif decr < 0:
                             Cnew_copy = Cnew.copy()
                             Cnew_copy[i] = C[i] - decr
                             Cnew_copy = 1 - Cnew_copy
-                            update = simple_point_test(faces, i, Cnew_copy)
+                            update = simple_point_test(faces, i, Cnew_copy, L_threshold)
+                        # The default is not to update
+                        else:
+                            update = 0
 
                         # Update the connectivity and probability values
                         if update:
                             if decr > 0:
                                 Cnew[i] = max([C[i] - decr, 0])
-                            else:
+                            elif decr < 0:
                                 Cnew[i] = min([C[i] - decr, 1])
                             probs[i] = prob(wl, L[i], Cnew[i], C[neighs], wt_neighbors)
 
@@ -263,8 +281,7 @@ def connect_the_dots(L, L_init, faces, dots, neighbors, indices):
             if count > 0:
                 n_points = sum(C > L_threshold)
                 diff_probs = (n_probs - n_probs_previous) / n_positive
-                if diff_probs < diff_threshold and \
-                   n_points == n_points_previous:
+                if diff_probs < diff_threshold and n_points == n_points_previous:
                     end_flag += 1
 
             # Reset for next iteration
