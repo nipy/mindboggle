8432c6f4d2150ab4988104245e2e9a2956fd52be
Arno Klein <arno@binarybottle.com>
2012-07-06 20:38:36 -0400

    Debugging extract_fundi() functions...
    Former-commit-id: a2aee64036a913a46629e146fb510799de06d384

--- mindboggle/extract/fundi_hmmf/extract_sulci.py
+++ mindboggle/extract/fundi_hmmf/extract_sulci.py
@@ -22,38 +22,7 @@ Authors:
 """
 
 import numpy as np
-
-#---------------
-# Find neighbors
-#---------------
-def find_neighbors(faces, index):
-    """
-    For a set of surface mesh faces and the index of a surface vertex,
-    find unique indices for neighboring vertices.
-
-    Inputs:
-    ------
-    faces: surface mesh vertex indices [#faces x 3] numpy array
-    index: index of surface vertex
-
-    Output:
-    ------
-    I: [#neighbors x 1] numpy array
-
-    """
-    # Create list of vertex indices sharing the same faces as "index"
-    I = [faces[np.where(faces[:,i] == index)[0]].tolist() for i in range(3)]
-
-    # Create single list from nested lists
-    I = [int(item) for sublist in I for subsublist in sublist for item in subsublist]
-
-    if len(I) > 0:
-
-        # Find unique indices not equal to "index"
-        I = np.unique(I)
-        I = I[I != index]
-
-    return I
+from find_neighbors import find_neighbors
 
 #========================
 # Segment surface patches
