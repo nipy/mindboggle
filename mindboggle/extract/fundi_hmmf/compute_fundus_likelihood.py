5221a4b02a38ac734331cb7ce84d9819237da6be
Arno Klein <arno@binarybottle.com>
2012-07-06 20:38:36 -0400

    Debugging extract_fundi() functions...
    Former-commit-id: 39772208bebb65fa90748562c906add8d7e2589e

--- mindboggle/extract/fundi_hmmf/compute_fundus_likelihood.py
+++ mindboggle/extract/fundi_hmmf/compute_fundus_likelihood.py
@@ -2,9 +2,10 @@
 """
 Compute fundus likelihood values.
 
+
 Authors:
-Yrjo Hame  .  yrjo.hame@gmail.com
-Arno Klein  .  arno@mindboggle.info  .  www.binarybottle.com
+    Yrjo Hame  .  yrjo.hame@gmail.com
+    Arno Klein  .  arno@mindboggle.info  .  www.binarybottle.com
 
 (c) 2012  Mindbogglers (www.mindboggle.info), under Apache License Version 2.0
 
@@ -15,7 +16,7 @@ import numpy as np
 #=================================
 # Compute fundus likelihood values
 #=================================
-def compute_fundus_likelihood(curvatures, depths, sulci, sulcus_index):
+def compute_likelihood(sulci, sulcus_index, depths, curvatures):
     """
     Compute fundus likelihood values.
 
@@ -53,46 +54,52 @@ def compute_fundus_likelihood(curvatures, depths, sulci, sulcus_index):
     increment1 = 0.01
     increment2 = 0.0001
 
-    slope_factor = np.log((1 / high_map_value) - 1)
+    slope_factor = np.log((1. / high_map_value) - 1)
 
     # Take the opposite of the curvature values
-    curvatures = (-1 * curvatures)
+    curvatures = -curvatures
 
     # Retain only sulcus numbers for the sulcus corresponding to sulcus_index
-    sulci[sulci != sulcus_index] = 0
-    Igt0 = sulci > 0
+    sulcus_bool = sulci == sulcus_index
+
     # Find sulcus depths and curvature values
-    len_sulcus = sum(Igt0)
-    sulcus_depths = depths[Igt0]
-    sulcus_curvatures = curvatures[Igt0]
+    len_sulcus = sum(sulcus_bool)
+    sulcus_depths = depths[sulcus_bool]
+    sulcus_curvatures = curvatures[sulcus_bool]
 
     # Find depth value where less than threshold1 are larger
     mass_left = 1
     depth_search = 0
-    while(mass_left > threshold1):
-        depth_search = depth_search + increment1
+    while mass_left > threshold1:
+        depth_search += increment1
         mass_left = sum(sulcus_depths > depth_search) / len_sulcus
     depth_found = depth_search
 
     # Find depth value where less than threshold2 are larger
-    while(mass_left > threshold2):
-        depth_search = depth_search + increment1
+    while mass_left > threshold2:
+        depth_search += increment1
         mass_left = sum(sulcus_depths > depth_search) / len_sulcus
-    slope_depth = -slope_factor / (depth_search - depth_found)
+    if depth_search == depth_found:
+        slope_depth = -np.Inf
+    else:
+        slope_depth = -slope_factor / (depth_search - depth_found)
 
     # Find slope for curvature values
     mass_left = 1
     depth_search = 0
-    while(mass_left > threshold3):
-        depth_search = depth_search + increment2
+    while mass_left > threshold3:
+        depth_search += increment2
         mass_left = sum(sulcus_curvatures > depth_search) / len_sulcus
-    slope_curvatures = -slope_factor / depth_search
+    if not depth_search:
+        slope_curvatures = -np.Inf
+    else:
+        slope_curvatures = -slope_factor / depth_search
 
     # Map curvature and depth values with sigmoidal functions to range [0,1]
     st2 = 1 / (1 + np.exp(-slope_curvatures * curvatures))
     st3 = 1 / (1 + np.exp(-slope_depth * (depths - depth_found)))
 
     L = st2 * st3  # element-wise multiplication
-    L[sulci == 0] = 0
+    L[~sulcus_bool] = 0
 
     return L
