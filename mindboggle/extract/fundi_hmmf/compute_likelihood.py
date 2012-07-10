b1ebddd1d4c7150025a8e8e537406018b2b334ea
Arno Klein <arno@binarybottle.com>
2012-07-10 19:45:37 -0400

    Reduce inputs and simplify outputs (lists) for fundus extraction functions, so that individual sulcus vertices and values are accepted by functions rather than entire mesh arrays.  Aggregate extract_fundus() with extract_fundi().
    Former-commit-id: 5b9453b2f7685a1f70f2022e5a70eab188433ba6

--- mindboggle/extract/fundi_hmmf/compute_likelihood.py
+++ mindboggle/extract/fundi_hmmf/compute_likelihood.py
@@ -19,7 +19,7 @@ print_debug = 0
 #=================================
 # Compute fundus likelihood values
 #=================================
-def compute_likelihood(sulcus, sulcus_depths, sulcus_curvatures):
+def compute_likelihood(depths, curvatures):
     """
     Compute fundus likelihood values.
 
@@ -27,9 +27,8 @@ def compute_likelihood(sulcus, sulcus_depths, sulcus_curvatures):
 
     Inputs:
     ------
-    sulcus: sulcus values [#sulcus vertices x 1] numpy array
-    sulcus_curvatures: mean curvature values [#sulcus vertices x 1] numpy array
-    sulcus_depths: depth values [#sulcus vertices x 1] numpy array
+    curvatures: mean curvature values [#sulcus vertices x 1] numpy array
+    depths: depth values [#sulcus vertices x 1] numpy array
 
     Parameters:
     ----------
@@ -44,7 +43,7 @@ def compute_likelihood(sulcus, sulcus_depths, sulcus_curvatures):
 
     Output:
     ------
-    sulcus_likelihood: fundus likelihood values [#sulcus vertices x 1] numpy array
+    likelihoods: likelihood values [#sulcus vertices x 1] numpy array
 
     """
     # Parameters for adaptive thresholding
@@ -60,64 +59,64 @@ def compute_likelihood(sulcus, sulcus_depths, sulcus_curvatures):
     slope_factor = np.log((1. / high_map_value) - 1)
 
     # Take the opposite of the curvature values
-    sulcus_curvatures = -sulcus_curvatures
+    curvatures = -curvatures
 
-    # Find sulcus depths and curvature values
-    len_sulcus = np.float(len(sulcus))
+    # Find depths and curvature values
+    len_object = np.float(len(depths))
 
     """
     # Alternative to resetting the threshold values below?
-    depth_found = scoreatpercentile(sulcus_depths, depth_threshold2)
+    depth_found = scoreatpercentile(depths, depth_threshold2)
     slope_depth = -slope_factor / (search - depth_found)
     # Map depth values with sigmoidal function to range [0,1]
     st_depths = 1 / (1 + np.exp(-slope_depth * (depths - depth_found)))
     """
 
-    # Find depth value where less than depth_threshold1 of sulcus vertices are deeper
+    # Find depth value where less than depth_threshold1 of vertices are deeper
     mass_left = 1
     search = 0
     while mass_left > depth_threshold1:
         search += depth_increment
-        mass_left = sum(sulcus_depths > search) / len_sulcus
+        mass_left = sum(depths > search) / len_object
     depth_found = search
     if print_debug:
         print(str(depth_threshold1) +
-              ' of sulcus vertices are deeper than ' + str(search))
+              ' of vertices are deeper than ' + str(search))
 
     # Find depth value where less than depth_threshold2 of sulcus vertices are deeper
     while mass_left > depth_threshold2:
         search += depth_increment
-        mass_left = sum(sulcus_depths > search) / len_sulcus
+        mass_left = sum(depths > search) / len_object
     if search == depth_found:
-        st_depths = np.zeros(len_sulcus)
+        st_depths = np.zeros(len_object)
     else:
         slope_depth = -slope_factor / (search - depth_found)
         # Map depth values with sigmoidal function to range [0,1]
-        st_depths = 1 / (1 + np.exp(-slope_depth * (sulcus_depths - depth_found)))
+        st_depths = 1 / (1 + np.exp(-slope_depth * (depths - depth_found)))
     if print_debug:
         print(str(depth_threshold2) +
-              ' of sulcus vertices are deeper than ' + str(search))
+              ' of vertices are deeper than ' + str(search))
 
     # Find slope for curvature values
     mass_left = 1
     search = 0
     while mass_left > curvature_threshold:
         search += curvature_increment
-        mass_left = sum(sulcus_curvatures > search) / len_sulcus
+        mass_left = sum(curvatures > search) / len_object
     if print_debug:
         print(str(curvature_threshold) +
-              ' of sulcus vertices have greater curvature than ' + str(search))
+              ' of vertices have greater curvature than ' + str(search))
     slope_curvature = -slope_factor / search
     # Prevent precsion errors
     if slope_curvature > 1000:
         if print_debug:
             print('(high slope curvature: ' + str(slope_curvature) + ')')
-        sulcus_curvatures[sulcus_curvatures < 0] = np.Inf
-        sulcus_curvatures[sulcus_curvatures > 0] = 0
+        curvatures[curvatures < 0] = np.Inf
+        curvatures[curvatures > 0] = 0
     # Map curvature values with sigmoidal function to range [0,1]
-    st_curvatures = 1 / (1 + np.exp(-slope_curvature * sulcus_curvatures))
+    st_curvatures = 1 / (1 + np.exp(-slope_curvature * curvatures))
 
-    # Assign likelihood values to sulcus vertices
-    sulcus_likelihood = st_depths * st_curvatures  # element-wise multiplication
+    # Assign likelihood values to vertices
+    likelihoods = st_depths * st_curvatures  # element-wise multiplication
 
-    return sulcus_likelihood
+    return likelihoods
--- mindboggle/extract/fundi_hmmf/find_anchors.py
+++ mindboggle/extract/fundi_hmmf/find_anchors.py
@@ -17,7 +17,7 @@ import numpy as np
 #===================
 # Find anchor points
 #===================
-def find_anchor_points(vertices, L, min_distances, thr=0.5, max_distance=8):
+def find_anchors(vertices, L, min_directions, thr, min_distance, max_distance):
     """
     Find anchor points.
 
@@ -25,17 +25,23 @@ def find_anchor_points(vertices, L, min_distances, thr=0.5, max_distance=8):
     for use in constructing fundus curves.
     Ensure that the anchor points are not close to one another.
 
+    ????
+    Should perhaps change this code because of instability.
+    Only the sulcus end points are necessary,
+    so that the fundus doesn't shrink.
+
     Inputs:
     ------
-    vertices: [#vertices x 3] numpy array
-    L: fundus likelihood values [#vertices x 1] numpy array
-    min_distances: [#vertices x 1] numpy array
-    thr: distance threshold
+    vertices: [#sulcus vertices x 3] numpy array
+    L: fundus likelihood values [#sulcus vertices x 1] numpy array
+    min_directions: [#sulcus vertices x 1] numpy array
+    thr: likelihood threshold
+    min_distance: minimum distance
     max_distance: maximum distance
 
     Output:
     ------
-    P: anchor points [#vertices x 1] numpy array
+    anchors: list of anchors (subset of surface mesh vertex indices)
 
     """
 
@@ -45,27 +51,24 @@ def find_anchor_points(vertices, L, min_distances, thr=0.5, max_distance=8):
 
     # Initialize anchor point array and reset maximum likelihood value
     P = np.zeros(len(L))
-    check_list = P.copy()
-    check_list[imax] = 1
     P[imax] = 1
     L[imax] = -1
 
-    # Loop until all sulcus vertices are checked
-    while min(check_list) == 0 and maxL > 0.5:
+    # Loop through high-likelihood vertices
+    while maxL > thr:
 
         # Find and reset maximum likelihood value
         maxL = max(L)
         imax = np.where(L == maxL)[0]
-        check_list[imax] = 1
         L[imax] = -1
 
-        # Anchor point vertices and distance values
+        # Anchor points and distance values
         IP = P > 0
         lenP = sum(IP)
         P_vertices = vertices[IP,:]
-        P_distances = min_distances[IP]
+        P_directions = min_directions[IP]
 
-        # Find anchor point vertices close to vertex with maximum likelihood value
+        # Find anchor points close to vertex with maximum likelihood value
         i = 0
         found = 0
         while i < lenP and found == 0:
@@ -74,14 +77,12 @@ def find_anchor_points(vertices, L, min_distances, thr=0.5, max_distance=8):
             D = np.linalg.norm(P_vertices[i,:] - vertices[imax,:])
 
             # Compute directional distance between points if they are close
-            # Perhaps change because of instability -- only sulcus end points necessary,
-            # so that the fundus doesn't shrink -- ????
-            if max_distance > D >= thr:
-                dirV = np.dot(P_vertices[i,:] - vertices[imax,:], P_distances[i])
+            if max_distance > D >= min_distance:
+                dirV = np.dot(P_vertices[i,:] - vertices[imax,:], P_directions[i])
                 D = np.linalg.norm(dirV)
 
             # If distance less than threshold, consider the point found
-            if D < thr:
+            if D < min_distance:
                 found = 1
 
             i += 1
@@ -91,4 +92,6 @@ def find_anchor_points(vertices, L, min_distances, thr=0.5, max_distance=8):
         if not found:
             P[imax] = 1
 
-    return P
+    anchors = np.where(P)[0].tolist()
+
+    return anchors
