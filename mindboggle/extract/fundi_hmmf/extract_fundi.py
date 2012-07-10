8020ba2f8b759807933ab510cbe26c1bb47b418c
Arno Klein <arno@binarybottle.com>
2012-07-10 19:45:37 -0400

    Reduce inputs and simplify outputs (lists) for fundus extraction functions, so that individual sulcus vertices and values are accepted by functions rather than entire mesh arrays.  Aggregate extract_fundus() with extract_fundi().
    Former-commit-id: 7bb687d40b26a6051e27152be66b6c515b637885

--- mindboggle/extract/fundi_hmmf/extract_fundi.py
+++ mindboggle/extract/fundi_hmmf/extract_fundi.py
@@ -15,17 +15,19 @@ import numpy as np
 from extract_sulci import extract_sulci
 from compute_likelihood import compute_likelihood
 from find_neighbors import find_neighbors
-from find_anchor_points import find_anchor_points
-from connect_the_dots import connect_the_dots
+from find_anchors import find_anchors
+from connect_anchors import connect_anchors
 from test import test_fundi_hmmf
 
-#=================
-# Extract a fundus
-#=================
-def extract_fundus(L, sulci, sulcus_index, vertices, faces, min_directions,
-                   thr=0.5, min_sulcus_size=30, max_neighbors=15, max_distance=8):
+#==================
+# Extract all fundi
+#==================
+def extract_fundi(vertices, faces, depths, mean_curvatures, min_directions,
+                  depth_threshold=0.2, min_sulcus_size=50, thr=0.5,
+                  min_distance=5, max_distance=8):
+#max_neighbors=15
     """
-    Extract a single fundus.
+    Extract all fundi.
 
     A fundus is a connected set of high-likelihood vertices in a surface mesh.
 
@@ -39,77 +41,30 @@ def extract_fundus(L, sulci, sulcus_index, vertices, faces, min_directions,
     min_distances: [#vertices x 1] numpy array
     thr: likelihood threshold
     min_sulcus_size: minimum sulcus size from which to find a fundus
-    max_neighbors: maximum number of neighbors per sulcus vertex
+    min_distance: minimum distance
+    max_distance: maximum distance
 
-    Output:
-    ------
-    fundus: [#fundus vertices x 1] numpy array
+#    max_neighbors: maximum number of neighbors per sulcus vertex
 
-    Calls:
-    -----
-    find_neighbors(): numpy array of indices
-    find_anchor_points()
-    connect_the_dots()
-
-    """
-
-    # Retain only likelihood values for the sulcus corresponding to
-    # sulcus_index
-    L[sulci != sulcus_index] = 0
-
-    # If the size of the sulcus (according to likelihood values)
-    # is sufficiently large, continue
-    if sum(L > thr) > min_sulcus_size:
-
-        # Find neighbors for each sulcus vertex and arrange as lists within a list
-        sulcus_indices = np.where(L > 0)[0]
-        sulcus_neighbors = []
-        for index in sulcus_indices:
-            neighbors = find_neighbors(faces, index)
-            sulcus_neighbors.append(neighbors)
-
-        # Find fundus points
-        print('Find anchor points...')
-        anchors = find_anchor_points(vertices, L, min_directions, thr, max_distance)
-        if any(anchors):
-            print('Connect anchor points to construct a fundus curve for each sulcus...')
-            fundus = connect_the_dots(L, faces, anchors, sulcus_neighbors, sulcus_indices, thr)
-        else:
-            fundus = np.zeros(len(sulci))
-    else:
-        print('Sulcus too small: ' + str(sum(L > thr)))
-        fundus = np.zeros(len(sulci))
-
-    return fundus
-
-#==================
-# Extract all fundi
-#==================
-def extract_fundi(vertices, faces, depths, mean_curvatures, min_directions,
-                  depth_threshold=0.2, min_sulcus_size=50):
-    """
-    Extract all fundi.
-
-    Inputs:
-    ------
     vertices: [#vertices x 3] numpy array
     faces: vertices for polygons [#faces x 3] numpy array
     depths: depth values [#vertices x 1] numpy array
     mean_curvatures: mean curvature values [#vertices x 1] numpy array
     min_directions: directions of minimum curvature [3 x #vertices] numpy array
     depth_threshold: depth threshold for defining sulci
+    thr: likelihood threshold
 
     Output:
     ------
-    fundi: [#vertices x #sulci] numpy array
-           Each column represents a fundus for a specific sulcus.
-           Values range from 0 to 1; values above 0.5 are considered part of the fundus.
+    fundi: list of #sulci lists of vertex indices.
 
     Calls:
     -----
     extract_sulci()
-    compute_fundus_likelihood()
-    extract_fundus()
+    compute_likelihood()
+    find_neighbors()
+    find_anchors()
+    connect_anchors()
 
     """
 
@@ -117,7 +72,7 @@ def extract_fundi(vertices, faces, depths, mean_curvatures, min_directions,
     if load_em:
         import pickle
 
-    # Extract sulci
+    # Extract sulci (vertex indices for each sulcus)
     if load_em:
         sulci = pickle.load(open("/drop/sulci.p","rb"))
         n_sulci = int(pickle.load(open("/drop/n_sulci.p","rb")))
@@ -125,39 +80,52 @@ def extract_fundi(vertices, faces, depths, mean_curvatures, min_directions,
         sulci, n_sulci = extract_sulci(faces, depths, depth_threshold,
                                        min_sulcus_size)
 
-    # Compute fundus likelihood values
-    if load_em:
-        n_vertices = len(depths)
-        L = pickle.load(open("/drop/L.p","rb"))
-    else:
-        print('Compute fundus likelihood values for ' + str(n_sulci) + ' sulci...')
-        n_vertices = len(depths)
-        L = np.zeros(n_vertices)
-        for sulcus_index in range(1, n_sulci + 1):
-            print('Compute fundus likelihood for sulcus ' + str(sulcus_index) + '...')
-
-            # Retain only sulcus numbers for the sulcus corresponding to sulcus_index
-            is_sulcus = sulci == sulcus_index
-            sulcus = sulci[is_sulcus]
-
-            # Find sulcus depths and curvature values
-            sulcus_depths = depths[is_sulcus]
-            sulcus_curvatures = mean_curvatures[is_sulcus]
-
-            sulcus_likelihood = compute_likelihood(sulcus, sulcus_depths,
-                                                   sulcus_curvatures)
-            L[is_sulcus] += sulcus_likelihood
-
-    # Extract individual fundi
+    # For each sulcus...
     print('Extract fundi from ' + str(n_sulci) + ' sulci...')
-    fundi = np.zeros((n_vertices, n_sulci))
-    for sulcus_index in range(1, n_sulci + 1):
-        print('Extract fundus from sulcus ' + str(sulcus_index) + '...')
-        fundi[:, sulcus_index - 1] = \
-        extract_fundus(L, sulci, sulcus_index, vertices, faces, min_directions)
+    fundi = []
+    for i, sulcus in enumerate(sulci):
 
-    # Remove empty fundi for sulci deemed too small in extract_fundus()
-    fundi = fundi[:, np.sum(fundi, axis = 0) > 0]
+        # Compute fundus likelihood values
+        print('Compute fundus likelihood for sulcus ' + str(i + 1) + '...')
+        sulcus_likelihoods = compute_likelihood(depths[sulcus],
+                                                mean_curvatures[sulcus])
+
+        # If the size of the sulcus (according to likelihood values)
+        # is sufficiently large, continue
+        if sum(sulcus_likelihoods > thr) > min_sulcus_size:
+            I = sulcus[sulcus_likelihoods > 0]
+            L = sulcus_likelihoods[sulcus_likelihoods > 0]
+
+            # Find fundus points
+            print('Find fundus points for sulcus ' + str(i + 1) + '...')
+            anchors = find_anchors(vertices[I], L, min_directions[I],
+                                   thr, min_distance, max_distance)
+            if any(anchors):
+
+                # Remove faces that contain a non-sulcus vertex
+                print('Remove faces that contain a non-sulcus vertex...')
+                fs = frozenset(sulcus)
+                faces_sulcus = [lst for lst in faces
+                                if len(fs.intersection(lst))==3]
+                faces_sulcus = np.reshape(np.ravel(faces_sulcus), (-1, 3))
+
+                # Replace mesh indices with sulcus indices
+                print('Replace mesh indices with sulcus indices')
+                Iset = np.unique(faces)
+                anchors2 = anchors.copy()
+                faces_sulcus2 = faces_sulcus.copy()
+                for index_new, index_old in enumerate(Iset):
+                    anchors2[I == index_old] = index_new
+                    faces_sulcus2[I == index_old] = index_new
+
+                # Extract fundus
+                print('Connect fundus points for sulcus ' + str(i + 1) + '...')
+                fundi.append(
+                      connect_anchors(anchors2, I2, faces_sulcus2, L, thr))
+            else:
+                fundi.append([])
+        else:
+            fundi.append([])
 
     return fundi
 
