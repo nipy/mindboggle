93697c29551705b5544ccf0b576e73251e05f324
Arno Klein <arno@binarybottle.com>
2012-07-11 15:41:49 -0400

    Remove positive likelihood filter in extract_fundi() to simplify subsequent function inputs.
    Former-commit-id: 153425634ba9706563c49e35d049fe3027e3a98a

--- mindboggle/extract/fundi_hmmf/extract_fundi.py
+++ mindboggle/extract/fundi_hmmf/extract_fundi.py
@@ -14,18 +14,15 @@ import numpy as np
 
 from extract_sulci import extract_sulci
 from compute_likelihood import compute_likelihood
-from find_neighbors import find_neighbors
 from find_anchors import find_anchors
 from connect_anchors import connect_anchors
-from test import test_fundi_hmmf
 
 #==================
 # Extract all fundi
 #==================
 def extract_fundi(vertices, faces, depths, mean_curvatures, min_directions,
-                  depth_threshold=0.2, min_sulcus_size=50, thr=0.5,
+                  depth_threshold=0.2, thr=0.5, min_sulcus_size=50,
                   min_distance=5, max_distance=8):
-#max_neighbors=15
     """
     Extract all fundi.
 
@@ -33,19 +30,6 @@ def extract_fundi(vertices, faces, depths, mean_curvatures, min_directions,
 
     Inputs:
     ------
-    L: fundus likelihood values [#vertices x 1] numpy array
-    sulci: sulcus values [#vertices x 1] numpy array
-    sulcus_index: sulcus number [int]
-    vertices: [#vertices x 3] numpy array
-    faces: vertices for polygons [#faces x 3] numpy array
-    min_distances: [#vertices x 1] numpy array
-    thr: likelihood threshold
-    min_sulcus_size: minimum sulcus size from which to find a fundus
-    min_distance: minimum distance
-    max_distance: maximum distance
-
-#    max_neighbors: maximum number of neighbors per sulcus vertex
-
     vertices: [#vertices x 3] numpy array
     faces: vertices for polygons [#faces x 3] numpy array
     depths: depth values [#vertices x 1] numpy array
@@ -53,6 +37,9 @@ def extract_fundi(vertices, faces, depths, mean_curvatures, min_directions,
     min_directions: directions of minimum curvature [3 x #vertices] numpy array
     depth_threshold: depth threshold for defining sulci
     thr: likelihood threshold
+    min_sulcus_size: minimum sulcus size from which to find a fundus
+    min_distance: minimum distance
+    max_distance: maximum distance
 
     Output:
     ------
@@ -87,37 +74,23 @@ def extract_fundi(vertices, faces, depths, mean_curvatures, min_directions,
     for i_sulcus, sulcus in enumerate(sulci):
 
         # Compute fundus likelihood values
-        print('Compute fundus likelihood for sulcus ' + str(i_sulcus + 1) + '...')
-        """
-        if load_em:
-            sulcus_likelihoods = pickle.load(open(load_path + "L.p","rb"))
-        else:
-        """
+        print('Compute fundus likelihood for sulcus ' +
+              str(i_sulcus + 1) + '...')
         sulcus_likelihoods = compute_likelihood(depths[sulcus],
                                                 mean_curvatures[sulcus])
 
-        # If the size of the sulcus (according to likelihood values)
-        # is sufficiently large, continue
+        # If the sulcus has enough high-likelihood vertices, continue
         if sum(sulcus_likelihoods > thr) > min_sulcus_size:
-            """
-            if load_em:
-
-                L = pickle.load(open(load_path + "L.p","rb"))
-                I = pickle.load(open(load_path + "I.p","rb"))
-            else:
-            """
-            I = [i for i,x in enumerate(sulcus) if sulcus_likelihoods[i] > 0]
-            L = sulcus_likelihoods[sulcus_likelihoods > 0]
-                #np.array([x for x in sulcus_likelihoods if x > 0])
 
             # Find fundus points
-            print('Find fundus points for sulcus ' + str(i_sulcus + 1) + '...')
-            anchors = find_anchors(vertices[I], L, min_directions[I],
+            print('Find fundus points for sulcus ' +
+                  str(i_sulcus + 1) + '...')
+            anchors = find_anchors(vertices[sulcus, :], sulcus_likelihoods,
+                                   min_directions[sulcus],
                                    thr, min_distance, max_distance)
             if len(anchors) > 0:
 
                 # Remove faces that contain a non-sulcus vertex
-                print('Remove faces that contain a non-sulcus vertex...')
                 fs = frozenset(sulcus)
                 faces_sulcus = [lst for lst in faces
                                 if len(fs.intersection(lst))==3]
@@ -126,15 +99,16 @@ def extract_fundi(vertices, faces, depths, mean_curvatures, min_directions,
                 # Replace mesh indices with sulcus indices
                 print('Replace mesh indices with sulcus indices')
                 V = np.unique(faces_sulcus)
+                anchors = np.array(anchors)
                 anchors2 = anchors.copy()
                 faces_sulcus2 = faces_sulcus.copy()
                 for index_new, index_old in enumerate(V):
                     anchors2[anchors == index_old] = index_new
                     faces_sulcus2[faces_sulcus == index_old] = index_new
-                print(faces_sulcus)
-                print(faces_sulcus2)
+
                 # Connect fundus points and extract fundus
-                print('Connect fundus points for sulcus ' + str(i_sulcus + 1) + '...')
+                print('Connect fundus points for sulcus ' +
+                      str(i_sulcus + 1) + '...')
                 fundi.append(
                       connect_anchors(anchors2, faces_sulcus2, L, thr))
             else:
@@ -154,10 +128,13 @@ if load_em:
     vertices = pickle.load(open(load_path + "vertices.p","rb"))
     faces = pickle.load(open(load_path + "faces.p","rb"))
 else:
+    from test import test_fundi_hmmf
     mean_curvatures, depths, vertices, faces, min_directions = test_fundi_hmmf()
     #mean_curvatures, depths, vertices, faces, min_directions, output_sulci, \
     #output_anchor_points, output_L, output_fundi = test_fundi_hmmf()
 
 #sulci, n_sulci = extract_sulci(faces, depths, depth_threshold=0.2, min_sulcus_size=50)
 
-fundi = extract_fundi(vertices, faces, depths, mean_curvatures, min_directions, depth_threshold=0.2)
+fundi = extract_fundi(vertices, faces, depths, mean_curvatures, min_directions,
+    depth_threshold=0.2, thr=0.5, min_sulcus_size=50,
+    min_distance=5, max_distance=8)
