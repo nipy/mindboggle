2ccc4c4aa4eea1a00ad355e6940735b793b9f22e
Arno Klein <arno@binarybottle.com>
2012-08-01 00:49:05 -0400

    Add evaluate_volume_labels.py

---
+++ mindboggle/label/evaluate_volume_labels.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python
+
+"""
+Compute volume label overlaps.
+
+Compute the Dice and Jaccard overlap measures for each labeled region
+of two labeled image volumes, for example one that has been manually labeled
+(atlas_file) and one that has been automatically labeled (input_file).
+Perform this computation for each label in a list.
+
+
+Author:  Arno Klein  .  arno@mindboggle.info  .  www.binarybottle.com
+
+(c) 2012  Mindbogglers (www.mindboggle.info), under Apache License Version 2.0
+
+"""
+
+
+def measure_volume_overlap(labels, atlas_file, input_file):
+    """
+    Measure overlap between individual label regions
+    in source and target nifti (nii.gz) images.
+
+    Input:
+    arg1:  list of label indices
+    arg2, arg3:  source, target images, consisting of index-labeled pixels/voxels
+
+    """
+
+    import numpy as np
+    import nibabel as nb
+
+    save_output = False
+
+    # Load labeled image volumes
+    input_data = nb.load(input_file).get_data().ravel()
+    atlas_data = nb.load(atlas_file).get_data().ravel()
+    #print(np.unique(input_data))
+    #print(np.unique(atlas_data))
+
+    # Initialize output
+    overlaps = np.zeros((len(labels), 3))
+    overlaps[:, 0] = labels
+
+    # Loop through labels
+    for ilabel, label in enumerate(labels):
+        label = int(label)
+
+        # Find which voxels contain the label in each volume
+        input_indices = np.where(input_data==label)[0]
+        atlas_indices = np.where(atlas_data==label)[0]
+        input_label_sum = len(input_indices)
+        atlas_label_sum = len(atlas_indices)
+
+        # Determine their intersection and union
+        intersect_label_sum = len(np.intersect1d(input_indices, atlas_indices))
+        union_label_sum = len(np.union1d(input_indices, atlas_indices))
+        #print('{} {} {} {} {}'.format(label, input_label_sum, atlas_label_sum,
+        #                              intersect_label_sum, union_label_sum))
+
+        # There must be at least one voxel with the label in each volume
+        if input_label_sum * atlas_label_sum > 0:
+
+            # Compute Dice and Jaccard coefficients
+            dice = 2.0 * intersect_label_sum / (input_label_sum + atlas_label_sum)
+            jacc = np.float(intersect_label_sum) / union_label_sum
+            overlaps[ilabel, 1:3] = [dice, jacc]
+            #print('label: {}, dice: {:.2f}, jacc: {:.2f}'.format(label, dice, jacc))
+
+        # NOTE:  untested:
+        if save_output:
+            out_file = os.path.join(os.getcwd(),
+                'label_volume_overlaps_' + input_file + '_vs_' + atlas_file + '.csv')
+            np.savetxt(out_file, overlaps, fmt='%.4f', delimiter='    ', newline='\n')
+
+    return overlaps
