8ec5330589c6ed0d49b7a3ad1445f37cc43f59d1
Joachim Giard <giard@pc-giard.(none)>
2012-08-07 10:32:22 +0200

    added a new class with a clean implementation of travel depth. Work in progress
    Former-commit-id: 5b8d2526f21487ca97ae5d51da32cc44c4f1429a

--- measure/surface_measures/travel_depth/TravelDepthMain.cpp
+++ measure/surface_measures/travel_depth/TravelDepthMain.cpp
@@ -1,3 +1,4 @@
+#include "../TravelDepth.h"
 #include "../MeshAnalyser.h"
 
 #include <iostream>
@@ -8,9 +9,14 @@ int main(int argc, char** argv)
 {
     time_t start= time(NULL);
 
-    MeshAnalyser* ma = new MeshAnalyser(argv[1]);
-    ma->ComputeTravelDepth(false);
-    ma->WriteIntoFile(argv[2], (char*)"depth");
+//    TravelDepth* depthComputer = new TravelDepth(argv[1]);
+//    depthComputer->ComputeDepth();
+//    depthComputer->WriteIntoFile(argv[2]);
+
+    MeshAnalyser* depthComputer = new MeshAnalyser(argv[1]);
+    depthComputer->ComputeTravelDepth(true);
+    depthComputer->WriteIntoFile(argv[2],(char*)"depth");
+
 
 
     cout<<"Elapsed time (meshTest): "<<time(NULL)-start<<" s"<<endl;
