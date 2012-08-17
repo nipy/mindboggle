550a7a23a5e37549e9b86f41deed890457becda8
Joachim Giard <giard@pc-giard.(none)>
2012-08-17 17:37:30 +0200

    added a surface overlap measure class.
    Former-commit-id: c696641a63f1a26a49011a792de8a7228e0a69d3

---
+++ mindboggle/measure/surface_measures/Overlap.h
@@ -0,0 +1,50 @@
+/* ********************************************************************
+ * Overlap
+ *
+ * Copyright 2012 Joachim Giard
+ * Université catholique de Louvain, Belgium
+ * Apache License, Version 2.0
+ *
+ * Class containing various methods to compute overlaps
+ * between pairs of labels
+ *
+ * *******************************************************************/
+
+#ifndef OVERLAP_H
+#define OVERLAP_H
+
+#include "vtkPolyData.h"
+
+class Overlap
+{
+public:
+    // Constructor. mesh1 and mesh2 must be the same mesh
+    // with a non-empty scalar field.
+    Overlap(vtkPolyData* mesh1, vtkPolyData* mesh2);
+    // destructor
+    ~Overlap();
+
+    //Compute Dice and Jaccard overlap score
+    void ComputeOverlap();
+
+    vtkDoubleArray* GetJaccard();
+    vtkDoubleArray* GetDice();
+
+private:
+    void ComputePointsArea();
+
+    vtkDoubleArray* m_pointsArea;
+    vtkDoubleArray* m_labels1Areas;
+    vtkDoubleArray* m_labels2Areas;
+    vtkDoubleArray* m_labelsCommonAreas;
+
+    vtkDoubleArray* m_jaccard;
+    vtkDoubleArray* m_dice;
+
+    vtkPolyData* m_mesh1;
+    vtkPolyData* m_mesh2;
+
+
+};
+
+#endif // OVERLAP_H
