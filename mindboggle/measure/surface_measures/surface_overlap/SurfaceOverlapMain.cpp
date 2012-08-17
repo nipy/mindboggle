2f20182285cea027562cd8914a64e3518eb1c505
Joachim Giard <giard@pc-giard.(none)>
2012-08-17 17:37:30 +0200

    added a surface overlap measure class.
    Former-commit-id: c696641a63f1a26a49011a792de8a7228e0a69d3

---
+++ mindboggle/measure/surface_measures/surface_overlap/SurfaceOverlapMain.cpp
@@ -0,0 +1,31 @@
+#include "../Overlap.h"
+
+#include <vtkPolyDataReader.h>
+
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+
+int main(int argc, char** argv)
+{
+    time_t start= time(NULL);
+
+    vtkPolyDataReader* reader1 = vtkPolyDataReader::New();
+    reader1->SetFileName(argv[1]);
+    reader1->Update();
+
+    vtkPolyDataReader* reader2 = vtkPolyDataReader::New();
+    reader2->SetFileName(argv[2]);
+    reader2->Update();
+
+    Overlap* surfOverlap = new Overlap(reader1->GetOutput(), reader2->GetOutput());
+    surfOverlap->ComputeOverlap();
+
+
+
+    cout<<"Elapsed time (meshTest): "<<time(NULL)-start<<" s"<<endl;
+    return 0;
+}
+
+
+
