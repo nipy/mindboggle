a975e8143b9dbf24c42be1cb7afb7ff3cdf92886
Arno Klein <arno@binarybottle.com>
2012-08-16 20:12:08 -0400

    Combine atlas labels (from 31 to 25 labels) -- new atlases and code to further reduce/combine labels.
    Former-commit-id: 94b963568c914193166bc77f2c7744fd027c4351

--- mindboggle/settings.py
+++ mindboggle/settings.py
@@ -16,21 +16,21 @@ import os, sys
 subjects = ['HLN-12-3'] #['MMRR-21-1']
 results_path = '/projects/Mindboggle/results'  # Where to save output
 
-
 #=============================================================================
 # System settings
 #=============================================================================
 #-----------------------------------------------------------------------------
 # Debugging options
 #-----------------------------------------------------------------------------
-do_combine_atlas_labels = False  # Combine atlas labels
 do_load_vtk_surfaces = False  # Load VTK surfaces (not FreeSurfer surfaces)
-do_save_folds = True  # Save folds as VTK file
+do_combine_atlas_labels = False  # Combine atlas labels
+do_save_folds = 0 #True  # Save folds as VTK file
 do_save_likelihoods = False  # Save likelihood values as VTK file
-do_save_fundi = True  # Save fundi as VTK file
-do_init_fs_labels = 1 #False  # Initialize with a FreeSurfer classifier atlas
-do_fill_volume_labels = True  # Fill (gray matter) volumes with surface labels
-do_evaluate_labels = 1 #False  # Compute volume overlap of auto vs. manual labels
+do_save_fundi = 0 #True  # Save fundi as VTK file
+do_init_fs_labels = False  # Initialize with a FreeSurfer classifier atlas
+do_fill_volume_labels = 0 #True  # Fill (gray matter) volumes with surface labels
+do_evaluate_surface_labels = 0 #False  # Compute surface overlap of auto vs. manual labels
+do_evaluate_volume_labels = 0 #1 #False  # Compute volume overlap of auto vs. manual labels
 do_generate_graphs = True
 #-----------------------------------------------------------------------------
 # Paths
@@ -42,8 +42,8 @@ temp_path = os.path.join(results_path, 'workingdir')  # Where to save temp files
 info_path = os.path.join(code_path, 'info')
 templates_path = os.path.join(base_path, 'data', 'templates')
 atlases_path = subjects_path
-label_string = 'labels.DKT31'
-#label_string = 'labels.DKT25'
+label_string_old = 'labels.DKT31'
+label_string = 'labels.DKT25'
 hemis = ['lh','rh']
 
 # Add to PYTHONPATH
@@ -57,9 +57,9 @@ if do_init_fs_labels:
 else:
     label_type = 'labels.max'
 
-"""
+
 # Load atlas list as subjects
 import utils.io_file as iof
-atlas_list_file = os.path.join(atlases_path, 'atlases101.txt')
+atlas_list_file = os.path.join('info/atlases101.txt')
 subjects = iof.read_list_strings(atlas_list_file)
-"""
+
