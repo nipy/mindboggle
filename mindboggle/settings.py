531f26fc843975fef6c60c9cb18f4156a5969a9f
binarybottle <arno@binarybottle.com>
2012-08-18 00:33:52 -0400

    Major renaming and workflow + output - related tidying.  Add surface evaluation function to call Joachim's new surface overlap code.

--- mindboggle/settings.py
+++ mindboggle/settings.py
@@ -13,49 +13,54 @@ import os, sys
 #=============================================================================
 # User settings
 #=============================================================================
-subjects = ['HLN-12-3'] #['MMRR-21-1']
-results_path = '/projects/Mindboggle/results'  # Where to save output
+subjects = ['HLN-12-3']
+output_path = '/projects/Mindboggle/output'  # Where to save output
 
 #=============================================================================
 # System settings
 #=============================================================================
 #-----------------------------------------------------------------------------
-# Debugging options
+# Labeling protocol used by Mindboggle:
+# 'DKT31': 'Desikan-Killiany-Tourville (DKT) protocol with 31 labeled regions
+# 'DKT25': 'fundus-friendly' version of the DKT protocol following fundi
 #-----------------------------------------------------------------------------
-do_load_vtk_surfaces = False  # Load VTK surfaces (not FreeSurfer surfaces)
-do_save_folds = True  # Save folds as VTK file
-do_save_likelihoods = False  # Save likelihood values as VTK file
-do_save_fundi = True  # Save fundi as VTK file
-do_init_fs_labels = 1 #False  # Initialize with a FreeSurfer classifier atlas
-do_fill_volume_labels = True  # Fill (gray matter) volumes with surface labels
-do_evaluate_surface_labels = 0 #False  # Compute surface overlap of auto vs. manual labels
-do_evaluate_volume_labels = 1 #False  # Compute volume overlap of auto vs. manual labels
+protocol = 'DKT31'
+#-----------------------------------------------------------------------------
+# Initialize labels with:
+# 'free': the standard FreeSurfer classifier atlas trained on the DK protocol
+# <FUTURE: 'freeDKT31': a FreeSurfer-style classifier atlas trained on the DKT protocol>
+# 'max': maximum probability (majority vote) labels from multiple atlases
+#-----------------------------------------------------------------------------
+init_labels = 'free'
+#-----------------------------------------------------------------------------
+# Labeling source:
+# 'manual': manual edits
+# <FUTURE: 'adjusted': manual edits that have been automatically aligned with major fundi>
+#-----------------------------------------------------------------------------
+label_method = 'manual'
 #-----------------------------------------------------------------------------
 # Paths
 #-----------------------------------------------------------------------------
 subjects_path = os.environ['SUBJECTS_DIR']  # FreeSurfer subjects directory
 base_path = os.environ['MINDBOGGLE_HOME']  # Mindboggle home directory
 code_path = os.environ['MINDBOGGLE_CODE']  # Mindboggle code directory
-temp_path = os.path.join(results_path, 'workingdir')  # Where to save temp files
+temp_path = os.path.join(output_path, 'workspace')  # Where to save temp files
 info_path = os.path.join(code_path, 'info')
 templates_path = os.path.join(base_path, 'data', 'templates')
 atlases_path = subjects_path
-#label_string_old = 'labels.DKT31'
-#label_string = 'labels.DKT25'
-label_string = 'labels.DKT31'
-hemis = ['lh','rh']
+hemis = ['lh','rh']  # Prepend ('lh.') indicating left and right surfaces
+sys.path.append(code_path)  # Add to PYTHONPATH
+#-----------------------------------------------------------------------------
+# Debugging options
+#-----------------------------------------------------------------------------
+input_vtk_surfaces = False  # Load VTK surfaces (not FreeSurfer surfaces)
+fill_volume_labels = True  # Fill (gray matter) volumes with surface labels
+evaluate_surface_labels = 0 #False  # Compute surface overlap of auto vs. manual labels
+evaluate_volume_labels = 1 #False  # Compute volume overlap of auto vs. manual labels
 
-# Add to PYTHONPATH
-sys.path.append(code_path)
+load_all_atlases = False  # Load atlas list as subjects
+if load_all_atlases:
+    import utils.io_file as iof
+    atlas_list_file = os.path.join('info/atlases101.txt')
+    subjects = iof.read_list_strings(atlas_list_file)
 
-if do_init_fs_labels:
-    label_type = 'labels.fs'
-else:
-    label_type = 'labels.max'
-
-"""
-# Load atlas list as subjects
-import utils.io_file as iof
-atlas_list_file = os.path.join('info/atlases101.txt')
-subjects = iof.read_list_strings(atlas_list_file)
-"""
