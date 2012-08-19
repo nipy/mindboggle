bc393a91f22007c4b7b4bef52772d021e1ce9bfb
Arno Klein <arno@binarybottle.com>
2012-08-19 17:55:19 -0400

    Fix BUG: FreeSurfer's mris_label2annot requires the list of label arguments to be in the same order as the list of labels in the colortable, whereas Nipype's MapNode does not preserve order, so change write_label_files and labels_to_annot_file to regular Nodes and save a list of ordered labels.
    Former-commit-id: ff925715aa6c889710b76a5fa318f8af9104c85f

--- mindboggle/utils/plot_label.py
+++ mindboggle/utils/plot_label.py
@@ -13,8 +13,9 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 if len(sys.argv) < 2:
-    sys.exit('Usage: %s label-file-name' %sys.argv[0])
-    #label_file = '/projects/mindboggle/results/workingdir/Mindboggle_workflow/Atlas_workflow/_hemi_lh_subject_MMRR-21-1/Write_label_files/mapflow/_Write_label_files30/lh.insula.label'
+    #sys.exit('Usage: %s label-file-name' %sys.argv[0])
+    label_file = '/projects/Mindboggle/output/workspace/Mindboggle/Fill_volume_prep/_hemi_lh_subject_HLN-12-3/Write_label_files/mapflow/_Write_label_files6/lh.inferiortemporal.label'
+    #label_file = '/projects/Mindboggle/output/workspace/Mindboggle/Fill_volume_prep/_hemi_lh_subject_HLN-12-3/Write_label_files/mapflow/_Write_label_files30/lh.insula.label'
 else:
     label_file = sys.argv[1]
 
@@ -28,8 +29,10 @@ if a.ndim == 2:
 
     fig = plt.figure()
     ax = fig.add_subplot(111) #, projection='3d')
-    ax.plot(x,y,z, 'b.')
+    ax.plot(x,y,z, 'k.')
     plt.show()
 
 else:
-    sys.exit('The label array is the wrong shape. Remember to comment out the top line (below the header) of the .label file containing the number of rows.')
+    sys.exit("The label array is the wrong shape." +\
+    "Remember to comment out the top line (below the header) of the .label file" +\
+    "containing the number of rows.")
