61c7b769eb7eec4102fc89438a6a91ea768f4b9a
Arno Klein <arno@binarybottle.com>
2012-08-20 12:14:43 -0400

    Fix connection so that new, relabeled volume is saved.
    Former-commit-id: e834609b9e422fb86709a71fc96d6aeac9df3ab5

---
+++ mindboggle/utils/debug_nipype.py
@@ -0,0 +1,5 @@
+#from nipype import config, logging
+#config.set('logging', 'interface_level', 'DEBUG')
+#config.set('logging', 'workflow_level', 'DEBUG')
+#logging.update_logging(config)
+#config.enable_debug_mode()
