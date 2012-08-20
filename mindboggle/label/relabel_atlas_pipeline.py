b76a3da697ac89abe13b92d15c867968ef1ee4a4
Arno Klein <arno@binarybottle.com>
2012-08-20 02:45:27 -0400

    Correction with regard to the previous commit's comment: the label indices don't matter for conversion from .label to .annot file if viewing them in tksurfer, but the indices do appear to matter when propagating the labels through the gray matter mask, so for the label volume-filling step let the incorrect colortable labels propagate through the gray matter mask, then correct the labels in the volume with a new function in the relabel.py file.  Reorganize all of the io_* files so that they are in three clear categories: io_file.py for files, io_free.py for freesurfer i/o functions, and io_vtk.py for all conversions to or from vtk.
    Former-commit-id: aca60b1fa027bf00d5e53ab5ce2ac0f5bf47bff5

--- mindboggle/label/relabel_atlas_pipeline.py
+++ mindboggle/label/relabel_atlas_pipeline.py
@@ -2,12 +2,9 @@
 """
 This is a Nipype pipeline for combining brain surface atlas labels.
 
-It will convert surfaces from VTK to annot format,
-combine the surface labels to create a new label set,
-and convert the labels back to VTK format.
+It will combine the surface labels in .annot format,
+and convert the labels to VTK format.
 
-NOTE:
-Alternative method to convert one .annot file to another with combined labels:
 - https://mail.nmr.mgh.harvard.edu/pipermail//freesurfer/2010-June/014620.html
 - mris_translate_annotation <subject> <hemi> <in annot> <translation file> <out annot>
 - <translation file>: text file that lists the labels (one per line)
@@ -16,14 +13,6 @@ Alternative method to convert one .annot file to another with combined labels:
   221     220     60      223     220     60
   221     220     160     223     220     60
   221     220     100     223     220     60
-
-
-For more information about Mindboggle,
-see the website: http://www.mindboggle.info
-and read the README.
-
-For information on Nipype: http://www.nipy.org/nipype/
-http://www.ncbi.nlm.nih.gov/pmc/articles/PMC3159964/
 
 
 Authors:  Arno Klein  .  arno@mindboggle.info  .  www.binarybottle.com
@@ -41,33 +30,30 @@ import os, sys
 # Steps to run
 #-----------------------------------------------------------------------------
 do_load_vtk_surfaces = False
-do_convert_surface = 1
-do_convert_atlas_annot = 1
 do_combine_atlas_labels = 1
-do_writelabels = 1
-do_writeannot = 1
+do_convert_atlas_annot = 1
 copy_to_fs_atlases = 1
 copy_to_mb_atlases = 1
 #-----------------------------------------------------------------------------
 # From settings.py
 #-----------------------------------------------------------------------------
-results_path = '/projects/Mindboggle/results'  # Where to save output
+output_path = '/projects/Mindboggle/output'  # Where to save output
 subjects_path = os.environ['SUBJECTS_DIR']  # FreeSurfer subjects directory
 base_path = os.environ['MINDBOGGLE_HOME']  # Mindboggle home directory
 code_path = os.environ['MINDBOGGLE_CODE']  # Mindboggle code directory
-temp_path = os.path.join(results_path, 'workingdir')  # Where to save temp files
+temp_path = os.path.join(output_path, 'workingdir')  # Where to save temp files
 info_path = os.path.join(code_path, 'info')
-atlases_path = subjects_path
 sys.path.append(code_path) # Add to PYTHONPATH
-label_string_old = 'labels.DKT31'
-label_string = 'labels.DKT25'
+label_string_old = 'labels.DKT31.manual'
+label_string = 'labels.DKT25.manual'
+relabel_file = os.path.join(info_path, 'labels.surface.DKT31to25.txt')
 hemis = ['lh','rh']
 #-----------------------------------------------------------------------------
 # Subjects to process
 #-----------------------------------------------------------------------------
 import utils.io_file as iof
 atlas_list_file = os.path.join(info_path, 'atlases101.txt')
-subjects = iof.read_list_strings(atlas_list_file)
+subjects = iof.read_columns(atlas_list_file, 1)[0]
 #subjects = ['MMRR-3T7T-2-1','MMRR-3T7T-2-2']
 #-----------------------------------------------------------------------------
 # Import system and nipype Python libraries
@@ -80,11 +66,8 @@ from nipype.interfaces.io import DataGrabber, DataSink
 #-----------------------------------------------------------------------------
 # Import Mindboggle Python libraries
 #-----------------------------------------------------------------------------
-from utils.io_vtk import surf_to_vtk, annot_to_vtk
-from utils.io_file import read_list_2strings
-from label.relabel_surface import relabel_surface
-from label.surface_labels_to_volume import write_label_file,\
-     label_to_annot_file
+from utils.io_vtk import annot_to_vtk
+from label.relabel import relabel_annot_file
 #-----------------------------------------------------------------------------
 # Initialize main workflow
 #-----------------------------------------------------------------------------
@@ -109,49 +92,45 @@ surf = Node(name = 'Surfaces',
             interface = DataGrabber(infields=['subject', 'hemi'],
                                     outfields=['surface_files']))
 surf.inputs.base_directory = subjects_path
-surf.inputs.template = '%s/surf/%s.%s'
+surf.inputs.template = os.path.join('%s', 'surf', '%s.%s')
 surf.inputs.template_args['surface_files'] = [['subject', 'hemi', 'pial']]
 flow.connect([(info, surf, [('subject','subject'), ('hemi','hemi')])])
 #-----------------------------------------------------------------------------
 # Outputs
 #-----------------------------------------------------------------------------
 datasink = Node(DataSink(), name = 'Results')
-datasink.inputs.base_directory = results_path
-datasink.inputs.container = 'output'
-if not os.path.isdir(results_path):  os.makedirs(results_path)
-
-#-----------------------------------------------------------------------------
-# Convert surface meshes to VTK
-#-----------------------------------------------------------------------------
-if do_convert_surface:
-    if not do_load_vtk_surfaces:
-        convertsurf = Node(name = 'Convert_surface',
-                           interface = Fn(function = surf_to_vtk,
-                                          input_names = ['surface_file'],
-                                          output_names = ['vtk_file']))
-        flow.connect([(surf, convertsurf, [('surface_files','surface_file')])])
+datasink.inputs.base_directory = output_path
+datasink.inputs.container = 'results'
+if not os.path.isdir(output_path):  os.makedirs(output_path)
 
 #=============================================================================
-#   Convert labels from annot to VTK, combine labels, and back to VTK
+#   Combine .annot labels and convert to VTK
 #=============================================================================
 atlasflow = Workflow(name='Atlas_workflow')
 atlasflow.base_dir = temp_path
 
+#-------------------------------------------------------------------------
+#   Combine atlas .annot labels
+#-------------------------------------------------------------------------
+if do_combine_atlas_labels:
+    combine_labels = Node(name='Combine_atlas_labels',
+                          interface = Fn(function = relabel_annot_file,
+                                         input_names = ['hemi',
+                                                        'subject',
+                                                        'annot_name',
+                                                        'new_annot_name',
+                                                        'relabel_file'],
+                                         output_names = ['new_annot_name']))
+    combine_labels.inputs.annot_name = label_string_old'
+    combine_labels.inputs.new_annot_name = label_string
+    combine_labels.inputs.relabel_file = relabel_file
+    flow.connect([(info, atlasflow, [('hemi','Combine_atlas_labels.hemi'),
+                                     ('subject','Combine_atlas_labels.subject')])])
+
 #-----------------------------------------------------------------------------
-#   Convert annot labels to VTK format
+#   Convert .annot labels to VTK format
 #-----------------------------------------------------------------------------
 if do_convert_atlas_annot:
-    # Input annot
-    atlas_annot = Node(name = 'Atlas_annot',
-                       interface = DataGrabber(infields=['subject','hemi'],
-                                               outfields=['atlas_annot_file']))
-    atlas_annot.inputs.base_directory = atlases_path
-    atlas_annot.inputs.template = '%s/label/%s.' + label_string_old + '.manual.annot'
-    atlas_annot.inputs.template_args['atlas_annot_file'] = [['subject','hemi']]
-    atlasflow.add_nodes([atlas_annot])
-    flow.connect([(info, atlasflow, [('subject','Atlas_annot.subject'),
-                                        ('hemi','Atlas_annot.hemi')])])
-    # Convert .annot to vtk function
     atlas_vtk = Node(name = 'Convert_atlas_labels',
                      interface = Fn(function = annot_to_vtk,
                                     input_names = ['surface_file',
@@ -160,12 +139,13 @@ if do_convert_atlas_annot:
                                                    'subjects_path',
                                                    'annot_name'],
                                     output_names = ['vtk_file']))
-    atlas_vtk.inputs.annot_name = label_string_old + '.manual.annot'
-    atlas_vtk.inputs.subjects_path = subjects_path
     atlasflow.add_nodes([atlas_vtk])
     flow.connect([(info, atlasflow,
                         [('hemi','Convert_atlas_labels.hemi'),
                          ('subject','Convert_atlas_labels.subject')])])
+    atlas_vtk.inputs.subjects_path = subjects_path
+    atlasflow.connect([(combine_labels, atlas_vtk,
+                     [('new_annot_name','annot_name')])])
     if do_load_vtk_surfaces:
         flow.connect([('surf', 'Convert_atlas_labels.atlas_vtk',
                        [('surface_files','Convert_atlas_labels.surface_file')])])
@@ -175,104 +155,9 @@ if do_convert_atlas_annot:
     flow.connect([(atlasflow, datasink,
                      [('Convert_atlas_labels.vtk_file','atlas_labels')])])
 
-#-------------------------------------------------------------------------
-#   Combine atlas labels
-#-------------------------------------------------------------------------
-if do_combine_atlas_labels:
-
-    # After converting .annot to vtk files, define the atlas_old node
-    atlas_old = Node(name = 'Atlas_old',
-                     interface = DataGrabber(infields=['subject','hemi'],
-                                             outfields=['atlas_old_file']))
-    atlas_old.inputs.base_directory = atlases_path
-    atlas_old.inputs.template = '%s/label/%s.' + label_string_old + '.manual.vtk'
-    atlas_old.inputs.template_args['atlas_old_file'] = [['subject','hemi']]
-    atlasflow.add_nodes([atlas_old])
-    flow.connect([(info, atlasflow, [('subject','Atlas_old.subject'),
-                                        ('hemi','Atlas_old.hemi')])])
-    # Combine labels
-    combine_atlas = Node(name='Combine_atlas_labels',
-                         interface = Fn(function = relabel_surface,
-                                        input_names = ['vtk_file',
-                                                       'relabel_list',
-                                                       'new_string'],
-                                        output_names = ['relabeled_vtk']))
-    combine_atlas.inputs.new_string = label_string + '.manual.vtk'
-    combine_atlas.inputs.relabel_list = os.path.join(info_path,
-                                                     'labels.DKT31to25.txt')
-    atlasflow.connect([(atlas_old, combine_atlas,
-                        [('atlas_old_file', 'vtk_file')])])
-    flow.connect([(atlasflow, datasink,
-                        [('Combine_atlas_labels.relabeled_vtk',
-                          'combine_atlas_labels')])])
-
-# After combining labels, define the atlas node
-atlas = Node(name = 'Atlas',
-             interface = DataGrabber(infields=['subject','hemi'],
-                                     outfields=['atlas_file']))
-atlas.inputs.base_directory = atlases_path
-
-atlas.inputs.template = '%s/label/%s.' + label_string + '.manual.vtk'
-atlas.inputs.template_args['atlas_file'] = [['subject','hemi']]
-atlasflow.add_nodes([atlas])
-
-flow.connect([(info, atlasflow, [('subject','Atlas.subject'),
-                                    ('hemi','Atlas.hemi')])])
-
-#-------------------------------------------------------------------------
-# Write .label files for surface vertices
-#-------------------------------------------------------------------------
-if do_writelabels:
-    writelabels = MapNode(name='Write_label_files',
-                          iterfield = ['label_number', 'label_name'],
-                          interface = Fn(function = write_label_file,
-                                         input_names = ['hemi',
-                                                        'surface_file',
-                                                        'label_number',
-                                                        'label_name',
-                                                        'scalar_name'],
-                                         output_names = ['label_file']))
-
-    ctx_labels_file = os.path.join(info_path, label_string + '.txt')
-    ctx_label_numbers, ctx_label_names = read_list_2strings(ctx_labels_file)
-
-    writelabels.inputs.label_number = ctx_label_numbers
-    writelabels.inputs.label_name = ctx_label_names
-    atlasflow.add_nodes([writelabels])
-    writelabels.inputs.scalar_name = 'Labels'
-    flow.connect([(info, atlasflow, [('hemi', 'Write_label_files.hemi')])])
-    atlasflow.connect([(atlas, writelabels, [('atlas_file','surface_file')])])
-
-#-------------------------------------------------------------------------
-# Write .annot file from .label files
-#-------------------------------------------------------------------------
-if do_writeannot:
-    writeannot = MapNode(name='Write_annot_file',
-                         iterfield = ['hemi'],
-                         interface = Fn(function = label_to_annot_file,
-                                        input_names = ['hemi',
-                                                       'subjects_path',
-                                                       'subject',
-                                                       'label_files',
-                                                       'colortable',
-                                                       'annot_name'],
-                                        output_names = ['annot_name',
-                                                        'annot_file']))
-    writeannot.inputs.annot_name = label_string + '.manual'
-    writeannot.inputs.subjects_path = subjects_path
-    writeannot.inputs.colortable = os.path.join(info_path, label_string + '.txt')
-    atlasflow.add_nodes([writeannot])
-    flow.connect([(info, atlasflow,
-                        [('hemi', 'Write_annot_file.hemi'),
-                         ('subject', 'Write_annot_file.subject')])])
-    atlasflow.connect([(writelabels, writeannot,
-                        [('label_file','label_files')])])
-    flow.connect([(writeannot, datasink,
-                      [('annot_file','Write_annot_file.annot')])])
-
 ##############################################################################
-#if __name__== '__main__':
-#    flow.run()
+if __name__== '__main__':
+    flow.run()
 
 #-------------------------------------------------------------------------
 # Copy results to atlas label directories
@@ -284,21 +169,18 @@ if copy_to_fs_atlases or copy_to_mb_atlases:
 
             srcs = []
 
-            if do_convert_atlas_annot:
-                src = os.path.join(results_path, 'output', 'atlas_labels',
-                                   '_hemi_' + h + '_subject_' + s,
-                                   h + '.' + label_string_old + '.manual.vtk')
-                srcs.append(src)
-
             if do_combine_atlas_labels:
-                src = os.path.join(results_path, 'output', 'combine_atlas_labels',
+                src = os.path.join(output_path, datasink.inputs.container,
+                                   'combine_labels',
                                    '_hemi_' + h + '_subject_' + s,
                                    h + '.' + label_string + '.manual.vtk')
                 srcs.append(src)
 
-            if do_writeannot:
-                src = os.path.join(atlases_path, s, 'label',
-                                   h + '.' + label_string + '.manual.annot')
+            if do_convert_atlas_annot:
+                src = os.path.join(output_path, datasink.inputs.container,
+                                   'atlas_vtk',
+                                   '_hemi_' + h + '_subject_' + s,
+                                   h + '.' + label_string + '.manual.vtk')
                 srcs.append(src)
 
             for src in srcs:
--- mindboggle/utils/io_file.py
+++ mindboggle/utils/io_file.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python
 """
 This Python library reads and writes different file types.
-In particular, it has functions to read some FreeSurfer files,
-including surface, curvature, and convexity files.
-The function read_surface reads in surface files,
-while the function read_curvature reads in both
-curvature (.curv) and convexity (.sulc) files.
 
 Authors:
 Forrest Sheng Bao  .  http://fsbao.net
@@ -14,137 +9,6 @@ Arno Klein  .  arno@mindboggle.info  .  www.binarybottle.com
 (c) 2012  Mindbogglers (www.mindboggle.info), under Apache License Version 2.0
 
 """
-
-def read_surface(filename):
-    """
-    Read in a FreeSurfer triangle surface mesh in binary format.
-
-    Parameters
-    ===========
-    filename : string
-        A binary FreeSurfer Triangle Surface file
-
-    Outputs
-    =======
-    Vertex : list of 3-tuples of floats
-        Each element is a 3-tuple (list) of floats, which are the X, Y and Z coordinates of a vertex, respectively.
-        A 3-tuple's index in the list *Vertex* is the ID of a vertex.
-
-    Face : list of 3-tuples of integers
-        Each element is a 3-tuple (list) of integers, which are the IDs of 3 vertexes that form one face
-
-    Example
-    ========
-
-    >>> import readFreeSurfer as rfs
-    >>> Vrtx, Face = rfs.read_surface('lh.pial')
-    >>> len(Vrtx)
-    130412
-    >>> len(Face)
-    260820
-    >>> Vrtx[10]
-    [-7.902474880218506, -95.6839370727539, -21.856534957885742]
-    >>> Face[10]
-    [2, 39, 3]
-
-    """
-
-    import os
-    import struct
-
-    f = open(filename, "rb")
-    f.seek(3)  # skip the first 3 Bytes "Magic" number
-
-    s = f.read(50)   # the second field is a string of variable length
-    End2 = s.find('\n\n',0)  # end of the second field is a '\n\n'
-
-    f.seek(3+End2+2)  # jump to immediate Byte after the creating information
-
-    s = f.read(8)
-    VertexCount, FaceCount = struct.unpack(">ii", s)
-    # print("This hemisphere has", VertexCount, "Vertexes and", FaceCount, "Faces")
-
-    Vertex, Face = [], []
-
-    for i in xrange(0, VertexCount):
-        s = f.read(8)
-        R, A = struct.unpack(">ff", s)
-        f.seek(-4, os.SEEK_CUR)
-        s = f.read(8)
-        A, S = struct.unpack(">ff", s)
-        Vertex.append([R,A,S]) # R, A, S are the coordinates of vertexes
-
-    for i in xrange(0, FaceCount):
-        s = f.read(8)
-        V0, V1 = struct.unpack(">ii", s)
-        f.seek(-4, os.SEEK_CUR)
-        s = f.read(8)
-        V1, V2 = struct.unpack(">ii", s)
-        Face.append([V0, V1, V2])
-
-    return Vertex, Face
-
-def read_curvature(filename):
-    """
-    Read in a FreeSurfer curvature (per-vertex) file.
-
-    Parameters
-    ==========
-
-    filename : string
-        A binary FreeSurfer curvature (pre-vertex) file
-
-    Outputs
-    ========
-
-    Curvature : list of floats
-        Each element is the curvature value of a FreeSurfer mesh vertex.
-        Elements are ordered by orders of vertexes in FreeSurfer surface file.
-
-    Example
-    ========
-
-    >>> import readFreeSurfer as rfs
-    >>> Curv = rfs.read_curvature('lh.curv')
-    >>> len(Curv)
-    130412
-    >>> Curv[10]
-    -0.37290969491004944
-
-    """
-
-    import struct
-
-    f = open(filename, "rb")
-
-    f.seek(3) # skip the first 3 Bytes "Magic" number
-
-    s = f.read(8)  # get the VertexCount and FaceCount
-    VertexCount, FaceCount = struct.unpack(">ii", s)
-    # print("# of Vertexes:", VertexCount, ", # of Faces:", FaceCount)
-
-    Curvature = [0.0]
-
-    s = f.read(8)
-    ValsPerVertex, Curvature[0] = struct.unpack(">if", s)
-
-    VertexCount -= 1  # because the first curvature value has been loaded
-
-    while VertexCount > 1:
-        s = f.read(8)
-        VertexVal1, VertexVal2  =  struct.unpack(">ff", s)
-        Curvature += [VertexVal1, VertexVal2]
-        VertexCount -= 2
-
-    if VertexCount != 0:  # number of vertexes is even (NOT ODD!!!)
-        f.seek(-4, os.SEEK_CUR)  # backward 4 Bytes from current position
-        s = f.read(8)
-        VertexVal1, VertexVal2 = struct.unpack(">ff", s)
-        Curvature.append(VertexVal2)
-
-    f.close()
-
-    return Curvature
 
 def write_list(filename, List):
     """
@@ -242,62 +106,31 @@ def write_lists(filename, input_lists):
         Fp.write('----\n')
     Fp.close()
 
-def read_list_strings(filename):
+def read_columns(filename, n_columns):
     """
-    Read a list file.
-    """
+    Read n-column text file.
 
-    import re
-
-    Fp = open(filename, 'r')
-    lines = Fp.readlines()
-    List = []
-    for line in lines:
-        if len(re.findall(r'\S+', line)):
-            List.append(re.findall(r'\S+', line)[0])
-    Fp.close()
-
-    return List
-
-def read_list_2strings(filename):
-    """
-    Read a 2-column list file.
+    Output a list of lists, one per column.
     """
 
     import re
 
     Fp = open(filename, 'r')
     lines = Fp.readlines()
-    column1 = []
-    column2 = []
+    columns = [[] for x in range(n_columns)]
     for line in lines:
-        if len(re.findall(r'\S+', line)):
-            column1.append(re.findall(r'\S+', line)[0])
-            column2.append(re.findall(r'\S+', line)[1])
+        if len(line) > 0:
+            row = re.findall(r'\S+', line)
+            if len(row) >= n_columns:
+                for icolumn in range(n_columns):
+                    columns[icolumn].append(row[icolumn])
+            else:
+                import os
+                os.error('The number of columns in {} is less than {}.'.format(
+                         filename, n_columns))
     Fp.close()
 
-    return column1, column2
-
-def read_list_3strings(filename):
-    """
-    Read a 3-column list file.
-    """
-
-    import re
-
-    Fp = open(filename, 'r')
-    lines = Fp.readlines()
-    column1 = []
-    column2 = []
-    column3 = []
-    for line in lines:
-        if len(re.findall(r'\S+', line)):
-            column1.append(re.findall(r'\S+', line)[0])
-            column2.append(re.findall(r'\S+', line)[1])
-            column3.append(' '.join(re.findall(r'\S+', line)[2::]))
-    Fp.close()
-
-    return column1, column2, column3
+    return columns
 
 def read_lists(filename):
     """The reverse function of write_lists
