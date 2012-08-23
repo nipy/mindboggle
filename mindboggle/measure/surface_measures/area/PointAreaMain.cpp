e8f435a398718141c31aeb4d86d7a6b94432cef7
Joachim Giard <giard@pc-giard.(none)>
2012-08-23 10:51:32 +0200

    Implemented the voronoi surface computer and an application to write area in the scalar field of vtk files.
    Former-commit-id: d65d1abfb2be5497039308274e6c7330b4749729

---
+++ mindboggle/measure/surface_measures/area/PointAreaMain.cpp
@@ -0,0 +1,33 @@
+#include "../PointAreaComputer.h"
+
+#include <vtkPolyDataReader.h>
+
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+
+void print_help()
+{
+    printf(
+    "Usage: PointAreaComputer InputVTKMesh\n"
+        );
+}
+
+int main(int argc, char** argv)
+{
+    time_t start= time(NULL);
+
+    vtkPolyDataReader* reader = vtkPolyDataReader::New();
+    reader->SetFileName(argv[1]);
+    reader->Update();
+
+    PointAreaComputer* area = new PointAreaComputer(reader->GetOutput());
+    area->ComputeArea();
+    area->WriteIntoFile(argv[2]);
+
+    cout<<"Elapsed time (area): "<<time(NULL)-start<<" s"<<endl;
+    return 0;
+}
+
+
+
