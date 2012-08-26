b075351e3cd41fd74e7a729af34b8dc3a9fb5034
binarybottle <arno@binarybottle.com>
2012-08-26 01:17:18 -0400

    Set up dist_utils setup.py, __init__.py, info.py, pkg_info.py, COMMIT_INFO.txt, and other files for package distribution.  Even the info/ text files are included in the installation.
    Former-commit-id: 3a37e69d8d5a105bc1d710100f2a0c903e6ea111

--- setup.py
+++ setup.py
@@ -2,131 +2,106 @@
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
 """
-Mindboggle : package for automated human brain image labeling and morphometry.
+Mindboggle: package for automated human brain image labeling and morphometry.
 
+<test>
 export PYTHONPATH=$PYTHONPATH:/desk/temp/lib/python2.7/site-packages/
 python setup.py install --prefix=/desk/temp
 python setup.py develop
 
 """
 
-from distribute_setup import use_setuptools
-use_setuptools()
-
-from setuptools import setup, find_packages
-
-setup(
-    name = "Mindboggle",
-    version = "0.01",
-    packages = find_packages(),
-    #scripts = ['mindboggle/pipeline.sh'],
-
-    # Project uses reStructuredText, so ensure that the docutils get
-    # installed or upgraded on the target machine
-    install_requires = ['docutils>=0.3'],
-
-    package_data = {
-        # If any package contains *.txt or *.rst files, include them:
-        '': ['*.txt', '*.rst'],
-    },
-
-    # metadata for upload to PyPI
-    author = "Arno Klein",
-    author_email = "arno@binarybottle.com",
-    description = "Automated human brain image automated anatomical labeling and shape analysis",
-    long_description = "Mindboggle is a package for automated anatomical labeling and morphometry of human brain images",
-    license = "Apache 2.0",
-    keywords = "Mindboggle human brain MRI automated labeling parcellation morphometry",
-    url = "http://mindboggle.info/",
-    download_url = "http://mindboggle.info/",
-)
-
-
-"""
+import os
 import sys
-from glob import glob
+from os.path import join as pjoin
 
-# Import build helpers
-try:
-    from nisext.sexts import package_check, get_comrec_build
-except ImportError:
-    raise RuntimeError('Need nisext package from nibabel installation'
-                       ' - please install nibabel first')
+# BEFORE importing distutils, remove MANIFEST. distutils doesn't properly
+# update it when the contents of directories change.
+if os.path.exists('MANIFEST'): os.remove('MANIFEST')
 
-from build_docs import cmdclass, INFO_VARS
-
-# Add custom commit-recording build command
-cmdclass['build_py'] = get_comrec_build('mindboggle')
-
-def configuration(parent_package='',top_path=None):
-    from numpy.distutils.misc_util import Configuration
-
-    config = Configuration(None, parent_package, top_path)
-    config.set_options(ignore_setup_xxx_py=True,
-                       assume_default_configuration=True,
-                       delegate_options_to_subpackages=True,
-                       quiet=True)
-    # The quiet=True option will silence all of the name setting warnings
-    config.get_version('mindboggle/__init__.py') # sets config.version
-    config.add_subpackage('mindboggle', 'mindboggle')
-    return config
-
-################################################################################
-# For some commands, use setuptools
-
+# For some commands, use setuptools.
 if len(set(('develop', 'bdist_egg', 'bdist_rpm', 'bdist', 'bdist_dumb',
             'bdist_wininst', 'install_egg_info', 'egg_info', 'easy_install',
             )).intersection(sys.argv)) > 0:
-    from setup_egg import extra_setuptools_args
+    # setup_egg imports setuptools setup, thus monkeypatching distutils.
+    import setup_egg
 
-# extra_setuptools_args can be defined from the line above, but it can
-# also be defined here because setup.py has been exec'd from
-# setup_egg.py.
-if not 'extra_setuptools_args' in globals():
-    extra_setuptools_args = dict()
+from distutils.core import setup
 
-# Hard and soft dependency checking
-package_check('nibabel', INFO_VARS['NIBABEL_MIN_VERSION'])
-package_check('numpy', INFO_VARS['NUMPY_MIN_VERSION'])
-package_check('nipype', INFO_VARS['NIPYPE_MIN_VERSION'])
+# Python 2 to 3 build
+from nisext.py3builder import build_py
+# Commit hash writing, and dependency checking
+from nisext.sexts import get_comrec_build, package_check
+cmdclass = {'build_py': get_comrec_build('mindboggle', build_py)}
 
-################################################################################
-# Import the documentation building classes.
+# Get version and release info, which is all stored in mindboggle/info.py
+ver_file = os.path.join('mindboggle', 'info.py')
+exec(open(ver_file).read())
 
-try:
-    from build_docs import cmdclass
-except ImportError:
-    "" Pass by the doc build gracefully if sphinx is not installed ""
-    print "Sphinx is not installed, docs cannot be built"
-    cmdclass = {}
-
-
-################################################################################
+# Do dependency checking
+package_check('numpy', NUMPY_MIN_VERSION)
+custom_pydicom_messages = {'missing opt': 'Missing optional package "%s"'
+        ' provided by package "pydicom"'
+}
+extra_setuptools_args = {}
+if 'setuptools' in sys.modules:
+    extra_setuptools_args = dict(
+        tests_require=['nose'],
+        test_suite='nose.collector',
+        zip_safe=False,
+        extras_require = dict(
+            doc='Sphinx>=0.3',
+            test='nose>=0.10.1',
+            nicom = 'dicom>=' + PYDICOM_MIN_VERSION)
+    )
 
 def main(**extra_args):
-    from numpy.distutils.core import setup
+    setup(name=NAME,
+          maintainer=MAINTAINER,
+          maintainer_email=MAINTAINER_EMAIL,
+          description=DESCRIPTION,
+          long_description=LONG_DESCRIPTION,
+          url=URL,
+          download_url=DOWNLOAD_URL,
+          license=LICENSE,
+          classifiers=CLASSIFIERS,
+          author=AUTHOR,
+          author_email=AUTHOR_EMAIL,
+          platforms=PLATFORMS,
+          version=VERSION,
+          requires=REQUIRES,
+          provides=PROVIDES,
+          packages     = ['mindboggle',
+                          'mindboggle.info',
+                          'mindboggle.utils',
+                          'mindboggle.extract',
+                          'mindboggle.extract.fundi_hmmf',
+                          'mindboggle.measure',
+                          'mindboggle.label'],
+          #                'mindboggle.externals',
+          #                'mindboggle.externals.tests',
+          #                'mindboggle.testing',
+          #                'mindboggle.tests',
+          #                'mindboggle.benchmarks',
+          # The package_data spec has no effect for me (on python 2.6) -- even
+          # changing to data_files doesn't get this stuff included in the source
+          # distribution -- not sure if it has something to do with the magic
+          # above, but distutils is surely the worst piece of code in all of
+          # python -- duplicating things into MANIFEST.in but this is admittedly
+          # only a workaround to get things started -- not a solution
 
-    setup(name=INFO_VARS['NAME'],
-          maintainer=INFO_VARS['MAINTAINER'],
-          maintainer_email=INFO_VARS['MAINTAINER_EMAIL'],
-          description=INFO_VARS['DESCRIPTION'],
-          long_description=INFO_VARS['LONG_DESCRIPTION'],
-          url=INFO_VARS['URL'],
-          download_url=INFO_VARS['DOWNLOAD_URL'],
-          license=INFO_VARS['LICENSE'],
-          classifiers=INFO_VARS['CLASSIFIERS'],
-          author=INFO_VARS['AUTHOR'],
-          author_email=INFO_VARS['AUTHOR_EMAIL'],
-          platforms=INFO_VARS['PLATFORMS'],
-          version=INFO_VARS['VERSION'],
-          requires=INFO_VARS['REQUIRES'],
-          configuration = configuration,
+          package_data = {'mindboggle':
+                          [pjoin('info', '*')]},
+          #                 pjoin('externals', 'tests', 'data', '*'),
+          #                ]},
+          #scripts      = [pjoin('bin', 'parrec2nii'),
+          #                pjoin('bin', 'nib-ls'),
+          #                ],
           cmdclass = cmdclass,
-          scripts = glob('bin/*'),
-          **extra_args)
-
-
+          **extra_args
+         )
 
 if __name__ == "__main__":
     main(**extra_setuptools_args)
-"""
+
+
