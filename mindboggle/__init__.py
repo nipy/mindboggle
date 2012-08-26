ea54d07311fe5ab4127077a7e29be861f147fb2a
binarybottle <arno@binarybottle.com>
2012-08-26 01:17:18 -0400

    Set up dist_utils setup.py, __init__.py, info.py, pkg_info.py, COMMIT_INFO.txt, and other files for package distribution.  Even the info/ text files are included in the installation.
    Former-commit-id: 3a37e69d8d5a105bc1d710100f2a0c903e6ea111

---
+++ mindboggle/__init__.py
@@ -0,0 +1,34 @@
+# emacs: -*- mode: python-mode; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+
+import os
+
+from .info import __version__, long_description as __doc__
+__doc__ += """
+Quickstart
+==========
+
+::
+
+   import mindboggle as mb
+
+   mindboggle('my_file.nii.gz')
+
+For more detailed information see the :ref:`manual`.
+"""
+# module imports
+#from . import blah as blah
+# object imports
+#from .blah import blah, blah
+
+# be friendly on systems with ancient numpy -- no tests, but at least importable
+try:
+    from numpy.testing import Tester
+    test = Tester().test
+    bench = Tester().bench
+    del Tester
+except ImportError:
+    def test(*args, **kwargs): raise RuntimeError('Need numpy >= 1.2 for tests')
+
+from .pkg_info import get_pkg_info as _get_pkg_info
+get_info = lambda : _get_pkg_info(os.path.dirname(__file__))
