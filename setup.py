b52515e6c6c00f3ad3e2338b9721afd52eb63cea
Arno Klein <arno@binarybottle.com>
2012-08-30 12:00:14 -0400

    Revise setup file names.
    Former-commit-id: fffc7464eedf754c1793bf792e5ab043cbbe84db

--- setup.py
+++ setup.py
@@ -32,8 +32,8 @@ from nisext.py3builder import build_py
 from nisext.sexts import get_comrec_build, package_check
 cmdclass = {'build_py': get_comrec_build('mindboggle', build_py)}
 
-# Get version and release info, which is all stored in mindboggle/info.py
-ver_file = os.path.join('mindboggle', 'info.py')
+# Get version and release info, which is all stored in mindboggle/dist_info.py
+ver_file = os.path.join('mindboggle', 'dist_info.py')
 exec(open(ver_file).read())
 
 # Do dependency checking
