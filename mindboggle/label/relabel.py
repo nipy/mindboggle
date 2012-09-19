29aa39d1a34c20f1a78138abd85e925df669cad6
Arno Klein <arno@binarybottle.com>
2012-09-19 14:12:51 -0400

    Add function to remove subcortical labels
    Former-commit-id: fb6af1962f77f84f6bf10e9d5a9c3e41feaa98d2

--- mindboggle/label/relabel.py
+++ mindboggle/label/relabel.py
@@ -13,16 +13,41 @@ Copyright 2012,  Mindboggle team (http://mindboggle.info), Apache v2.0 License
 
 def relabel_volume(input_file, old_labels, new_labels):
     """
-    Relabel volume labels in a nifti file.
+    Relabel volume labels.
 
     Parameters
     ----------
     input_file : labeled nibabel-readable (e.g., nifti) file
-    old_labels : lists of old labels
-    new_labels : lists of new labels
+    old_labels : list of old labels
+    new_labels : list of new labels
+
+    Example
+    -------
+    >>> import os
+    >>> relabel_file = os.path.join('info', 'labels.volume.DKT31to25.txt')
+    >>> old_labels, new_labels = read_columns(relabel_file, 2)
+    >>> input_file = 'labels.DKT31.manual.nii.gz'
+    >>> relabel_volume(input_file, old_labels, new_labels)
+
+    >>> input_filename = 'aparcNMMjt+aseg.nii.gz'
+    >>> output_filename = 'labels.DKT25.manual+aseg.nii.gz'
+    >>> subjects_path = '/home/arno/Data/Brains/Mindboggle101/subjects'
+    >>> output_subjects_path = subjects_path
+    >>> from label.relabel import relabel_volume
+    >>> from utils.io_file import read_columns
+    >>> relabel_file = os.path.join('info', 'labels.volume.DKT31to25.txt')
+    >>> old_labels, new_labels = read_columns(relabel_file, 2)
+    >>> atlas_subjects = read_columns('info/atlases101.txt', 1)[0]
+    >>> for subject in atlas_subjects:
+    >>>     input_file = os.path.join(subjects_path, subject, 'mri', input_filename)
+    >>>     output_file1 = os.path.join(os.getcwd(), input_filename)
+    >>>     output_file2 = os.path.join(output_subjects_path, subject, 'mri', output_filename)
+    >>>     if os.path.exists(input_file):
+    >>>         relabel_volume(input_file, old_labels, new_labels)
+    >>>         cmd = 'mv ' + output_file1 + ' ' + output_file2
+    >>>         print(cmd); os.system(cmd)
 
     """
-
     import os
     import numpy as np
     import nibabel as nb
@@ -54,6 +79,52 @@ def relabel_volume(input_file, old_labels, new_labels):
 
     return output_file
 
+def remove_volume_labels(input_file, labels_to_remove):
+    """
+    Remove labels from an image volume.
+
+    Parameters
+    ----------
+    input_file : labeled nibabel-readable (e.g., nifti) file
+    labels_to_remove : list of labels to remove
+
+    Example
+    -------
+    >>> import os
+    >>> input_file = 'labels.DKT25.manual+aseg.nii.gz'
+    >>> labels_to_remove = range(1,300) # Remove noncortical (+aseg) labels
+    >>> labels_to_remove.extend([1000,1001,2000,2001])
+    >>> remove_volume_labels(input_file, labels_to_remove)
+
+    """
+    import os
+    import numpy as np
+    import nibabel as nb
+
+    # Load labeled image volume and extract data as 1-D array
+    vol = nb.load(input_file)
+    xfm = vol.get_affine()
+    data = vol.get_data().ravel()
+
+    # Initialize output
+    new_data = data.copy()
+
+    # Loop through labels
+    for ilabel, label in enumerate(labels_to_remove):
+        label = int(label)
+        # Relabel
+        new_data[np.where(data==label)[0]] = 0
+
+    # Reshape to original dimensions
+    new_data = np.reshape(new_data, vol.shape)
+
+    # Save relabeled file
+    output_file = os.path.join(os.getcwd(), os.path.basename(input_file))
+    img = nb.Nifti1Image(new_data, xfm)
+    img.to_filename(output_file)
+
+    return output_file
+
 def relabel_surface(vtk_file, relabel_list, new_string):
     """
     Relabel surface in a VTK file.
@@ -66,7 +137,6 @@ def relabel_surface(vtk_file, relabel_list, new_string):
     new_string : new ending of vtk_file name (e.g., 'labels.DKT25.vtk')
 
     """
-
     import os
     import numpy as np
     import utils.io_vtk as io_vtk
@@ -118,8 +188,6 @@ def relabel_annot_file(hemi, subject, annot_name, new_annot_name, relabel_file):
     new_annot_name : new .annot name
 
     """
-
-    import os
     from nipype.interfaces.base import CommandLine
 
     cli = CommandLine(command='mris_translate_annotation')
