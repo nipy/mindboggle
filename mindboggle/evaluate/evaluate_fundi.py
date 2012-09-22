bda7c41c5285f72e6db5b32421cfd56aab908df6
Arno Klein <arno@binarybottle.com>
2012-09-22 00:36:09 -0400

    Add distance functions to mesh_operations. Refactor Yrjo's evaluate_fundi.py code.
    Former-commit-id: 639105617f885b89084ae5bc2afe58cc2332c118

--- mindboggle/evaluate/evaluate_fundi.py
+++ mindboggle/evaluate/evaluate_fundi.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python
 
 """
-Evaluate fundi.
+Evaluate fundi by computing closest distances from label boundary vertices
+corresponding to fundi in the Desikan-Killiany-Tourville cortical labeling
+protocol.
+
+Example
+-------
+
+$ python evaluate_fundi.py <fundi_file> <folds_file> <labels_file>
 
 
 Authors:
@@ -12,447 +19,142 @@ Copyright 2012,  Mindboggle team (http://mindboggle.info), Apache v2.0 License
 
 """
 
-def findNeighboringLabels(ind,Faces,labels):
+def compute_fundus_distances(label_boundary_fundi, fundi, folds, points, n_fundi):
+    """
+    Create a fundus distance matrix.
 
-    nlabels = [nan,nan,nan,nan]
+    Compute the minimum distance from each label boundary vertex corresponding
+    to a fundus in the Desikan-Killiany-Tourville cortical labeling protocol
+    to all of the fundus vertices in the same fold.
 
-    for i in range(len(Faces)):
-        for j in range(0,3):
-            if Faces[i,j] == ind:
+    In the resulting distance matrix, the leftmost column contains distances
+    between label boundaries and fundi for each point, and subsequent columns
+    represent fundi, with the distance values repeated only for fundus points
+    (values default to -1).
 
-                for k in range(0,3):
-                    found = 0
-                    nextFree = -1
-                    for s in range(0,4):
-                        if nlabels[s] == nan:
-                            if nextFree < 0:
-                                nextFree = s
-
-                        if nlabels[s] == labels[Faces[i,k]]:
-                            found = 1
-                    if found == 0:
-                        nlabels[nextFree] = labels[Faces[i,k]]
-
-    nlabels = sorted(nlabels)
-
-    return nlabels
-
-
-def extractLabelFundi(B):
-
+    """
     import numpy as np
+    from utils.mesh_operations import closest_distance
 
-    n_vertices = len(B)
+    n_points = len(points)
 
-    labels = set(labels)
-    subset_indices = []
-    for Id, pair_list in enumerate(sulcus_label_lists):
-        if labels.issubset(set(pair_list)):
-            subset_indices.append(Id)
+    distances = np.zeros(n_points)
+    distance_matrix = -1 * np.ones((n_points, n_fundi))
 
-    return subset_indices
+    sum_distances = 0
+    num_distances = 0
 
-    LF = np.zeros(n_vertices)
+    # For each label boundary fundus point
+    for i_label_point, fundus_ID in enumerate(label_boundary_fundi):
+        if fundus_ID > 0:
 
-    lab1 = 0
-    lab2 = 0
-    fnum = 0
+            # Find indices of fundus points in the same fold
+            I_fundus_points = [i for i,x in enumerate(fundi)
+                               if x > 0
+                               if folds[i] == folds[i_label_point]]
 
-    for i in range(n_vertices):
-        if B[i,0] is not 'nan':
+            # Find the closest fundus point to label boundary fundus point
+            d = closest_distance(points[i_label_point],
+                                 points[I_fundus_points])
+            distances[i_label_point] = d
+            distance_matrix[i_label_point, fundus_ID - 1] = d
 
+            sum_distances += d
+            num_distances += 1
 
-            for j in range(1,46):
+            if i_label_point % 1000 == 0 and num_distances > 0:
+                percent_done = 100.0 * float(i_label_point) / float(n_points)
+                mean_distance = sum_distances / num_distances
+                print('Done: {0} pct, mean dist {1}, {2}'.format(
+                      percent_done, mean_distance, i_label_point))
 
-                if j == 1:
-                    lab1 = 12
-                    lab2 = 28
-                    fnum = 1
+    mean_distance = sum_distances / num_distances
+    print('Done: 100 pct, mean dist {0}'.format(mean_distance))
 
-                elif j == 2:
-                    lab1 = 3
-                    lab2 = 28
-                    fnum = 2
-
-                elif j == 3:
-                    lab1 = 3
-                    lab2 = 18
-                    fnum = 3
-
-                elif j == 4:
-                    lab1 = 3
-                    lab2 = 24
-                    fnum = 4
-
-                elif j == 5:
-                    lab1 = 24
-                    lab2 = 28
-                    fnum = 4
-
-                elif j == 6:
-                    lab1 = 18
-                    lab2 = 24
-                    fnum = 4
-
-                elif j == 7:
-                    lab1 = 22
-                    lab2 = 24
-                    fnum = 5
-
-                elif j == 8:
-                    lab1 = 22
-                    lab2 = 29
-                    fnum = 6
-
-                elif j == 9:
-                    lab1 = 22
-                    lab2 = 31
-                    fnum = 6
-
-                elif j == 10:
-                    lab1 = 29
-                    lab2 = 31
-                    fnum = 7
-
-                elif j == 11:
-                    lab1 = 8
-                    lab2 = 29
-                    fnum = 7
-
-                elif j == 12:
-                    lab1 = 8
-                    lab2 = 31
-                    fnum = 8
-
-                elif j == 13:
-                    lab1 = 30
-                    lab2 = 31
-                    fnum = 9
-
-                elif j == 14:
-                    lab1 = 8
-                    lab2 = 11
-                    fnum = 10
-
-                elif j == 15:
-                    lab1 = 11
-                    lab2 = 29
-                    fnum = 10
-
-                elif j == 16:
-                    lab1 = 11
-                    lab2 = 15
-                    fnum = 11
-
-                elif j == 17:
-                    lab1 = 9
-                    lab2 = 11
-                    fnum = 11
-
-                elif j == 18:
-                    lab1 = 15
-                    lab2 = 30
-                    fnum = 12
-
-                elif j == 19:
-                    lab1 = 9
-                    lab2 = 15
-                    fnum = 13
-
-                elif j == 20:
-                    lab1 = 30
-                    lab2 = 35
-                    fnum = 14
-
-                elif j == 21:
-                    lab1 = 34
-                    lab2 = 35
-                    fnum = 14
-
-                elif j == 22:
-                    lab1 = 12
-                    lab2 = 35
-                    fnum = 14
-
-                elif j == 23:
-                    lab1 = 2
-                    lab2 = 35
-                    fnum = 14
-
-                elif j == 24:
-                    lab1 = 24
-                    lab2 = 35
-                    fnum = 14
-
-                elif j == 25:
-                    lab1 = 22
-                    lab2 = 35
-                    fnum = 14
-
-                elif j == 26:
-                    lab1 = 31
-                    lab2 = 35
-                    fnum = 14
-
-                elif j == 27:
-                    lab1 = 30
-                    lab2 = 34
-                    fnum = 15
-
-                elif j == 28:
-                    lab1 = 2
-                    lab2 = 14
-                    fnum = 16
-
-                elif j == 29:
-                    lab1 = 2
-                    lab2 = 28
-                    fnum = 16
-
-                elif j == 30:
-                    lab1 = 2
-                    lab2 = 17
-                    fnum = 16
-
-                elif j == 31:
-                    lab1 = 17
-                    lab2 = 25
-                    fnum = 16
-
-                elif j == 32:
-                    lab1 = 17
-                    lab2 = 28
-                    fnum = 17
-
-                elif j == 33:
-                    lab1 = 5
-                    lab2 = 25
-                    fnum = 18
-
-                elif j == 34:
-                    lab1 = 13
-                    lab2 = 25
-                    fnum = 19
-
-                elif j == 35:
-                    lab1 = 2
-                    lab2 = 13
-                    fnum = 19
-
-                elif j == 36:
-                    lab1 = 14
-                    lab2 = 28
-                    fnum = 20
-
-                elif j == 37:
-                    lab1 = 2
-                    lab2 = 4
-                    fnum = 21
-
-                elif j == 38:
-                    lab1 = 12
-                    lab2 = 18
-                    fnum = 22
-
-                elif j == 39:
-                    lab1 = 3
-                    lab2 = 12
-                    fnum = 22
-
-                elif j == 40:
-                    lab1 = 12
-                    lab2 = 14
-                    fnum = 23
-
-                elif j == 41:
-                    lab1 = 7
-                    lab2 = 9
-                    fnum = 24
-
-                elif j == 42:
-                    lab1 = 7
-                    lab2 = 11
-                    fnum = 24
-
-                elif j == 43:
-                    lab1 = 6
-                    lab2 = 7
-                    fnum = 25
-
-                elif j == 44:
-                    lab1 = 7
-                    lab2 = 16
-                    fnum = 25
-
-                elif j == 45:
-                    lab1 = 7
-                    lab2 = 13
-                    fnum = 25
+    return distances, distance_matrix
 
 
-                if B[i,0] == lab1 or B[i,1] == lab1 or B[i,2] == lab1:
-                    if B[i,1] == lab2 or B[i,2] == lab2 or B[i,3] == lab2:
-                        LF[i] = fnum
-                        #print('LOC3 {0}, {1}, {2}'.format(i,B[i],LF[i]))
+if __name__ == "__main__":
 
-
-    return LF
-
-
-
-def extractLabelBoundaries(n_vertices,Faces,labels,folds):
-
+    import sys
     import numpy as np
-
-    B = np.zeros((n_vertices,4))
-    B = B + 1000
-
-    counter = 0
-    for i in range(len(Faces)):
-
-        for j in range(0,3):
-            currVert = Faces[i,j]
-
-            if folds[currVert] > 0.0:
-                i1 = Faces[i,0]
-                i2 = Faces[i,1]
-                i3 = Faces[i,2]
-
-                nlabels = [labels[i1],labels[i2],labels[i3]]
-
-
-                for k in range(0,3):
-                    found = 0
-                    nextFree = -1
-                    for s in range(0,4):
-
-                        if B[currVert,s] == 1000:
-                            if nextFree < 0:
-                                nextFree = s
-
-                        if nlabels[k] == B[currVert,s]:
-                            found = 1
-                    if found == 0:
-                        B[currVert,nextFree] = nlabels[k]
-
-                B[currVert] = sorted(B[currVert])
-
-                if B[currVert,1] == 1000:
-                    B[currVert] = [1000,1000,1000,1000]
-
-    return B
-
-def compEucDist(a,b):
-
-    import math
-
-    dist = pow(a[0] - b[0],2) + pow(a[1] - b[1],2) + pow(a[2] - b[2], 2)
-    dist = math.sqrt(dist)
-
-    return dist
-
-def findDistToClosestFundusPoint(currVertex,fundi,vertices):
-
-    minDist = 10000
-
-    for i in range(len(fundi)):
-        if fundi[i] > 1.0:
-            currDist = compEucDist(currVertex,vertices[i])
-            if currDist < minDist:
-                minDist = currDist
-
-    return minDist
-
-def compFundusPointDists(LF,fundi,vertices):
-
-    import numpy as np
-
-    n_vertices = len(vertices)
-
-    D = np.zeros((n_vertices,max(LF)+1))
-    D = D - 1
-
-    allDist = 0
-    numDist = 0
-
-    for i in range(n_vertices):
-    #for i in range(0,6000):
-        if LF[i] > 0:
-            D[i,0] = findDistToClosestFundusPoint(vertices[i],fundi,vertices)
-            D[i,LF[i]] = D[i,0]
-
-            allDist += D[i,0]
-            numDist += 1
-
-        if i % 1000 == 0 and numDist > 0:
-            pctDone = 100.0 * float(i) / float(n_vertices)
-            meanDist = allDist / numDist
-            print('Done: {0} pct, mean dist {1}, {2}'.format(pctDone,meanDist,i))
-
-    meanDist = allDist / numDist
-    print('Done: 100 pct, mean dist {0}'.format(meanDist))
-    return D
-
-def main():
-
-    import re, string, sys, os
-
-    mb_path = os.path.abspath('/Users/yrjo/yrjo_work/mindboggle2/fundus_evaluation/mindboggle')
-    sys.path.append(mb_path)
-    vtk_python_path = os.path.abspath('/Users/yrjo/yrjo_work/VTK_bin/Wrapping/Python')
-    sys.path.append(vtk_python_path)
-    vtk_bin_path = os.path.abspath('/Users/yrjo/yrjo_work/VTK_bin/bin')
-    sys.path.append(vtk_bin_path)
-
-
-    import vtk
-    import numpy as np
-    import mindboggle
-    from mindboggle.utils.io_vtk import load_scalar, write_scalars
-    from mindboggle.utils.io_vtk import write_scalars
-
-    print(sys.argv[0])
-    print('Version 20120908a')
-
-    print('***')
-    print('Input fundi:')
-    print(sys.argv[1])
-
-    print('***')
-    print('Input folds:')
-    print(sys.argv[2])
-
-    print('***')
-    print('Input labels:')
-    print(sys.argv[3])
-    print('***')
-
+    from utils.io_vtk import load_scalar
+    from info.sulcus_boundaries import sulcus_boundaries
+    from utils.mesh_operations import detect_boundaries, find_all_neighbors
 
     fundi_file = sys.argv[1]
     folds_file = sys.argv[2]
     labels_file = sys.argv[3]
 
-    #fundi_file = '/Users/yrjo/yrjo_work/mindboggle2/fundus_evaluation/results/features/_hemi_lh_subject_HLN-12-3/fundi.vtk'
-    #folds_file = '/Users/yrjo/yrjo_work/mindboggle2/fundus_evaluation/results/features/_hemi_lh_subject_HLN-12-3/folds.vtk'
-    #labels_file = '/Users/yrjo/yrjo_work/mindboggle2/fundus_evaluation/results/labels/_hemi_lh_subject_HLN-12-3/lh.pial.labels.max.vtk'
+    print('***')
+    print('Input fundi:' + fundi_file)
+    print('Input folds:' + folds_file)
+    print('Input labels:' + labels_file)
+    print('***')
 
-    vertices, Faces, fundi = load_scalar(fundi_file, return_arrays=1)
-    vertices, Faces, folds = load_scalar(folds_file, return_arrays=1)
-    vertices, Faces, labels = load_scalar(labels_file, return_arrays=1)
+    # Load fundi, folds, labels
+    points, faces, fundi = load_scalar(fundi_file, return_arrays=1)
+    points, faces, folds = load_scalar(folds_file, return_arrays=1)
+    points, faces, labels = load_scalar(labels_file, return_arrays=1)
+    n_points = len(points)
 
+    # List of lists of folds
+    unique_fold_IDs = set(folds)
+    fold_lists = []
+    for id in unique_fold_IDs:
+        if id > 0:
+            fold = [i for i,x in enumerate(folds) if x == id]
+            fold_lists.append(fold)
 
-    n_vertices = len(vertices)
+    # Calculate neighbor lists for all points
+    neighbor_lists = find_all_neighbors(faces)
 
-    B = extractLabelBoundaries(n_vertices,Faces,labels,folds)
+    # Prepare list of all unique sorted label pairs in the labeling protocol
+    label_pair_lists = sulcus_boundaries()
+    n_fundi = len(label_pair_lists)
 
-    LF = extractLabelFundi(B)
+    # Initialize an array of label boundaries fundus IDs
+    # (label boundary vertices that define sulci in the labeling protocol)
+    label_boundary_fundi = np.zeros(n_points)
 
-    D = compFundusPointDists(LF,fundi,vertices)
+    # For each list of label pairs (corresponding to a sulcus)
+    for isulcus, label_pairs in enumerate(label_pair_lists):
 
-    #write_scalars(vtk_file, vertices, len(), Faces, D, LUT_names=[])
-    TEST = write_scalars('test.vtk', vertices, len(vertices), Faces, D)
+        # For each fold
+        for fold in fold_lists:
 
-    return 0
+            # Find label boundary points within the fold
+            boundary_points, boundary_labels = detect_boundaries(labels,
+                 fold, neighbor_lists)
 
-if __name__ == '__main__':
-    main()
+            # Keep the boundary points with label pair labels
+            fundus_points = [x for i,x in enumerate(boundary_points)
+                             if boundary_labels[i] in label_pairs]
+
+            # Store the points as sulcus IDs in the fundus IDs array
+            label_boundary_fundi[fundus_points] = isulcus + 1
+
+    # Create a distance matrix:
+    # The leftmost column contains distances between label boundaries
+    # and fundi for each point, and subsequent columns represent fundi,
+    # with the distance values repeated only for fundus points (else -1).
+    distances, distance_matrix = compute_fundus_distances(label_boundary_fundi,
+        fundi, folds, points, n_fundi)
+
+    # Compute mean distances for each fundus
+    fundus_mean_distances = np.zeros(n_fundi)
+    fundus_std_distances = np.zeros(n_fundi)
+    for ifundus in range(n_fundi):
+        fundus_distances = distance_matrix[:, ifundus]
+        fundus_distances = [x for x in fundus_distances if x != -1]
+        fundus_mean_distances[ifundus] = np.mean(fundus_distances)
+        fundus_std_distances[ifundus] = np.std(fundus_distances)
+
+    print('Fundus mean distances')
+    print(fundus_mean_distances)
+    print('Fundus standard deviations of distances')
+    print(fundus_std_distances)
+
+    # Write resulting fundus-label boundary distances to VTK file
+    write_scalars('evaluate_fundi.vtk', points, range(n_points), faces,
+                  [distances], ['fundus-label boundary distances'])
