3bd14fc293a5cf1cb17b6eada572bccf815b5552
Forrest Sheng Bao <forrest.bao@gmail.com>
2012-09-30 18:37:19 -0500

    disable options for secondary surface feature output in fs_extract.py
    Former-commit-id: afd086cd39fbb360372ec39ce6a418b862fb47d3

--- mindboggle/extract/fundi_from_pits/fs_extract.py
+++ mindboggle/extract/fundi_from_pits/fs_extract.py
@@ -1,26 +1,48 @@
+#!/usr/bin/python
+"""
+Extracting features from FreeSurfer-format input files. 
+
+Authors:
+    - Forrest Sheng Bao  (forrest.bao@gmail.com)  http://fsbao.net
+
+Copyright 2012,  Mindboggle team (http://mindboggle.info), Apache v2.0 License
+
+For algorithmic details, please check:     
+    Forrest S. Bao, et al., Automated extraction of nested sulcus features from human brain MRI data, 
+    IEEE EMBC 2012, San Diego, CA
+
+Dependencies:
+    numpy
+
+Last updated on 2012-09-30: 
+    1. Disable secondary surface input and the ability to output features onto a secondary surface.
+    2. If users want to visualize features on a secondary surface, check maps_to_second_surface.py in utils folder. 
+    
+"""
+
 def print_help():
     print "\n  Usage: python extract.py [OPTIONS] SurfFile\n"
     print "  Options (at least one of --curv and --convex are needed): "
     print "  --thick ThicknessFile"
     print "    the thickness file provided by FreeSurfer\n"
-    print "  --second SecondSurfaceFile"
-    print "    the 2nd surface to save feature(s) extracted, in FreeSurfer surface format\n"
+#    print "  --second SecondSurfaceFile"
+#    print "    the 2nd surface to save feature(s) extracted, in FreeSurfer surface format\n"
     print "  --conv ConvexityFile"
     print "    the convexity file provided by FreeSurfer\n"
     print "  --curv CurvatureFile"
     print "    the convexity file provided by FreeSurfer\n"    
     print "  --fundi FundiVTK"
     print "    the VTK output file to save fundi, onto the same surface as VTKFile\n"
-    print "  --fundi2 Fundi2VTK"
-    print "    the VTK output file to save fundi, onto the second surface in SecondSurfaceFile\n"
+#    print "  --fundi2 Fundi2VTK"
+#    print "    the VTK output file to save fundi, onto the second surface in SecondSurfaceFile\n"
     print "  --pits PitsVTK"
     print "    the VTK output file to save pits, onto the same surface as VTKFile\n"
-    print "  --pits2 Pits2VTK"
-    print "    the VTK output file to save pits, onto the second surface in SecondSurfaceFile\n"
+#    print "  --pits2 Pits2VTK"
+#    print "    the VTK output file to save pits, onto the second surface in SecondSurfaceFile\n"
     print "  --sulci SulciVTK"
     print "    the VTK output file to save sulci, onto the same surface as VTKFile\n"
-    print "  --sulci2 Sulci2VTK"
-    print "    the VTK output file to save sulci, onto the second surface in SecondSurfaceFile\n"
+#    print "  --sulci2 Sulci2VTK"
+#    print "    the VTK output file to save sulci, onto the second surface in SecondSurfaceFile\n"
     print "  --use MapToExtractFeatures"
     print "    Choose a map to use for extracting features. Other maps will be used as shape descriptors associated with features only. Values as follows."
     print "    conv: use convexity map (usually of suffix .sulc in FreeSurfer) to extract features (default)"
@@ -28,7 +50,7 @@ def print_help():
     print "  --sulciThld SulciThld"
     print "    SulciThld: the value to threshold the surface to get sulci (default = 0.2)"
     print "  Examples: "
-    print "    python extract.py --use conv --thick lh.thickness --second lh.inflated --conv lh.sulc --curv lh.curv --fundi lh.pial.fundi.vtk --pits lh.pial.pits.vtk --sulci lh.pial.sulci.vtk --fundi2 lh.inflated.fundi.vtk --pits2 lh.inflated.pits.vtk --sulci2 lh.inflated.sulci.vtk --sulciThld 0.15 lh.pial \n"
+    print "    python extract.py --use conv --thick lh.thickness --conv lh.sulc --curv lh.curv --fundi lh.pial.fundi.vtk --pits lh.pial.pits.vtk --sulci lh.pial.sulci.vtk --sulciThld 0.15 lh.pial \n"
 
 def check_opt(opts, args):
     '''Check whether opts and args satisfy constraints 
@@ -42,7 +64,7 @@ def check_opt(opts, args):
         sys.exit()
         
     ## Check whether second surface is provided 
-    Need2nd, Given2nd = False, False # A second surface is needed if options to save features into second surface are on. 
+#    Need2nd, Given2nd = False, False # A second surface is needed if options to save features into second surface are on. 
     # check whether at least one of --curv and --convex is provided; check wether curv or convex is given according to --use flag parameter
     GivenCurv, GivenConvex = False, False
     
@@ -53,10 +75,10 @@ def check_opt(opts, args):
             print "[ERROR] The option",o, "is missing parameter. Please check usage and provide it."
             print "To check usage, run: python extract.py"
             sys.exit()
-        elif o in ["--fundi2","--pits2","--sulci2"]:
-            Need2nd = True # we will need the option --second and its parameter
-        elif o=="--second":
-            Given2nd = True   
+#        elif o in ["--fundi2","--pits2","--sulci2"]:
+#            Need2nd = True # we will need the option --second and its parameter
+#        elif o=="--second":
+#            Given2nd = True   
         elif o=="--curv": 
             GivenCurv = True
         elif o=="--conv":
@@ -64,10 +86,10 @@ def check_opt(opts, args):
         elif o=="--use":
             Use=p
 #    print Need2nd, Given2nd
-    if Need2nd and not Given2nd: # second surface is needed but not provided
-        print "[ERROR] Second surface is needed but not provided. Please check usage and provide a second surface using --second option."
-        print "To check usage, run: python extract.py"
-        sys.exit()
+#    if Need2nd and not Given2nd: # second surface is needed but not provided
+#        print "[ERROR] Second surface is needed but not provided. Please check usage and provide a second surface using --second option."
+#        print "To check usage, run: python extract.py"
+#        sys.exit()
     if not GivenCurv and not GivenConvex:
         print "[ERROR] At least one map in FreeSurfer curvature format needed after --curv or --convex is needed."
         print "To check usage, run: python extract.py"
@@ -89,9 +111,9 @@ def process_opt(opts, args):
     OutputColor = "\033[36m"
     EndColor = "\033[0m"
     
-    ThickFile, ConvFile, CurvFile, SurfFile2 = '', '', '', ''
+    ThickFile, ConvFile, CurvFile = '', '', ''
     FundiVTK, PitsVTK, SulciVTK = '','',''
-    Fundi2VTK, Pits2VTK, Sulci2VTK = '','',''
+#    Fundi2VTK, Pits2VTK, Sulci2VTK = '','',''
     Use = 'conv'
     SulciThld = 0.2
 
@@ -99,10 +121,10 @@ def process_opt(opts, args):
     print "  [Input] FreeSurfer surface file:" + InputColor + SurfFile  + EndColor 
     
     for o,p in opts:
-        if o == '--second' :
-            SurfFile2 = p
-            print "  [Input] 2nd surface:" + InputColor + SurfFile2 + EndColor
-        elif o == '--conv':
+#        if o == '--second' :
+#            SurfFile2 = p
+#            print "  [Input] 2nd surface:" + InputColor + SurfFile2 + EndColor
+        if o == '--conv':
             ConvFile = p
             print "  [Input] Convexity File:" + InputColor + ConvFile + EndColor
         elif o == '--thick':
@@ -120,15 +142,15 @@ def process_opt(opts, args):
         elif o == '--sulci':
             SulciVTK = p 
             print "  [Output] Sulci in:", OutputColor, SulciVTK, EndColor
-        elif o == '--fundi2':
-            Fundi2VTK = p
-            print "  [Output] Fundi on 2nd surface in:", OutputColor, Fundi2VTK, EndColor
-        elif o == '--pits2':
-            Pits2VTK = p
-            print "  [Output] Pits on 2nd surface in:", OutputColor, Pits2VTK, EndColor
-        elif o == '--sulci2':
-            Sulci2VTK = p 
-            print "  [Output] Sulci on 2nd surface in:", OutputColor, Sulci2VTK, EndColor
+#        elif o == '--fundi2':
+#            Fundi2VTK = p
+#            print "  [Output] Fundi on 2nd surface in:", OutputColor, Fundi2VTK, EndColor
+#        elif o == '--pits2':
+#            Pits2VTK = p
+#            print "  [Output] Pits on 2nd surface in:", OutputColor, Pits2VTK, EndColor
+#        elif o == '--sulci2':
+#            Sulci2VTK = p 
+#            print "  [Output] Sulci on 2nd surface in:", OutputColor, Sulci2VTK, EndColor
         elif o == '--use':
             Use = p
             if Use == 'curv':
@@ -148,19 +170,22 @@ def process_opt(opts, args):
             print "[ERROR] Option", o, "is unrecognized. Please check usage."
             sys.exit()
 
-    return SurfFile, SurfFile2, ThickFile, CurvFile, ConvFile,\
-        FundiVTK, PitsVTK, SulciVTK, Fundi2VTK, Pits2VTK, Sulci2VTK, Use, SulciThld
+    return SurfFile, ThickFile, CurvFile, ConvFile,\
+        FundiVTK, PitsVTK, SulciVTK, Use, SulciThld
 
 if __name__ == "__main__":
     import sys, getopt # this line imports pulic libraries
     import libfundi
 
     try:
-        opts, args = getopt.getopt(sys.argv[1:],"",["thick=","second=","conv=","fundi=","fundi2=", "pits=","pits2=","sulci=","sulci2=", "curv=", "use=", "sulciThld="])
+        opts, args = getopt.getopt(sys.argv[1:],"",["thick=","conv=","fundi=", "pits=","sulci=", "curv=", "use=", "sulciThld="])
     except getopt.GetoptError, err:
         print str(err) # will print something like "option -a not recognized"
     #    print_help()
         print "To check usage, run: python extract.py"
+        print "If you use options like --second, --pits2, --fundi2, --sulci2,",
+        "please note that we have disabled mapping features onto secondary surface.",
+        "Please check maps_to_second_surface.py under utils directory to do so."
         sys.exit(2)
     
     check_opt(opts, args)
