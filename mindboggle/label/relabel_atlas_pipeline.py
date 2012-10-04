53a335d2597fc4516c472bebd04aa9baf5a8d54a
Arno Klein <arno@binarybottle.com>
2012-10-04 10:01:13 -0400

    Update relabel_atlas_pipeline.py to consolidate DKT31 to DKT25 labels before copying to Mindboggle-101 release data.
    Former-commit-id: 74324cdb301d2eedf6ea547bae1d3cc0193f3273

--- mindboggle/label/relabel_atlas_pipeline.py
+++ mindboggle/label/relabel_atlas_pipeline.py
@@ -35,13 +35,14 @@ import os
 do_load_vtk_surfaces = False
 do_combine_atlas_labels = 1
 do_convert_atlas_annot = 1
-copy_to_fs_atlases = 1
-copy_to_mb_atlases = 1
+do_convert_original_atlas_annot = 1
+copy_to_output = 1
 #-----------------------------------------------------------------------------
 # From settings.py
 #-----------------------------------------------------------------------------
-output_path = '/projects/Mindboggle/output'  # Where to save output
+output_path = '/projects/Mindboggle/output'  # Where to save processing output
 subjects_path = os.environ['SUBJECTS_DIR']  # FreeSurfer subjects directory
+copy_path = subjects_path
 base_path = '/projects/Mindboggle/mindboggle'  # Mindboggle home directory
 info_path = '/projects/Mindboggle/mindboggle/mindboggle/info'  # info directory
 temp_path = os.path.join(output_path, 'workspace')  # Where to save temp files
@@ -55,7 +56,7 @@ hemis = ['lh','rh']
 import utils.io_file as iof
 atlas_list_file = os.path.join(info_path, 'atlases101.txt')
 subjects = iof.read_columns(atlas_list_file, 1)[0]
-#subjects = ['MMRR-3T7T-2-1','MMRR-3T7T-2-2']
+subjects = ['OASIS-TRT-20-11']
 #-----------------------------------------------------------------------------
 # Import system and nipype Python libraries
 #-----------------------------------------------------------------------------
@@ -66,7 +67,8 @@ from nipype.interfaces.io import DataGrabber, DataSink
 #-----------------------------------------------------------------------------
 # Import Mindboggle Python libraries
 #-----------------------------------------------------------------------------
-from utils.io_free import freeannot_to_vtk
+from utils.io_free import read_surface
+from utils.io_vtk import freeannot_to_vtk, freesurface_to_vtk
 from label.relabel import relabel_annot_file
 #-----------------------------------------------------------------------------
 # Initialize main workflow
@@ -103,6 +105,16 @@ datasink.inputs.base_directory = output_path
 datasink.inputs.container = 'results'
 if not os.path.isdir(output_path):  os.makedirs(output_path)
 
+#-------------------------------------------------------------------------------
+# Convert surfaces to VTK
+#-------------------------------------------------------------------------------
+if not do_load_vtk_surfaces:
+    convertsurf = Node(name = 'Surf_to_VTK',
+                       interface = Fn(function = freesurface_to_vtk,
+                                      input_names = ['surface_file'],
+                                      output_names = ['vtk_file']))
+    flow.connect([(surf, convertsurf, [('surface_files','surface_file')])])
+
 #=============================================================================
 #   Combine .annot labels and convert to VTK
 #=============================================================================
@@ -121,6 +133,7 @@ if do_combine_atlas_labels:
                                                         'new_annot_name',
                                                         'relabel_file'],
                                          output_names = ['new_annot_name']))
+    atlasflow.add_nodes([combine_labels])
     combine_labels.inputs.annot_name = label_string_old
     combine_labels.inputs.new_annot_name = label_string
     combine_labels.inputs.relabel_file = relabel_file
@@ -132,7 +145,7 @@ if do_combine_atlas_labels:
 #-----------------------------------------------------------------------------
 if do_convert_atlas_annot:
     atlas_vtk = Node(name = 'Convert_atlas_labels',
-                     interface = Fn(function = annot_to_vtk,
+                     interface = Fn(function = freeannot_to_vtk,
                                     input_names = ['surface_file',
                                                    'hemi',
                                                    'subject',
@@ -152,8 +165,33 @@ if do_convert_atlas_annot:
     else:
         flow.connect([(convertsurf, atlasflow,
                          [('vtk_file','Convert_atlas_labels.surface_file')])])
-    flow.connect([(atlasflow, datasink,
-                     [('Convert_atlas_labels.vtk_file','atlas_labels')])])
+#    flow.connect([(atlasflow, datasink,
+#                     [('Convert_atlas_labels.vtk_file','atlas_labels')])])
+
+
+if do_convert_original_atlas_annot:
+    orig_atlas_vtk = Node(name = 'Convert_original_atlas_labels',
+                     interface = Fn(function = freeannot_to_vtk,
+                                    input_names = ['surface_file',
+                                                   'hemi',
+                                                   'subject',
+                                                   'subjects_path',
+                                                   'annot_name'],
+                                    output_names = ['vtk_file']))
+    atlasflow.add_nodes([orig_atlas_vtk])
+    flow.connect([(info, atlasflow,
+                        [('hemi','Convert_original_atlas_labels.hemi'),
+                         ('subject','Convert_original_atlas_labels.subject')])])
+    orig_atlas_vtk.inputs.subjects_path = subjects_path
+    orig_atlas_vtk.inputs.annot_name = label_string_old
+    if do_load_vtk_surfaces:
+        flow.connect([('surf', 'Convert_original_atlas_labels.atlas_vtk',
+                       [('surface_files','Convert_original_atlas_labels.surface_file')])])
+    else:
+        flow.connect([(convertsurf, atlasflow,
+                         [('vtk_file','Convert_original_atlas_labels.surface_file')])])
+#    flow.connect([(atlasflow, datasink,
+#                     [('Convert_original_atlas_labels.vtk_file','atlas_labels')])])
 
 ##############################################################################
 if __name__== '__main__':
@@ -162,31 +200,30 @@ if __name__== '__main__':
 #-------------------------------------------------------------------------
 # Copy results to atlas label directories
 #-------------------------------------------------------------------------
-if copy_to_fs_atlases or copy_to_mb_atlases:
+if copy_to_output:
 
     for s in subjects:
         for h in hemis:
 
-            srcs = []
-
-            if do_combine_atlas_labels:
-                src = os.path.join(output_path, datasink.inputs.container,
-                                   'combine_labels',
-                                   '_hemi_' + h + '_subject_' + s,
-                                   h + '.' + label_string + '.manual.vtk')
-                srcs.append(src)
-
             if do_convert_atlas_annot:
-                src = os.path.join(output_path, datasink.inputs.container,
-                                   'atlas_vtk',
+                src = os.path.join(temp_path, #output_path, datasink.inputs.container,
+                                   'Atlas_relabeling_workflow',
+                                   'Atlas_workflow',
                                    '_hemi_' + h + '_subject_' + s,
-                                   h + '.' + label_string + '.manual.vtk')
-                srcs.append(src)
+                                   'Convert_atlas_labels',
+                                   h + '.pial.' + label_string + '.vtk')
+                tgt = os.path.join(copy_path, s, 'label', h + '.' + label_string + '.vtk')
+                cmd = ' '.join(['cp', src, tgt])
+                print(cmd); os.system(cmd)
 
-            for src in srcs:
-                if copy_to_output:
-                    tgt = os.path.join(base_path, 'data', 'atlases',
-                                       'freesurfer', s, 'label')
-                    cmd = ' '.join(['cp', src, tgt])
-                    print(cmd); os.system(cmd)
+            if do_convert_original_atlas_annot:
+                src = os.path.join(temp_path, #output_path, datasink.inputs.container,
+                                   'Atlas_relabeling_workflow',
+                                   'Atlas_workflow',
+                                   '_hemi_' + h + '_subject_' + s,
+                                   'Convert_original_atlas_labels',
+                                   h + '.pial.' + label_string_old + '.vtk')
+                tgt = os.path.join(copy_path, s, 'label', h + '.' + label_string_old + '.vtk')
+                cmd = ' '.join(['cp', src, tgt])
+                print(cmd); os.system(cmd)
 
