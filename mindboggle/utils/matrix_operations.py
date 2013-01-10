c47ad6e5109b3c0fadc3bbc17fe841fd96b77ee7
Arno Klein <arno@binarybottle.com>
2013-01-10 12:28:21 -0500

    Corrected rotate90() to accept only isometric voxels
    Former-commit-id: 7d5d058076f0ffaefd0400230e2cd59b59eed77f

--- mindboggle/utils/matrix_operations.py
+++ mindboggle/utils/matrix_operations.py
@@ -91,13 +91,13 @@ def rotate90(input_file, rotations=[1,0,0],
              use_matrix=False, use_header=True):
     """
     Rotate image volume data 90 degrees about x, y, and/or z axes
-    without regard to the file header.
+    without regard to the file header.  Assumes isometric voxels.
 
     Parameters
     ----------
     input_file : image volume
     rotations : list of integers in {-1,0,1}
-        rotate -90, 0, or +90 degrees about x-,y-,z-axis
+        rotate -90, 0, and/or +90 degrees about x-, y-, or z-axis (in order)
     use_matrix : Boolean
         use input file's affine matrix?
     use_header : Boolean
@@ -107,6 +107,20 @@ def rotate90(input_file, rotations=[1,0,0],
     -------
     out_file : string
         output image volume
+
+    Examples
+    --------
+    >>> # Example without proper inputs specified:
+    >>> # Make voxels isometric:
+    >>> import os
+    >>> d = os.listdir('.')
+    >>> for f in d:
+    >>>     if '1stvol' in f:
+    >>>         c=' '.join(['c3d',f,'-resample 128x128x128','-o isometric_'+f])
+    >>>         print(c); os.system(c)
+    >>> # Rotate 90 degrees about x-axis:
+    >>> from mindboggle.utils.matrix_operations import rotate90
+    >>> rotate90(input_file, rotations=[1,0,0], use_matrix=False, use_header=True)
 
     """
     import os
@@ -121,30 +135,34 @@ def rotate90(input_file, rotations=[1,0,0],
     if use_header:
         hdr = img.get_header()
     lenx, leny, lenz = np.shape(dat)
+    if lenx != leny or lenx != lenz or leny != lenz:
+        os.error('Input file should have isometric voxels.')
+
     dat_new = np.zeros((lenx, leny, lenz))
 
-    minxyz = min([lenx, leny, lenz])
-
     # Rotate about x-axis
-    for y in range(min([leny, lenz])):
-        if rotations[0]==1:
-            dat_new[0:minxyz, 0:minxyz, y] = dat[0:minxyz, y, 0:minxyz]
-        elif rotations[0]==-1:
-            dat_new[0:minxyz, 0:minxyz, leny-1-y] = dat[0:minxyz, y, 0:minxyz]
+    if rotations[0] != 0:
+        for y in range(lenx):
+            if rotations[0]==1:
+                dat_new[:, :, y] = dat[:, y, :]
+            elif rotations[0]==-1:
+                dat_new[:, :, leny-1-y] = dat[:, y, :]
 
     # Rotate about y-axis
-    for z in range(min([lenx, lenz])):
-        if rotations[1]==1:
-            dat_new[z, 0:minxyz, 0:minxyz] = dat[0:minxyz, 0:minxyz, z]
-        elif rotations[1]==-1:
-            dat_new[lenz-1-z, 0:minxyz, 0:minxyz] = dat[0:minxyz, 0:minxyz, z]
+    if rotations[1] != 0:
+        for z in range(lenx):
+            if rotations[1]==1:
+                dat_new[z, :, :] = dat[:, :, z]
+            elif rotations[1]==-1:
+                dat_new[lenz-1-z, :, :] = dat[:, :, z]
 
     # Rotate about z-axis
-    for x in range(min([lenx, leny])):
-        if rotations[1]==1:
-            dat_new[0:minxyz, x, 0:minxyz] = dat[x, 0:minxyz, 0:minxyz]
-        elif rotations[1]==-1:
-            dat_new[0:minxyz, lenx-1-x, 0:minxyz] = dat[x, 0:minxyz, 0:minxyz]
+    if rotations[2] != 0:
+        for x in range(lenx):
+            if rotations[1]==1:
+                dat_new[:, x, :] = dat[x, :, :]
+            elif rotations[1]==-1:
+                dat_new[:, lenx-1-x, :] = dat[x, :, :]
 
     # Save output
     out_file = 'reorient_' + os.path.basename(input_file)
@@ -167,10 +185,21 @@ def rotate90(input_file, rotations=[1,0,0],
 #------------------------------------------------------------------------------
 if __name__ == "__main__":
 
+    import os
     from mindboggle.utils.matrix_operations import rotate90
 
-    input_file = 'PhStr_CU_20121130.nii.gz'
-    rotate90(input_file, rotations=[1,0,0],
-             use_matrix=False, use_header=False)
+    input_dir = '/drop/EMBARC/Data/Phantom_DTI_Updated20121214'
+    input_files = ['isometric_PhDif_1stvol_CU_20120607.nii.gz',
+                   'isometric_PhDif_1stvol_CU_20120711.nii.gz',
+                   'isometric_PhDif_1stvol_CU_20120915.nii.gz',
+                   'isometric_PhDif_1stvol_CU_20121206.nii.gz',
+                   'isometric_PhDif_1stvol_UM_20120803.nii.gz',
+                   'isometric_PhDif_1stvol_UM_20121105.nii.gz',
+                   'isometric_PhDif_1stvol_UM_20121204.nii.gz']
+
+    for input_file in input_files:
+        input_file = os.path.join(input_dir, input_file)
+        rotate90(input_file, rotations=[1,0,0],
+                 use_matrix=False, use_header=False)
 
 
