619f5f52bae040a3851bdfabdb54828ee667f63d
Arno Klein <arno@binarybottle.com>
2013-03-12 21:31:14 -0400

    Add plot_scalar_histogram(), subfolds to pipeline. Switch computers.

--- mindboggle/utils/mesh.py
+++ mindboggle/utils/mesh.py
@@ -16,7 +16,7 @@ Copyright 2013,  Mindboggle team (http://mindboggle.info), Apache v2.0 License
 #------------------------------------------------------------------------------
 def plot_vtk(vtk_file):
     """
-    Use mayavi2 to visualize .vtk surface mesh data.
+    Use mayavi2 to visualize VTK surface mesh data.
 
     Inputs
     ------
@@ -25,9 +25,37 @@ def plot_vtk(vtk_file):
     """
     import os, sys
 
-    args = ['mayavi2', '-d' , vtk_file, '-m Surface &']
+    args = ['mayavi2', '-d', vtk_file, '-m Surface &']
     c = ' '.join(args)
     print(c); os.system(c)
+
+#------------------------------------------------------------------------------
+# Plot histogram of VTK surface mesh scalar values
+#------------------------------------------------------------------------------
+def plot_scalar_histogram(vtk_file, nbins=100):
+    """
+    Plot histogram of VTK surface mesh scalar values.
+
+    Examples
+    --------
+    >>> import os
+    >>> from mindboggle.utils.mesh import plot_scalar_histogram
+    >>> path = os.environ['MINDBOGGLE_DATA']
+    >>> vtk_file = os.path.join(path, 'arno', 'measures', 'lh.pial.depth.vtk')
+    >>> plot_scalar_histogram(vtk_file, nbins=500)
+
+    """
+    import matplotlib.pyplot as plt
+    from mindboggle.utils.io_vtk import read_scalars
+
+    # Load values:
+    values, name = read_scalars(vtk_file)
+
+    # Histogram:
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.hist(values, nbins, normed=1, facecolor='gray', alpha=0.1)
+    plt.show()
 
 #------------------------------------------------------------------------------
 # Apply affine transform to the points of a VTK surface mesh
