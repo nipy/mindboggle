95014ec2d86fc8fa7315e04b71e9f90a813008a4
Arno Klein <arno@binarybottle.com>
2013-03-27 02:57:32 -0400

    Rewrite Yrjo Hame's likelihood computing Matlab code as Python code with Mindboggle-ready functions.
    Tests appear to pass for training:
    'Learn distributions from training data (different surface meshes).'
    
    Debugging needed for application to a new brain:
    'Compute likelihood values for any surface mesh based on learned parameters.'

--- mindboggle/features/parameterizeDepthAndCurvature.m
+++
@@ -1,7 +0,0 @@
-function [aDepth, aCurv] = parameterizeDepthAndCurvature(norm_depths,curvature,label_borders_sulci,Sulci)
-
-x = 0:0.02:1;
-aDepth = learnDistributionParameters(norm_depths,Sulci,label_borders_sulci,x);
-
-x = -1:0.02:1;
-aCurv = learnDistributionParameters(curvature,Sulci,label_borders_sulci,x);
