99d3311771a6ee87419b956eb1d759f3948f0136
= <=>
2013-04-24 07:10:06 -0400

    Redistribute some of mesh.py and io_vtk.py functions.
    Former-commit-id: c41f0c4cbac6e655a579a80411aab9ecc35b2a48

--- mindboggle/utils/io_vtk.py
+++ mindboggle/utils/io_vtk.py
@@ -952,5 +952,147 @@ def scalars_checker(scalars, scalar_names):
 
     return scalars, scalar_names
 
+#------------------------------------------------------------------------------
+# Apply affine transform to the points of a VTK surface mesh
+#------------------------------------------------------------------------------
+def read_itk_transform(affine_transform_file):
+    """
+    Read ITK transform file and output transform array.
+
+    ..ITK affine transform file format ::
+
+        #Insight Transform File V1.0
+        #Transform 0
+        Transform: MatrixOffsetTransformBase_double_3_3
+        Parameters: 0.90768 0.043529 0.0128917 -0.0454455 0.868937 0.406098 \
+                    0.0179439 -0.430013 0.783074 -0.794889 -18.3346 -3.14767
+        FixedParameters: -0.60936 21.1593 10.6148
+
+    Parameters
+    ----------
+    affine_transform_file : string
+        name of ITK affine transform file
+
+    Returns
+    -------
+    affine_transform : numpy array
+        4x4 affine transform matrix
+    fixed_parameters : numpy array
+        FixedParameters vector
+
+    Examples
+    --------
+    >>> import os
+    >>> from mindboggle.utils.io_vtk import read_itk_transform
+    >>> path = os.environ['MINDBOGGLE_DATA']
+    >>> affine_transform_file = os.path.join(path, 'arno', 'mri',
+    >>>                             't1weighted_brain.MNI152Affine.txt')
+    >>> read_itk_transform(affine_transform_file)
+        (array([[  9.07680e-01,   4.35290e-02,   1.28917e-02, -7.94889e-01],
+               [ -4.54455e-02,   8.68937e-01,   4.06098e-01, -1.83346e+01],
+               [  1.79439e-02,  -4.30013e-01,   7.83074e-01, -3.14767e+00],
+               [  0.00000e+00,   0.00000e+00,   0.00000e+00, 1.00000e+00]]),
+         [-0.60936, 21.1593, 10.6148])
+
+    """
+    import numpy as np
+
+    affine_transform = np.eye(4)
+
+    # Read ITK transform file
+    fid = open(affine_transform_file, 'r')
+    affine_lines = fid.readlines()
+
+    transform = affine_lines[3]
+    transform = transform.split()
+    transform = [np.float(x) for x in transform[1::]]
+    transform = np.reshape(transform, (4,3))
+    linear_transform = transform[0:3,:]
+    translation = transform[3,:]
+    affine_transform[0:3,0:3] = linear_transform
+    affine_transform[0:3,3] = translation
+
+    fixed_parameters = affine_lines[4]
+    fixed_parameters = fixed_parameters.split()
+    fixed_parameters = [np.float(x) for x in fixed_parameters[1::]]
+
+    return affine_transform, fixed_parameters
+
+def apply_affine_transform(transform_file, vtk_file):
+    """
+    Transform coordinates using an affine matrix.
+
+    Parameters
+    ----------
+    transform file : string
+        name of ITK affine transform file
+    vtk_file : string
+        name of VTK file containing point coordinate data
+
+    Returns
+    -------
+    affined_points : list of lists of floats
+        transformed coordinates
+    output_file : string
+        name of VTK file containing transformed point data
+
+    Examples
+    --------
+    >>> import os
+    >>> from mindboggle.utils.io_vtk import apply_affine_transform, plot_vtk
+    >>> path = os.environ['MINDBOGGLE_DATA']
+    >>> transform_file = os.path.join(path, 'arno', 'mri',
+    >>>                               't1weighted_brain.MNI152Affine.txt')
+    >>> vtk_file = os.path.join(path, 'arno', 'shapes', 'lh.pial.depth.vtk')
+    >>> apply_affine_transform(transform_file, vtk_file)
+    >>> # View
+    >>> plot_vtk('affine_lh.pial.depth.vtk')
+
+
+    """
+    import os
+    import numpy as np
+
+    from mindboggle.utils.io_vtk import read_vtk, write_vtk, read_itk_transform
+
+    print("\n\n\nUNDER CONSTRUCTION!!!\n\n\n")
+
+    # Read ITK affine transform file
+#    transform, fixed_parameters = read_itk_transform(transform_file)
+
+    import nibabel as nb
+    subject_path = '/Applications/freesurfer/subjects/Twins-2-1'
+    native_volume_mgz = subject_path + '/mri/orig/001.mgz'
+    conformed_volume_mgz = subject_path + '/mri/brain.mgz'
+    M = np.array([[-1,0,0,128],
+                  [0,0,1,-128],
+                  [0,-1,0,128],
+                  [0,0,0,1]],dtype=float)
+    native = nb.freesurfer.load(native_volume_mgz)
+    conformed = nb.freesurfer.load(conformed_volume_mgz)
+    affine_native = native.get_affine()
+    affine_conformed = conformed.get_affine()
+    transform = np.dot(affine_conformed, np.linalg.inv(M))
+
+    # Read VTK file
+    faces, lines, indices, points, npoints, scalars, name, input_vtk = read_vtk(vtk_file)
+
+    # Transform points
+    points = np.array(points)
+#    points += fixed_parameters
+
+    points = np.concatenate((points, np.ones((np.shape(points)[0],1))), axis=1)
+    affine_points = np.transpose(np.dot(transform, np.transpose(points)))[:,0:3]
+#    affine_points -= fixed_parameters
+#    #affine_points += [0,256,0]
+
+    # Output transformed VTK file
+    output_file = os.path.join(os.getcwd(), 'affine_' + os.path.basename(vtk_file))
+
+    # Write VTK file
+    write_vtk(output_file, affine_points.tolist(), indices, lines, faces, scalars, name)
+
+    return affine_points, output_file
+
 
 #if __name__ == "__main__" :
