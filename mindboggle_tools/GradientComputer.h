45db8cfcde5a18cc13226651222aeb45c68894d3
Joachim Giard <joachim.giard@uclouvain.be>
2013-04-26 17:40:59 +0200

    added a gradient computer
    Former-commit-id: 2409ebf1ef28316d8ab34c1080d4c76d67cd7911

---
+++ mindboggle_tools/GradientComputer.h
@@ -0,0 +1,45 @@
+/* ********************************************************************
+ * Gradient Computer
+ *
+ * Copyright 2013 Joachim Giard
+ * Université catholique de Louvain, Belgium
+ * Apache License, Version 2.0
+ *
+ * Class containing various methods to compute gradients of a scalar
+ * field on a mesh
+ *
+ * *******************************************************************/
+
+#ifndef GRADIENTCOMPUTER_H
+#define GRADIENTCOMPUTER_H
+
+#include "vtkPolyData.h"
+
+class GradientComputer
+{
+public:
+    // Constructor.
+    GradientComputer(vtkPolyData* mesh);
+
+    // destructor
+    ~GradientComputer();
+
+    void WriteIntoFile(char* fileName);
+
+    void ComputeGradient();
+    void ComputeGradient(vtkDataArray* scalar);
+
+private:
+
+    void ProjectPointToPlane(double* pointToProject, double* inPlanePoint, double* normal, double* projectedPoint);
+    void GetPointNeighbors(vtkIdType id, vtkIdList *neighbors);
+
+    vtkDoubleArray* m_pointsArea;
+    vtkPolyData* m_mesh;
+    vtkDataArray* m_normals;
+    vtkDoubleArray* m_gradientValueArray;
+    vtkDoubleArray* m_steepestDescentArray;
+
+};
+
+#endif // GRADIENTCOMPUTER_H
