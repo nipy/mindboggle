75ede40ef0c046956a79984f29bc0ecf6dc01fa2
= <=>
2013-05-01 23:24:00 -0400

    Segment fundus skeletons by sulcus labels to obtain sulcus-assigned fundi.
    Former-commit-id: d838463853848cf7160ab86169b06846bbafec32

--- mindboggle/features/fundi.py
+++ mindboggle/features/fundi.py
@@ -12,26 +12,33 @@ Copyright 2013,  Mindboggle team (http://mindboggle.info), Apache v2.0 License
 #=============================================================================
 # Extract fundi
 #=============================================================================
-def extract_fundi(folds_or_file, depth_file, likelihoods_or_file,
-                  smooth_skeleton=False, save_file=False):
+def extract_fundi(folds, sulci, likelihoods, rescaled_depth_file,
+                  depth_file, smooth_skeleton=False, save_file=False):
     """
     Extract fundi from folds.
 
-    A fundus is a connected set of high-likelihood vertices in a surface mesh.
+    A fundus is a branching curve that runs along the deepest and most
+    highly curved portions of a sulcus fold.
 
     Steps ::
 
         1. Find fundus endpoints using find_outer_anchors().
         2. Connect fundus endpoints using connect_points_erosion().
+        3. FIX: Optionally smooth fundi using connect_points_hmmf().
+        4. Segment fundi by sulcus definitions.
 
     Parameters
     ----------
-    folds_or_file : list or string
-        fold number for each vertex or name of VTK file containing folds scalars
-    depth_file :  string
+    folds : list of integers
+        fold number for each vertex
+    sulci : list of integers
+        sulcus number for each vertex
+    likelihoods : list of integers
+        fundus likelihood value for each vertex
+    rescaled_depth_file :  string
         surface mesh file in VTK format with scalar rescaled depth values
-    likelihoods_or_file : list or string
-        fundus likelihood values or name of VTK file with the scalar values
+    depth_file :  string
+        surface mesh file in VTK format with (complete) scalar depth values
     smooth_skeleton : Boolean
         smooth skeleton?
     save_file : Boolean
@@ -43,8 +50,6 @@ def extract_fundi(folds_or_file, depth_file, likelihoods_or_file,
         fundus numbers for all vertices (-1 for non-fundus vertices)
     n_fundi :  integer
         number of fundi
-    fundus_endpoints : list of integers
-        indices to fundus anchor vertices
     fundi_file : string (if save_file)
         name of output VTK file with fundus numbers (-1 for non-fundus vertices)
 
@@ -57,8 +62,12 @@ def extract_fundi(folds_or_file, depth_file, likelihoods_or_file,
     >>> from mindboggle.utils.plots import plot_vtk
     >>> from mindboggle.features.fundi import extract_fundi
     >>> path = os.environ['MINDBOGGLE_DATA']
+    >>> sulci_file = os.path.join(path, 'arno', 'features', 'sulci.vtk')
+    >>> sulci, name = read_scalars(sulci_file, True, True)
+    >>> likelihoods_file = os.path.join(path, 'arno', 'features', 'likelihoods.vtk')
+    >>> likelihoods, name = read_scalars(likelihoods_file, True, True)
+    >>> rescaled_depth_file = os.path.join(path, 'arno', 'shapes', 'depth_rescaled.vtk')
     >>> depth_file = os.path.join(path, 'arno', 'shapes', 'lh.pial.depth.vtk')
-    >>> likelihoods_or_file = os.path.join(path, 'arno', 'features', 'likelihoods.vtk')
     >>> single_fold = True
     >>> if single_fold:
     >>>     folds_file = os.path.join(path, 'arno', 'features', 'folds.vtk')
@@ -67,18 +76,18 @@ def extract_fundi(folds_or_file, depth_file, likelihoods_or_file,
     >>>     folds[folds != fold_number] = -1
     >>> else:
     >>>     folds_file = os.path.join(path, 'arno', 'features', 'folds.vtk')
-    >>>     folds, name = read_scalars(folds_file, return_first=True, return_array=True)
+    >>>     folds, name = read_scalars(folds_file, True, True)
     >>> #
     >>> smooth_skeleton = False
-    >>> fundi, n_fundi, endpoints, fundi_file = extract_fundi(folds, depth_file,
-    >>>     likelihoods_or_file, smooth_skeleton, save_file=True)
+    >>> save_file = True
+    >>> fundi, n_fundi, fundi_file = extract_fundi(folds, sulci, likelihoods,
+    >>>     rescaled_depth_file, depth_file, smooth_skeleton, save_file)
     >>> #
     >>> # View:
     >>> plot_vtk(fundi_file)
 
     """
     import os
-    import sys
     import numpy as np
     from time import time
 
@@ -88,52 +97,34 @@ def extract_fundi(folds_or_file, depth_file, likelihoods_or_file,
     from mindboggle.utils.morph import dilate
     from mindboggle.utils.io_vtk import read_scalars, rewrite_scalars
 
-    # Load fold numbers:
-    if isinstance(folds_or_file, str):
-        folds, name = read_scalars(folds_or_file)
-    elif isinstance(folds_or_file, list):
-        folds = folds_or_file
-    elif isinstance(folds_or_file, np.ndarray):
-        folds = folds_or_file.tolist()
-    else:
-        sys.error('folds_or_file is not a string, list, or array.')
-
-    # Load likelihood values:
-    if isinstance(likelihoods_or_file, str):
-        likelihoods, name = read_scalars(likelihoods_or_file, True, True)
-    elif isinstance(likelihoods_or_file, list):
-        likelihoods = np.array(likelihoods_or_file)
-    elif isinstance(likelihoods_or_file, np.ndarray):
-        likelihoods = likelihoods_or_file
-
-    # Initialize variables:
-    t1 = time()
-    count = 0
+    # Load depths and neighbors:
     neighbor_lists = find_neighbors_from_file(depth_file)
-    depths, name = read_scalars(depth_file)
-    npoints = len(folds)
-    Z = np.zeros(npoints)
-    fundi = -1 * np.ones(npoints)
-    fundus_endpoints = []
-    nedges = 2
+    depths, name = read_scalars(rescaled_depth_file)
 
-    # For each fold region...
+    #-------------------------------------------------------------------------
+    # Loop through folds:
+    #-------------------------------------------------------------------------
+    t1 = time()
+    skeletons = []
     unique_fold_IDs = [x for x in np.unique(folds) if x != -1]
+
     print("Extract a fundus from each of {0} regions...".
           format(len(unique_fold_IDs)))
+
     for fold_ID in unique_fold_IDs:
         indices_fold = [i for i,x in enumerate(folds) if x == fold_ID]
         if indices_fold:
             print('  Fold {0}:'.format(int(fold_ID)))
 
+            #-----------------------------------------------------------------
             # Find fundus endpoints on the boundary of the surface region:
+            #-----------------------------------------------------------------
             endpoints, endtracks = find_outer_anchors(indices_fold,
                 neighbor_lists, likelihoods, depths, min_edges=5)
 
             #-----------------------------------------------------------------
-            # Connect endpoints to each other:
+            # Connect endpoints to create skeleton:
             #-----------------------------------------------------------------
-            # Connect endpoints via a skeleton:
             B = -1 * np.ones(len(folds))
             B[indices_fold] = 1
             skeleton = connect_points_erosion(B, endpoints, neighbor_lists,
@@ -146,19 +137,26 @@ def extract_fundi(folds_or_file, depth_file, likelihoods_or_file,
                 # Smooth skeleton:
                 #-------------------------------------------------------------
                 if smooth_skeleton:
+                    nedges = 2
                     padded_skeleton = dilate(skeleton, nedges, neighbor_lists)
                     skeleton = connect_points_hmmf(endpoints, padded_skeleton,
                                                    likelihoods, neighbor_lists)
 
-                fundi[skeleton] = fold_ID
-                count += 1
+                skeletons.extend(skeleton)
 
-    n_fundi = count
+    #-------------------------------------------------------------------------
+    # Create fundi by segmenting skeletons with overlapping sulcus labels:
+    #-------------------------------------------------------------------------
+    fundi = -1 * np.ones(len(folds))
+    indices = [x for x in skeletons if sulci[x] != -1]
+    fundi[indices] = sulci[indices]
+
+    n_fundi = len([x for x in np.unique(fundi) if x != -1])
     print('  ...Extracted {0} fundi ({1:.2f} seconds)'.
           format(n_fundi, time() - t1))
 
     #-------------------------------------------------------------------------
-    # Return fundi, number of fundi, fundus points, and file name:
+    # Return fundi, number of fundi, and file name:
     #-------------------------------------------------------------------------
     fundi = fundi.tolist()
 
@@ -168,7 +166,7 @@ def extract_fundi(folds_or_file, depth_file, likelihoods_or_file,
     else:
         fundi_file = None
 
-    return fundi, n_fundi, fundus_endpoints, fundi_file
+    return fundi, n_fundi, fundi_file
 
 
 # Example
@@ -181,9 +179,13 @@ if __name__ == "__main__" :
     from mindboggle.utils.plots import plot_vtk
     from mindboggle.features.fundi import extract_fundi
     path = os.environ['MINDBOGGLE_DATA']
+    sulci_file = os.path.join(path, 'arno', 'features', 'sulci.vtk')
+    sulci, name = read_scalars(sulci_file, True, True)
+    likelihoods_file = os.path.join(path, 'arno', 'features', 'likelihoods.vtk')
+    likelihoods, name = read_scalars(likelihoods_file, True, True)
+    rescaled_depth_file = os.path.join(path, 'arno', 'shapes', 'depth_rescaled.vtk')
     depth_file = os.path.join(path, 'arno', 'shapes', 'lh.pial.depth.vtk')
-    likelihoods_or_file = os.path.join(path, 'arno', 'features', 'likelihoods.vtk')
-    single_fold = True
+    single_fold = False
     if single_fold:
         folds_file = os.path.join(path, 'arno', 'features', 'folds.vtk')
         folds, name = read_scalars(folds_file, True, True)
@@ -191,11 +193,12 @@ if __name__ == "__main__" :
         folds[folds != fold_number] = -1
     else:
         folds_file = os.path.join(path, 'arno', 'features', 'folds.vtk')
-        folds, name = read_scalars(folds_file, return_first=True, return_array=True)
+        folds, name = read_scalars(folds_file, True, True)
     #
     smooth_skeleton = False
-    fundi, n_fundi, endpoints, fundi_file = extract_fundi(folds, depth_file,
-        likelihoods_or_file, smooth_skeleton, save_file=True)
+    save_file = True
+    fundi, n_fundi, fundi_file = extract_fundi(folds, sulci, likelihoods,
+        rescaled_depth_file, depth_file, smooth_skeleton, save_file)
     #
     # View:
     plot_vtk(fundi_file)
