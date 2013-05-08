98e3ca5448f9fb56d93f11810c28a61eff31077e
= <=>
2013-05-08 11:47:51 -0400

    Fix examples, try white/gray surface
    Former-commit-id: f9f34c5f5b2ea6ca2e73883d1559f571612831ac

--- mindboggle/utils/plots.py
+++ mindboggle/utils/plots.py
@@ -44,7 +44,7 @@ def plot_scalar_histogram(vtk_file, nbins=100):
     Examples
     --------
     >>> import os
-    >>> from mindboggle.utils.mesh import plot_scalar_histogram
+    >>> from mindboggle.utils.plots import plot_scalar_histogram
     >>> path = os.environ['MINDBOGGLE_DATA']
     >>> vtk_file = os.path.join(path, 'arno', 'shapes', 'lh.pial.depth.vtk')
     >>> plot_scalar_histogram(vtk_file, nbins=500)
