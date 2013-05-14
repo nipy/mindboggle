7d56d6972ded71f988d330dca69260807ba65b5a
Arno Klein <arno@binarybottle.com>
2013-05-14 09:55:18 -0400

    Add ability to plot_vtk() to filter mesh with the non -1 values from a second (same-size) mesh.
    Former-commit-id: 9adccb92b241e12ebcc05dbc5fb63026ec37ff79

--- mindboggle/utils/plots.py
+++ mindboggle/utils/plots.py
@@ -12,7 +12,7 @@ Copyright 2013,  Mindboggle team (http://mindboggle.info), Apache v2.0 License
 #------------------------------------------------------------------------------
 # Plot VTK surface mesh
 #------------------------------------------------------------------------------
-def plot_vtk(vtk_file):
+def plot_vtk(vtk_file, mask_file='', masked_output=''):
     """
     Use mayavi2 to visualize VTK surface mesh data.
 
@@ -20,18 +20,44 @@ def plot_vtk(vtk_file):
     ------
     vtk_file : string
         name of VTK surface mesh file
+
+    Examples
+    --------
+    >>> import os
+    >>> from mindboggle.utils.plots import plot_vtk
+    >>> path = os.environ['MINDBOGGLE_DATA']
+    >>> vtk_file = os.path.join(path, 'arno', 'shapes', 'lh.pial.mean_curvature.vtk')
+    >>> mask_file = os.path.join(path, 'arno', 'features', 'folds.vtk')
+    >>> masked_output = ''
+    >>> plot_vtk(vtk_file, mask_file, masked_output)
+
     """
     import os
 #    import subprocess
 
-    cmd = ["mayavi2", "-d", vtk_file, "-m", "Surface"]
-    print(' '.join(cmd))
+    # Filter mesh with the non -1 values from a second (same-size) mesh:
+    if mask_file:
+
+        from mindboggle.utils.io_vtk import read_scalars, rewrite_scalars
+
+        scalars, name = read_scalars(vtk_file)
+        mask, name = read_scalars(mask_file)
+        if not masked_output:
+            masked_output = 'temp.vtk'
+        rewrite_scalars(vtk_file, masked_output, scalars, 'masked', mask)
+
+        cmd = ["mayavi2", "-d", masked_output, "-m", "Surface"]
+
+    else:
+
+        cmd = ["mayavi2", "-d", vtk_file, "-m", "Surface"]
 
 # Note: subprocess won't allow me to put the command in the background:
 #    p = subprocess.Popen(cmd)
 #    p.communicate()
-    c = ' '.join(cmd) + ' &'
-    print(c); os.system(c)
+    cmd = ' '.join(cmd) + ' &'
+    print(cmd)
+    os.system(cmd)
 
 
 #------------------------------------------------------------------------------
