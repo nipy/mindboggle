3b4c2a0de2796e9b6593deeedd1983e41b937596
Arno Klein <arno@binarybottle.com>
2013-07-26 10:33:55 -0700

    Remove sphinx extension form conf.py
    Former-commit-id: b77efc424d50ca1d1d0a578b6a2a0cfe9e8a6a43

--- doc/tools/build_modref_templates.py
+++ doc/tools/build_modref_templates.py
@@ -19,8 +19,8 @@ if __name__ == '__main__':
                                         r'\.pgk_info$',
                                         ]
     # XXX: Avoid mindboggle.label.rebound while developing
-    docwriter.module_skip_patterns += [r'\.label\.rebound',
-                                        ]
+    #docwriter.module_skip_patterns += [r'\.label\.rebound',
+    #                                    ]
     docwriter.write_api_docs(outdir)
     docwriter.write_index(outdir, 'gen', relative_to='api')
     print '%d files written' % len(docwriter.written_modules)
