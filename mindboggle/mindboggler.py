bff73c76a3963adc6893d8ce7adbb2ec2fe83f28
Arno Klein <arno@binarybottle.com>
2013-08-07 18:24:56 -0400

    Update mindboggler.py calls at bottom of pipeline.

--- mindboggle/mindboggler.py
+++ mindboggle/mindboggler.py
@@ -1900,29 +1900,33 @@ import os
 from mindboggle.utils.io_table import read_columns
 from mindboggle.utils.utils import execute
 
-out_path = '/homedir/Data/Mindboggle101_mindboggled/'
-atlas_list_file = '/homedir/Data/Brains/Mindboggle101/code/mindboggle101_atlases.txt'
+out_path = '/homedir/Mindboggle101/mindboggled/'
+atlas_list_file = '/homedir/Mindboggle101/code/mindboggle101_atlases.txt'
 atlas_list = read_columns(atlas_list_file, 1)[0]
-cmd = ' '.join(['python mindboggler.py', '-o', out_path, atlas])
 
 for atlas in atlas_list:
     if 'MMRR-21-' in atlas:
+        cmd = ' '.join(['python mindboggler.py', '-o', out_path, atlas])
         execute(cmd)
 
 for atlas in atlas_list:
     if 'NKI-RS-' in atlas:
+        cmd = ' '.join(['python mindboggler.py', '-o', out_path, atlas])
         execute(cmd)
 
 for atlas in atlas_list:
     if 'NKI-TRT-' in atlas:
+        cmd = ' '.join(['python mindboggler.py', '-o', out_path, atlas])
         execute(cmd)
 
 for atlas in atlas_list:
     if 'OASIS-TRT-20' in atlas:
+        cmd = ' '.join(['python mindboggler.py', '-o', out_path, atlas])
         execute(cmd)
 
 for atlas in atlas_list:
     if 'HLN-' in atlas or 'Twins-' in atlas or 'Colin' in atlas or 'After' in atlas or '3T7T' in atlas:
+        cmd = ' '.join(['python mindboggler.py', '-o', out_path, atlas])
         execute(cmd)
 
 """
