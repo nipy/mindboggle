062a9b59a160dfc484c41ed6d6a7e68fedf5ad83
ohinds <ohinds@gmail.com>
2013-09-10 13:39:45 -0400

    Filled out implementation of shortest path, needs testing.
    Former-commit-id: 3531ac088daba414f9e904b58bbc66d78c4bae48

--- mindboggle/labels/propagate_fundus_lines.py
+++ mindboggle/labels/propagate_fundus_lines.py
@@ -9,17 +9,21 @@ Copyright 2013,  Mindboggle team (http://mindboggle.info), Apache v2.0 License
 
 """
 
-def propagate_fundus_lines(fundus_lines_file):
+def propagate_fundus_lines(surf_file, fundus_lines_file):
     from mindboggle.utils.io_vtk import read_vtk, read_scalars, write_vtk
     from mindboggle.utils.mesh import find_neighbors
 
     faces, _, _, points, num_points, fundus_lines, _, _ = read_vtk(
-        fundus_lines_file, return_first=True, return_array=True)
+        surf_file, return_first=True, return_array=True)
+
+    fundus_lines, _ = read_scalars(fundus_lines_file)
 
     neighbor_lists = find_neighbors(faces, num_points)
 
     endpoints = _find_fundus_line_endpoints(fundus_lines, neighbor_lists)
 
+    closed_fundus_lines = _close_fundus_lines(points, fundus_lines,
+                                              neighbor_lists, endpoints)
 
     import ipdb; ipdb.set_trace()
 
@@ -39,32 +43,65 @@ def _find_fundus_line_endpoints(fundus_lines, neighbor_lists):
 
     return endpoints
 
-def _find_path_to_nearest_endpoint(points, faces, fundus_lines,
-                                   neighbor_lists,
-                                   endpoint_indices):
+def _close_fundus_lines(points, fundus_lines,
+                        neighbor_lists, endpoint_indices):
+    import numpy as np
     import Queue
 
-    # make a parallel queue and map, keep them synced.
+    import ipdb; ipdb.set_trace()
 
-    closed_fundus_lines = [1 if x >= 0 for x in fundus_lines]
+    closed_fundus_lines = fundus_lines
 
     for endpoint in endpoint_indices:
-        open_set = Queue.PriorityQueue()
-        open_set.put((0, endpoint))
-        closed_set = {}
-        while not open_set.empty():
-            current = open_set.get()
-            if current[1] in endpoint_indices:
+        open_queue = Queue.PriorityQueue()
+        distance_map = {}
+        predicessor_map = {}
+        closed_set = set()
+
+        cur = [0, endpoint]
+        open_queue.put(cur)
+        distance_map[endpoint] = cur
+        predicessor_map[endpoint] = None
+        first_point = None
+
+        while not open_queue.empty():
+            dist, vertex = open_queue.get()
+            if vertex in endpoint_indices:
+                first_point = predicessor_map[vertex]
                 break
 
-            for neighbor in neighbor_lists(current):
-                if neighbor not in closed_set.keys():
+            added_any = False
+            for neighbor in neighbor_lists(vertex):
+                if neighbor not in closed_set:
+                    neighbor_dist = np.dot(points[vertex], points[neighbor])
+                    if (neighbor not in distance_map or
+                        distance_map[neighbor][0] > neighbor_dist + dist):
+                        distance_map[neighbor_dist][0] = neighbor_dist + dist
+                        predicessor_map[neighbor] = vertex
+                        added_any = True
 
+            # Hack to force resorting of the priority queue based on
+            # new values
+            if added_any:
+                dummy = [-1, -1]
+                open_queue.put(dummy)
+                open_queue.get()
 
+        # Trace back predicessors
+        if first_point is None:
+            raise RuntimeError("No path found for endpoint %d" % endpoint)
 
+        print "found path from %d to %d" % (endpoint, first_point)
+
+        cur_point = first_point
+        while cur_point != endpoint:
+            closed_fundus_lines[cur_point] = 22
+            cur_point = predicessor_map[cur_point]
+
+    return closed_fundus_lines
 
 def main(argv):
-    propagate_fundus_lines(argv[1])
+    propagate_fundus_lines(argv[1], argv[2])
 
 if __name__ == "__main__":
     import sys
