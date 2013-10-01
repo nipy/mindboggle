c989c2182283aefd4ad0d477935f917c9c05c579
= <arno@binarybottle.com>
2013-10-01 13:58:51 -0400

    Fix orig vs. rawavg in FreeSurfer's mri directory
    Former-commit-id: 419eb9e31f9c890c362c2a0f6595823eb3cdbc2f

--- mindboggle/shapes/thickness.py
+++ mindboggle/shapes/thickness.py
@@ -353,14 +353,15 @@ def run_thickinthehead(subjects, labels, out_dir='', atropos_dir='',
         else:
             aparc = os.path.join(subjects_dir, subject,
                                  'mri', 'aparc+aseg.mgz')
-            rawavg = os.path.join(subjects_dir, subject,
-                                  'mri', 'orig', '001.mgz')
-            if not os.path.exists(rawavg):
-                rawavg = os.path.join(subjects_dir, subject,
-                                      'mri', 'orig', '001.mgz')
-                if not os.path.exists(rawavg):
-                    sys.exit('Could not find ' + rawavg +
+            orig = os.path.join(subjects_dir, subject,
+                                'mri', 'orig', '001.mgz')
+            if not os.path.exists(orig):
+                orig = os.path.join(subjects_dir, subject,
+                                    'mri', 'rawavg.mgz')
+                if not os.path.exists(orig):
+                    sys.exit('Could not find ' + orig +
                              ' for subject ' + subject)
+
             cmd = ['mri_vol2vol --mov', aparc, '--targ', rawavg,
                    '--interp nearest', '--regheader --o', labeled_file]
             execute(cmd)
