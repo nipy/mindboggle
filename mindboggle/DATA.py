e135dcf2a2f52022e3adbc9a08cc44a9644cbc01
= <arno@binarybottle.com>
2013-10-05 07:53:09 -0400

    Update DATA.py and LABELS.py (formerly data.py and protocol.py)
    Former-commit-id: 82c8724bea34ff572a31e723b2402d2f130bbddc

--- mindboggle/DATA.py
+++ mindboggle/DATA.py
@@ -41,9 +41,13 @@ def hashes_url():
     #-------------------------------------------------------------------------
     # Atlases and templates:
     #-------------------------------------------------------------------------
+    hashes['OASIS-30_Atropos_template.nii.gz'] = 'f95dbe37ab40e8ad59c1b1eabc7f230c'
     hashes['OASIS-30_Atropos_template_in_MNI152.nii.gz'] = '2efbe9c0755f4aa4506ad78e60ec319f'
-    hashes['OASIS-TRT-20_jointfusion_DKT31_CMA_labels_in_MNI152.nii.gz'] = '9a35811682293871e2acb9ff4e2fc45d'
-    hashes['OASIS-TRT-20_jointfusion_DKT31_CMA_label_probabilities_in_MNI152.nii.gz'] = 'a491432f5cbc07a1ec1b8122da78ff00'
+    hashes['OASIS-30_Atropos_template_to_MNI152_affine.txt'] = 'f36e3d5d99f7c4a9bb70e2494ed7340b'
+    hashes['OASIS-TRT-20_jointfusion_DKT31_CMA_labels_in_OASIS-30.nii.gz'] = '706dd2515b111b8eb9d7a7a0d76e833a'
+    hashes['OASIS-TRT-20_jointfusion_DKT31_CMA_labels_in_MNI152.nii.gz'] = 'f0b48c3d717a27c022cd62f1f5c18e8f'
+    hashes['OASIS-TRT-20_jointfusion_DKT31_CMA_label_probabilities_in_OASIS-30.nii.gz'] = 'a27a66e8c3955ed2b3833f8dae9a637b'
+    hashes['OASIS-TRT-20_jointfusion_DKT31_CMA_label_probabilities_in_MNI152.nii.gz'] = '529af0aa3254f2d496dc902cb0b93aa4'
     hashes['MNI152_T1_1mm_brain.nii.gz'] = '7c47f203858f57c50ee93bcf7b0662fc'
     hashes['lh.DKTatlas40.gcs'] = 'b28a600a713c269f4c561f66f64337b2'
     hashes['rh.DKTatlas40.gcs'] = '50150021c84b0e829aed3a68709404d7'
