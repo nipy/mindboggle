5e483d23731d06ae583c61df015b788cda201fa1
= <arno@binarybottle.com>
2013-10-23 21:28:40 -0400

    Update functions to output distinctive file names that don't clash when output by nipype.
    Former-commit-id: ed9730df7e67a517420b8e0eb287d0164d2e16bf

--- mindboggle/labels/relabel.py
+++ mindboggle/labels/relabel.py
@@ -341,8 +341,8 @@ def overwrite_volume_labels(source, target, output_file='', ignore_labels=[0],
     import nibabel as nb
 
     if not output_file:
-        output_file = os.path.join(os.getcwd(), 'overwrite_volume_labels.nii.gz')
-
+        output_file = os.path.join(os.getcwd(), os.path.basename(source) +
+                                   '_to_' + os.path.basename(target))
     # Load labeled image volumes:
     vol_source = nb.load(source)
     vol_target = nb.load(target)
