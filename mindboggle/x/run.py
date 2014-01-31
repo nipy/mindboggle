0d6be4dd4f7b3f757bc3036ce3c7725f95c310e3
Arno Klein <arno@binarybottle.com>
2014-01-30 21:40:02 -0800

    Add all Mindboggle-101 data to run.py
    Former-commit-id: fc69aaddd5b6fc20bf00245bd6d4ce61f52bef96

--- mindboggle/x/run.py
+++ mindboggle/x/run.py
@@ -9,7 +9,18 @@ ants_dir = '/data/Brains/Mindboggle101/antsCorticalThickness' #OASIS-TRT-20_volu
 for i,name in enumerate(names):
     number = numbers[i]
     for n in range(1,number+1):
-        s = 'mindboggle -n 8 {0}-{1} --ants_prefix {2}/{0}_volumes/{0}-{1}/ants --sulci --fundi --vertices --spectra 10 --thickness'.format(name, n, '/data/Brains/Mindboggle101/antsCorticalThickness', 'ants')
-        #s = 'mindboggle -n 8 {0}-{1} --ants_prefix {2}/{0}_volumes/{0}-{1}/ants'.format(name, n, ants_dir)
+        s = 'mindboggle -n 8 {0}-{1} --ants_prefix {2}/{0}_volumes/{0}-{1}/ants --sulci --fundi --vertices --spectra 10 --thickness'.format(name, n, ants_dir, prefix)
+        print(s)
+        os.system(s)
+
+names = ['Afterthought', 'Colin27', 'Twins-2', 'MMRR-3T7T-2', 'HLN-12']
+numbers = [1,1,2,2,12]
+prefix = 'ants'
+ants_dir = '/data/Brains/Mindboggle101/antsCorticalThickness/Extra-18_volumes'
+
+for i,name in enumerate(names):
+    number = numbers[i]
+    for n in range(1,number+1):
+        s = 'mindboggle -n 8 {0}-{1} --ants_prefix {2}/{0}-{1}/ants --sulci --fundi --vertices --spectra 10 --thickness'.format(name, n, ants_dir, prefix)
         print(s)
         os.system(s)
