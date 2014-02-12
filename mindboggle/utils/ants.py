243a489caff5dea1ff36fcae286634228fa8eef8
= <arno@binarybottle.com>
2014-02-12 02:31:16 -0800

    Fix the node repeat bug that leads to the nipype 'stop_on_first_rerun' error.
    TEST: I ran two subjects through mindboggle, s1 and s2, and there were no errors. Specifically, I ran s1, then ran s1 again with stop_on_first_rerun, and no errors. I did the same for s2. However, when I ran s1 again with stop_on_first_rerun, I got an error. When I looked at the nipype working directory, I found that there are five functions that do not have the subject name anywhere in its path.
    
    I found out that all of them are downstream of Fetch_ants_data, which did not specify a subject.
    To fix this, I will have this function take in a subject name as an argument to force nipype to keep each subject's results separate.
    
    
    Former-commit-id: 5abd7040c58ba60ebc6e43abb2df62737ec8376b

--- mindboggle/utils/ants.py
+++ mindboggle/utils/ants.py
@@ -10,7 +10,7 @@ Copyright 2013,  Mindboggle team (http://mindboggle.info), Apache v2.0 License
 """
 
 
-def fetch_ants_data(segmented_file):
+def fetch_ants_data(segmented_file, subject):
     """
     Fetch antsCorticalThickness.sh output.
 
@@ -25,6 +25,8 @@ def fetch_ants_data(segmented_file):
     ----------
     segmented_file : string
         full path to a subject's antsCorticalThickness.sh segmented file
+    subject : string
+        name of FreeSurfer subject directory (needed for nipype)
 
     Returns
     -------
@@ -45,7 +47,8 @@ def fetch_ants_data(segmented_file):
     --------
     >>> from mindboggle.utils.ants import fetch_ants_data
     >>> segmented_file = 'ants_subjects/OASIS-TRT-20-1/tmpBrainSegmentation.nii.gz'
-    >>> fetch_ants_data(segmented_file)
+    >>> subject = 'OASIS-TRT-20-1'
+    >>> fetch_ants_data(segmented_file, subject)
 
     """
     import os
