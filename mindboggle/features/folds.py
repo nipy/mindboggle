3aafd6a9b59c06c92b8882045c8176521e11815e
Arno Klein <arno@binarybottle.com>
2014-02-25 21:08:40 -0800

    Enforce integer lists for labels, fundi, and sulci: labels.tolist() -> [int(x) for x in labels]
    Former-commit-id: 2e1d87abc1efeb329e3a1a8e2f87badee89f783f

--- mindboggle/features/folds.py
+++ mindboggle/features/folds.py
@@ -206,6 +206,8 @@ def extract_folds(depth_file, min_fold_size=50, tiny_depth=0.001, save_file=Fals
     else:
         print('  No deep vertices')
 
+    folds = [int(x) for x in folds]
+
     #-------------------------------------------------------------------------
     # Return folds, number of folds, file name
     #-------------------------------------------------------------------------
@@ -219,8 +221,6 @@ def extract_folds(depth_file, min_fold_size=50, tiny_depth=0.001, save_file=Fals
 
     else:
         folds_file = None
-
-    folds = folds.tolist()
 
     return folds, n_folds, depth_threshold, bins, bin_edges, folds_file
 
