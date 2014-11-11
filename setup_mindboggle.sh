74b9b52475877a0453e14e9df7cc7c283f8f484c
= <arno@binarybottle.com>
2014-11-11 00:52:43 -0800

    Write Daniel Clark's install script as setup_mindboggle.sh and add clean up -- NOT TESTED YET.

--- setup_mindboggle.sh
+++ setup_mindboggle.sh
@@ -1,151 +1,146 @@
 #!/bin/bash
-
-# This script provides directions for installing Mindboggle
-# (http://mindboggle.info) and its dependencies.
-# You need a good Internet connection.
-# NOTE: We highly recommend installing Mindboggle as a virtual machine
-#       (http://mindboggle.info/users/INSTALL.html).
+#=============================================================================
+# This script provides directions for installing Mindboggle and dependencies
+# (http://mindboggle.info).  Running it requires a good Internet connection.
+# We highly recommend installing Mindboggle as a virtual machine
+# (http://mindboggle.info/users/INSTALL.html).
 #
-# Copy this file into a setup directory and run::
+# Usage:
+#     ./install_mindboggle <download_dir> <install_dir>
 #
-#    mkdir /Users/arno/mindboggle_setup
-#    cp build_mindboggle.sh /Users/arno/mindboggle_setup/
-#    cd /Users/arno/mindboggle_setup/
-#    bash build_mindboggle.sh
-
-CWD=$(pwd)
-
-#-----------------------------------------------------------------------------
-# Install Anaconda Python distribution:
+# Note:
+#     <download_dir> must already exist and <install_dir> must not exist.
 #
-# Scientific Python environment
-# -----------------------------
-# Easily-installed Python distributions are available such as Anaconda,
-# Canopy, and PythonXY that include components necessary to run Mindboggle:
-# Python, NumPy, SciPy, Networkx.  These distributions also include
-# packages that benefit the user, such as IPython (parallel processing) and
-# Sphinx (generating documentation).
+# Authors:
+#     - Daniel Clark, 2014
+#     - Arno Klein, 2014  (arno@mindboggle.info)  http://binarybottle.com
 #
-# Your system must have basic compilation software (cmake_ and a C++ compiler
-# such as gcc), pip for installing Python libraries, and the Visualization
-# Toolkit (after installing the Anaconda Python distribution).
-#-----------------------------------------------------------------------------
-wget http://repo.continuum.io/miniconda/Miniconda-2.2.2-Linux-x86_64.sh -O miniconda.sh
-chmod +x miniconda.sh
-./miniconda.sh -b
-export PATH=$CWD/anaconda/bin:$PATH
+# Copyright 2014,  Mindboggle team, Apache v2.0 License
+#=============================================================================
 
 #-----------------------------------------------------------------------------
-# Install Nipype and remaining dependencies:
-#
-# Nipype is a software pipeline framework.
-# Nibabel is a medical image read/write library.
+# Assign download and installation path arguments:
 #-----------------------------------------------------------------------------
-conda install --yes pip cmake
-conda install --yes numpy scipy nose traits networkx
-conda install --yes dateutil ipython-notebook matplotlib
-pip install nibabel --use-mirrors
-pip install https://github.com/RDFLib/rdflib/archive/master.zip
-pip install https://github.com/satra/prov/archive/rdf.zip
-pip install https://github.com/nipy/nipype/archive/master.zip
+DL_PREFIX=$1
+INSTALL_PREFIX=$2
 
 #-----------------------------------------------------------------------------
-# Install VTK (for ANTs and Mindboggle):
+# System-wide dependencies:
 #-----------------------------------------------------------------------------
-conda install --yes vtk
-VTK_DIR=$CWD/anaconda/lib/vtk-5.10  # Needed for ANTs
-export PATH=$VTK_DIR:$PATH
+apt-get update
+apt-get install -y g++ git make xorg
 
 #-----------------------------------------------------------------------------
-# Install compiling utilities:
+# Anaconda's miniconda Python distribution for local installs:
 #-----------------------------------------------------------------------------
-sudo apt-get -y update
-sudo apt-get -y install g++
-sudo apt-get -y install make
-sudo apt-get -y install git
-sudo apt-get -y install xorg openbox
+CONDA_DL=${DL_PREFIX}/Miniconda-3.7.0-Linux-x86_64.sh
+wget http://repo.continuum.io/miniconda/Miniconda-3.7.0-Linux-x86_64.sh -P $DL_PREFIX
+chmod +x $CONDA_DL
+$CONDA_DL -b -p $INSTALL_PREFIX
+# Setup PATH
+export PATH=${INSTALL_PREFIX}/bin:$PATH
 
 #-----------------------------------------------------------------------------
-# Install Mindboggle (pip for code, git for C++ code):
-#
-# To check out the latest development version using git:
-# git clone git://github.com/binarybottle/mindboggle.git
-#
-# Step 1: Install Mindboggle Python software
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Change into the ``mindboggle`` directory and install mindboggle
-# (depending on permissions you may need to use ``sudo``)::
-#    python setup.py install
-#
-# Step 2: Compile Mindboggle C++ code
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# In the mindboggle_tools/bin directory, run cmake (or ccmake) and make::
-#    cd mindboggle/mindboggle_tools/bin/
-#    cmake ..
-#    make
-#
-# Step 3: Set environment variables
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# For your system to recognize the C++ code we just compiled, include
-# the following lines in your system's equivalent of a .bash_profile file.
+# Additional resources for installing packages:
 #-----------------------------------------------------------------------------
-pip install https://github.com/binarybottle/mindboggle/archive/master.zip
-git clone https://github.com/binarybottle/mindboggle.git
-MINDBOGGLE_TOOLS=$CWD/mindboggle/mindboggle_tools/bin
-export PATH=$MINDBOGGLE_TOOLS:$PATH
-cd $MINDBOGGLE_TOOLS
-cmake ../ -DVTK_DIR:STRING=$CWD/anaconda/lib/vtk-5.10
+conda install --yes cmake pip
+
+#-----------------------------------------------------------------------------
+# Python packages:
+#-----------------------------------------------------------------------------
+conda install --yes numpy scipy matplotlib nose networkx traits vtk ipython
+# Nipype: software pipeline framework; Nibabel: medical image read/write lib
+pip install nibabel nipype
+VTK_DIR=${INSTALL_PREFIX}/lib/vtk-5.10
+#pip install https://github.com/RDFLib/rdflib/archive/master.zip
+#pip install https://github.com/satra/prov/archive/rdf.zip
+
+#-----------------------------------------------------------------------------
+# Mindboggle:
+#-----------------------------------------------------------------------------
+MB_DL=${DL_PREFIX}/mindboggle
+git clone https://github.com/binarybottle/mindboggle.git $MB_DL
+cd $MB_DL
+python setup.py install --prefix=${INSTALL_PREFIX}
+cd ${MB_DL}/mindboggle_tools/bin
+cmake ../ -DVTK_DIR:STRING=${VTK_DIR}
 make
-cd $CWD
+cd $DL_PREFIX
+cp -r ${MB_DL}/mindboggle_tools ${INSTALL_PREFIX}
 
 #-----------------------------------------------------------------------------
-# Non-Python software packages
-# ----------------------------
-# Use FreeSurfer_ and ANTS_ to process T1-weighted MRI data for use by
-# Mindboggle.  See README (http://mindboggle.info/users/README.html)
-# for instructions on running recon-all in FreeSurfer and
-# antsCorticalThickness.sh in ANTs.
+# FreeSurfer's recon-all (http://surfer.nmr.mgh.harvard.edu)
+# provides labeled cortical surfaces and non/cortical volumes for Mindboggle.
 #-----------------------------------------------------------------------------
-# Install FreeSurfer:
-#
-# FreeSurfer (http://surfer.nmr.mgh.harvard.edu)
-# provides labeled cortical surfaces and non/cortical volumes.
-# (Note: Mindboggle expects the FreeSurfer SUBJECTS_DIR environment
-# variable to be set.)
-#-----------------------------------------------------------------------------
-# http://surfer.nmr.mgh.harvard.edu/fswiki/Download
-wget -c ftp://surfer.nmr.mgh.harvard.edu/pub/dist/freesurfer/5.3.0/freesurfer-Linux-centos4_x86_64-stable-pub-v5.3.0.tar.gz
-sudo tar xzvf freesurfer-Linux-centos4_x86_64-stable-pub-v5.3.0.tar.gz
-rm freesurfer-Linux-centos4_x86_64-stable-pub-v5.3.0.tar.gz
-FREESURFER_HOME=$CWD/freesurfer
-SUBJECTS_DIR=$FREESURFER_HOME/subjects
-export PATH=$FREESURFER_HOME:$SUBJECTS_DIR:$PATH
-source $FREESURFER_HOME/SetUpFreeSurfer.sh
-
-# Remove some of the larger directories:
-cd freesurfer
-sudo rm -r average fsfast matlab mni subjects tktools trctrain
-cd lib
-sudo rm -r cuda qt tcl tcltktixblt
-cd $CWD
+FS_DL=${DL_PREFIX}/freesurfer-Linux-centos4_x86_64-stable-pub-v5.3.0.tar.gz
+wget -c ftp://surfer.nmr.mgh.harvard.edu/pub/dist/freesurfer/5.3.0/freesurfer-Linux-centos4_x86_64-stable-pub-v5.3.0.tar.gz -P $DL_PREFIX
+tar -xvzf $FS_DL -C ${INSTALL_PREFIX}
 
 #-----------------------------------------------------------------------------
-# Install ANTs:
-#
-# ANTs (http://brianavants.wordpress.com/2012/04/13/updated-ants-compile-instructions-april-12-2012/)
-# optionally provides tissue segmentation, affine registration to standard
-# space, and nonlinear registration for whole-brain labeling, to improve and
-# extend Mindboggle results.
-# (Note: After installing ANTS, be sure to copy the files in ANTs/Scripts/
-# to the antsbin/bin/ directory, and set the ANTSPATH environment variable.)
+# ANTs (http://brianavants.wordpress.com/2012/04/13/
+#              updated-ants-compile-instructions-april-12-2012/)
+# antsCorticalThickness.h pipeline optionally provides tissue segmentation,
+# affine registration to standard space, and nonlinear registration for
+# whole-brain labeling, to improve and extend Mindboggle results.
 #-----------------------------------------------------------------------------
-# http://brianavants.wordpress.com/2012/04/13/
-#        updated-ants-compile-instructions-april-12-2012/
-git clone git://github.com/stnava/ANTs.git
-mkdir antsbin
-cd antsbin
-cmake ../ANTs -DVTK_DIR:STRING=$CWD/anaconda/lib/vtk-5.10
-make #-j 4
-cp ../ANTs/Scripts/* bin/
-cd $CWD
-export ANTSPATH=$CWD/antsbin/bin/
+ANTS_DL=${DL_PREFIX}/ants
+git clone https://github.com/stnava/ANTs.git $ANTS_DL
+cd $ANTS_DL
+git checkout tags/v2.1.0rc2
+mkdir ${INSTALL_PREFIX}/ants
+cd ${INSTALL_PREFIX}/ants
+cmake $ANTS_DL -DVTK_DIR:STRING=${VTK_DIR}
+make
+cp -r ${ANTS_DL}/Scripts/* ${INSTALL_PREFIX}/ants/bin
+
+#-----------------------------------------------------------------------------
+# Create a global environment sourcing script and set environment variables:
+#-----------------------------------------------------------------------------
+MB_ENV=/etc/profile.d/mb_env.sh
+touch $MB_ENV
+
+# -- Local install --
+echo "# Local install prefix" >> $MB_ENV
+echo "export PATH=${INSTALL_PREFIX}/bin:\$PATH" >> $MB_ENV
+
+# -- ANTs --
+echo "# ANTs" >> $MB_ENV
+echo "export ANTSPATH=${INSTALL_PREFIX}/ants/bin" >> $MB_ENV
+echo "export PATH=\$ANTSPATH:\$PATH" >> $MB_ENV
+
+# -- FreeSurfer --
+echo "# FreeSurfer" >> $MB_ENV
+echo "export FREESURFER_HOME=${INSTALL_PREFIX}/freesurfer" >> $MB_ENV
+echo "source \${FREESURFER_HOME}/SetUpFreeSurfer.sh" >> $MB_ENV
+echo "export PATH=\${FREESURFER_HOME}/bin:\$PATH" >> $MB_ENV
+
+# -- Mindboggle --
+echo "# Mindboggle" >> $MB_ENV
+echo "export MINDBOGGLE_TOOLS=${INSTALL_PREFIX}/mindboggle_tools/bin" >> $MB_ENV
+echo "export PATH=\$MINDBOGGLE_TOOLS:\$PATH" >> $MB_ENV
+
+#-----------------------------------------------------------------------------
+# Finally, remove downloads directory and other non-essential directories:
+#-----------------------------------------------------------------------------
+rm ${DL_PREFIX}/* -rf
+
+rm_extras=1
+if [ $rm_extras == 1 ]
+    rm -r ${FREESURFER_HOME}/average
+    rm -r ${FREESURFER_HOME}/data
+    rm -r ${FREESURFER_HOME}/diffusion
+    rm -r ${FREESURFER_HOME}/docs
+    rm -r ${FREESURFER_HOME}/matlab
+    rm -r ${FREESURFER_HOME}/mni
+    rm -r ${FREESURFER_HOME}/subjects/fsaverage
+    rm -r ${FREESURFER_HOME}/tktools
+    rm -r ${FREESURFER_HOME}/trctrain
+    rm -r ${FREESURFER_HOME}/lib/cuda
+    rm -r ${FREESURFER_HOME}/lib/qt
+    rm -r ${FREESURFER_HOME}/lib/tcl
+    rm -r ${FREESURFER_HOME}/lib/tcltktixblt
+
+    mv ${INSTALL_PREFIX}/ants/bin ${INSTALL_PREFIX}/freesurfer/ants_bin
+    rm -r ${INSTALL_PREFIX}/ants/*
+    mv ${INSTALL_PREFIX}/freesurfer/ants_bin ${INSTALL_PREFIX}/ants/bin
+fi
