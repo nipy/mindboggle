e8bce7bb05e33bd5cd9a69f9895bceaffaa1b3fb
= <arno@binarybottle.com>
2015-03-01 21:54:39 -0800

    add html

--- doc/make_html.sh
+++ doc/make_html.sh
@@ -1,4 +1,4 @@
 
 make html
 
-rsync -avz --sparse --exclude-from=/homedir/.rsync-exclude -e /usr/bin/ssh build/html/* binarybottle@binarybottle.com:/home/binarybottle/mindboggle.info
+mv build/html ..
