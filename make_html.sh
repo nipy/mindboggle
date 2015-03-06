91f9bc189294e418c0a344dc69ab837538d086ff
= <arno@binarybottle.com>
2015-03-06 01:37:01 -0800

    update make_html.sh

--- make_html.sh
+++ make_html.sh
@@ -15,14 +15,17 @@ git push origin master
 # GH-PAGES BRANCH:
 
 git checkout gh-pages
-git checkout master -- doc/build/html/faq
+git checkout master -- doc/build/html/api
 git checkout master -- doc/build/html/genindex.html
 git checkout master -- doc/build/html/documentation.html
+
+rm -r api
+mv doc/build/html/* .
 
 sed -i .bk 's/_static/shared/g' *.html
 sed -i .bk 's/_static/shared/g' api/generated/*.html
 rm *.bk
 
-git add doc
+git add api genindex.html documentation.html
 git commit -m "Update api and faq for website."
 git push origin gh-pages
