52da615527ee266fd2e7525927b3ffbd04643a60
Arno Klein <arno@binarybottle.com>
2015-03-27 18:49:02 -0400

    Update evaluate_labels.py to compute manual/automatic label overlap measures in Mindboggle-101 volumes.

--- mindboggle/evaluate/evaluate_labels.py
+++ mindboggle/evaluate/evaluate_labels.py
@@ -17,15 +17,20 @@ Copyright 2012,  Mindboggle team (http://mindboggle.info), Apache v2.0 License
 
 """
 
-def measure_surface_overlap(command, labels_file1, labels_file2):
+def measure_surface_overlap(command, labels_file1, labels_file2, overlap_file):
     """
     Measure surface overlap using Joachim Giard's code.
 
     Parameters
     ----------
-    command : surface overlap C++ executable command
-    labels_file1 : ``vtk file`` with index labels for scalar values
-    labels_file2 : ``vtk file`` with index labels for scalar values
+    command : string
+        surface overlap C++ executable command
+    labels_file1 : string
+        ``vtk file`` with index labels for scalar values
+    labels_file2 : string
+        ``vtk file`` with index labels for scalar values
+    overlap_file : string
+        (optional) output file name
 
     Returns
     -------
@@ -34,7 +39,7 @@ def measure_surface_overlap(command, labels_file1, labels_file2):
 
     Examples
     --------
-                import os
+    >>> import os
     >>> from mindboggle.evaluate.evaluate_labels import measure_surface_overlap
     >>> from mindboggle.mindboggle import hashes_url
     >>> from mindboggle.mio.fetch_data import fetch_check_data
@@ -45,16 +50,17 @@ def measure_surface_overlap(command, labels_file1, labels_file2):
     >>> label_file2 = 'lh.labels.DKT31.manual.vtk'
     >>> file1 = fetch_check_data(label_file1, url, hashes, cache_env, cache)
     >>> file2 = fetch_check_data(label_file2, url, hashes, cache_env, cache)
-    >>> #
-    >>> measure_surface_overlap(command, file1, file2)
+    >>> overlap_file = ''
+    >>> measure_surface_overlap(command, file1, file2, overlap_file)
 
     """
     import os
     from nipype.interfaces.base import CommandLine
 
-    overlap_filename = os.path.basename(labels_file1) + '_and_' + \
-                       os.path.basename(labels_file2) + '.txt'
-    overlap_file = os.path.join(os.getcwd(), overlap_filename)
+    if not overlap_file:
+        overlap_file = os.path.basename(labels_file1) + '_and_' + \
+                           os.path.basename(labels_file2) + '.txt'
+    overlap_file = os.path.join(os.getcwd(), overlap_file)
     cli = CommandLine(command = command)
     cli.inputs.args = ' '.join([labels_file1, labels_file2, overlap_file])
     cli.cmdline
@@ -63,22 +69,27 @@ def measure_surface_overlap(command, labels_file1, labels_file2):
     return overlap_file
 
 
-def measure_volume_overlap(labels, file1, file2):
+def measure_volume_overlap(labels, file1, file2, overlap_file):
     """
     Measure overlap between individual label regions
     in source and target nifti (nii.gz) images.
 
     Parameters
     ----------
-    labels : list of label indices
-    file1 : source image, consisting of index-labeled pixels/voxels
-    file2 : target image, consisting of index-labeled pixels/voxels
+    labels : list
+        label indices
+    file1 : string
+        source image, consisting of index-labeled pixels/voxels
+    file2 : string
+        target image, consisting of index-labeled pixels/voxels
+    overlap_file : string
+        (optional) output file name
 
     Returns
     -------
     overlaps : numpy array
         overlap values
-    out_file : string
+    overlap_file : string
         output text file name with overlap values
 
     Examples
@@ -90,7 +101,8 @@ def measure_volume_overlap(labels, file1, file2):
     >>> file1 = os.path.join(path, 'arno', 'labels', 'labels.DKT25.manual.nii.gz')
     >>> file2 = os.path.join(path, 'arno', 'labels', 'labels.DKT31.manual.nii.gz')
     >>> dkt = DKTprotocol()
-    >>> measure_volume_overlap(dkt.label_numbers, file1, file2)
+    >>> overlap_file = ''
+    >>> measure_volume_overlap(dkt.label_numbers, file1, file2, overlap_file)
 
     """
     import os
@@ -102,12 +114,18 @@ def measure_volume_overlap(labels, file1, file2):
     # Load labeled image volumes
     file1_data = nb.load(file1).get_data().ravel()
     file2_data = nb.load(file2).get_data().ravel()
-    #print(file1 + ' ' + file2)
+    #print('Files: ' + file1 + ' ' + file2)
     #print(np.unique(file1_data))
     #print(np.unique(file2_data))
 
     # Initialize output
     overlaps = np.zeros((len(labels), 3))
+    if save_output:
+        file1_name = os.path.splitext(os.path.basename(file1))[0]
+        file2_name = os.path.splitext(os.path.basename(file2))[0]
+        if not overlap_file:
+            overlap_file = os.path.join(os.getcwd(), 'labelvolume_dice_jacc_' +
+                                        file2_name + '_vs_' + file1_name + '.txt')
 
     # Loop through labels
     for ilabel, label in enumerate(labels):
@@ -137,16 +155,12 @@ def measure_volume_overlap(labels, file1, file2):
             print('label: {0}, dice: {1:.2f}, jacc: {2:.2f}'.format(
                   label, dice, jacc))
 
-        # NOTE:  untested:
-        if save_output:
-            file1_name = os.path.splitext(os.path.basename(file1))[0]
-            file2_name = os.path.splitext(os.path.basename(file2))[0]
-            out_file = os.path.join(os.getcwd(), 'labelvolume_dice_jacc_' +
-                                    file2_name + '_vs_' + file1_name + '.txt')
-            np.savetxt(out_file, overlaps, fmt='%d %.4f %.4f',
-                       delimiter='\t', newline='\n')
+    # Save output:
+    if save_output:
+        np.savetxt(overlap_file, overlaps, fmt='%d %.4f %.4f',
+                   delimiter='\t', newline='\n')
 
-    return overlaps, out_file
+    return overlaps, overlap_file
 
 
 #-----------------------------------------------------------------------------
@@ -165,13 +179,31 @@ if __name__ == "__main__":
     #-------------------------------------------------------------------------
     # Settings:
     #-------------------------------------------------------------------------
-    ants_str = '_no_ants'
+    label_method = 'freesurfer'  # 'ants'
+    use_ants_segmentation = True  # False
+
+    #-------------------------------------------------------------------------
+    # File names, paths:
+    #-------------------------------------------------------------------------
+    if use_ants_segmentation:
+        ants_str = ''
+    else:
+        ants_str = '_no_ants'
+    if label_method == 'ants':
+        volstem = 'ants_filled_labels'
+    else:
+        volstem = 'freesurfer_wmparc_filled_labels'
+        if not use_ants_segmentation:
+            volstem += '_in_freesurfer_segmentation'
+    volfile = volstem + '.nii.gz'
 
     names = ['OASIS-TRT-20', 'MMRR-21', 'NKI-RS-22', 'NKI-TRT-20',
              'Afterthought', 'Colin27', 'Twins-2', 'MMRR-3T7T-2', 'HLN-12']
     numbers = [20,21,22,20, 1,1,2,2,12]
     mindboggled = '/mnt/nfs-share/Mindboggle101/mindboggled/auto' + ants_str
     labels_dir = '/mnt/nfs-share/Mindboggle101/mindboggled/manual' + ants_str
+    autosurf = 'freesurfer_cortex_labels.vtk'
+    mansurf = 'relabeled_labels.DKT31.manual.vtk'
 
     #-------------------------------------------------------------------------
     # Evaluate surface labels:
@@ -187,9 +219,11 @@ if __name__ == "__main__":
             for isurf, surf in enumerate(surfs):
                 mdir = os.path.join(mindboggled, subject, 'labels', surf)
                 ldir = os.path.join(labels_dir, subject, 'labels', surf)
-                file1 = os.path.join(mdir, 'freesurfer_cortex_labels.vtk')
-                file2 = os.path.join(ldir, 'relabeled_labels.DKT31.manual.vtk')
-                measure_surface_overlap(command, file1, file2)
+                file1 = os.path.join(mdir, autosurf)
+                file2 = os.path.join(ldir, mansurf)
+                overlap_file = "{0}_{1}_{2}_overlap.csv".\
+                    format(subject, autosurf, mansurf)
+#                measure_surface_overlap(command, file1, file2, overlap_file)
 
     #-------------------------------------------------------------------------
     # Evaluate volume labels:
@@ -199,8 +233,7 @@ if __name__ == "__main__":
         number = numbers[iname]
         for n in range(1, number+1):
             subject = name+'-'+str(n)
-            file1 = os.path.join(mindboggled, subject, 'labels',
-                                 'freesurfer_wmparc_filled_labels.nii.gz')
-            file2 = os.path.join(labels_dir, subject,
-                                 'freesurfer_wmparc_filled_labels.nii.gz')
-            measure_volume_overlap(dkt.label_numbers, file1, file2)
+            file1 = os.path.join(mindboggled, subject, 'labels', volfile)
+            file2 = os.path.join(labels_dir, subject, 'labels', volfile)
+            overlap_file = "{0}_{1}_overlap.csv".format(subject, volstem)
+            measure_volume_overlap(dkt.label_numbers, file1, file2, overlap_file)
