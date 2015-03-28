d8cb41dc085da0f176e6261f42f0a2c4efd5febd
= <arno@binarybottle.com>
2015-03-28 00:09:00 -0700

    Fix select_column_from_mindboggle_tables()

--- mindboggle/mio/tables.py
+++ mindboggle/mio/tables.py
@@ -26,6 +26,16 @@ def read_columns(filename, n_columns=1, trail=False):
     -------
     columns :  a list of lists of strings, one list per column of text.
 
+    Examples
+    --------
+    >>> import os
+    >>> from mindboggle.mio.tables import read_columns
+    >>> filename = '/homedir/mindboggled/Twins-2-1/tables/volume_for_each_freesurfer_label.csv'
+    #left_cortical_surface/vertices.csv')
+    >>> n_columns = 2
+    >>> trail = False
+    >>> columns = read_columns(filename, n_columns=n_columns, trail=trail)
+
     """
     import os
     import re
@@ -1372,7 +1382,7 @@ def select_column_from_mindboggle_tables(subjects, hemi, tables_dir,
     for subject in subjects:
         if is_surface_table:
             table = os.path.join(tables_dir, subject, 'tables',
-                                 hemi+'_surface', table_name)
+                                 hemi+'_cortical_surface', table_name)
         else:
             table = os.path.join(tables_dir, subject, 'tables', table_name)
         tables.append(table)
@@ -1388,96 +1398,3 @@ def select_column_from_mindboggle_tables(subjects, hemi, tables_dir,
 
     return tables, columns, column_name, row_names, row_names_title, \
            row_stats, row_stats_names, output_table, output_stats_table
-
-
-def concatenate_mindboggle_tables(subjects, hemi, tables_dir,
-        table_name, delimiter=',', is_surface_table=True, output_table=''):
-    """
-    Concatenate specified Mindboggle surface shape tables across subjects.
-
-    This function concatenates specified surface tables, with the first row
-    written only once at top, and 'Subject: <subject name>' preceding each
-    subject's table.
-
-    Expects::
-        <tables_dir>/<subject>/tables/['left','right']_surface/<table_name>
-        Or:
-        <tables_dir>/<subject>/tables/<table_name>
-
-    Parameters
-    ----------
-    subjects :  list of strings
-        names of subjects processed by Mindboggle
-    hemi :  string
-        hemisphere in {'left', 'right}
-    tables_dir : string
-        name of Mindboggle tables directory
-    table_name : string
-        name of Mindboggle table file
-    delimiter : string
-        delimiter between output table columns, such as ','
-    is_surface_table : Boolean
-        if True, use path to surface tables
-    output_table : string
-        output table file name
-
-    Returns
-    -------
-    output_table :  string
-        output table file name
-
-    Examples
-    --------
-    >>> import os
-    >>> from mindboggle.mio.tables import concatenate_mindboggle_tables
-    >>> subjects = ['Twins-2-1', 'Twins-2-2']
-    >>> hemi = 'left'
-    >>> tables_dir = os.path.join(os.environ['HOME'], 'mindboggled')
-    >>> table_name = "label_shapes.csv"
-    >>> #table_name = "volumes_FreeSurfer_labels.csv"
-    >>> delimiter = ','
-    >>> is_surface_table = True
-    >>> output_table = ''
-    >>> #
-    >>> concatenate_mindboggle_tables(subjects, hemi, tables_dir,
-    >>>     table_name, delimiter, is_surface_table, output_table)
-
-    """
-    import os
-
-    if not output_table:
-        output_table = os.path.join(os.getcwd(), 'concatenated_' + table_name)
-    Fout = open(output_table, 'w')
-
-    #-------------------------------------------------------------------------
-    # Concatenate Mindboggle shape tables:
-    #-------------------------------------------------------------------------
-    for isubject, subject in enumerate(subjects):
-        if is_surface_table:
-            table = os.path.join(tables_dir, subject, 'tables',
-                                 hemi+'_surface', table_name)
-        else:
-            table = os.path.join(tables_dir, subject, 'tables', table_name)
-
-        if os.path.isfile(table):
-            Fin = open(table, 'r')
-            lines = Fin.readlines()
-            for iline, line in enumerate(lines):
-                if isubject == 0 and iline == 0:
-                    Fout.write(line)
-                if iline == 0:
-                    Fout.write("Subject: {0}\n".format(subject))
-                else:
-                    Fout.write(line)
-                    #row = [x.strip() for x in line.split(delimiter)]
-
-            Fin.close()
-
-            #from mindboggle.guts.utilities import execute
-            #type = 'os'
-            #cmd = ' '.join(['cat', table, '>>', output_table])
-            #execute(cmd, type)
-
-    Fout.close()
-
-    return output_table
