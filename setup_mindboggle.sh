f18628895fb0ed73dfc76eed2f4739ff61ef9598
= <arno@binarybottle.com>
2015-12-08 11:15:03 -0800

    Update setup script and Vagrantfile configuration script.

--- setup_mindboggle.sh
+++ setup_mindboggle.sh
@@ -38,10 +38,10 @@ MB_ENV=$3
 #-----------------------------------------------------------------------------
 # Reset arguments to change formats, versions, and switch from linux to osx:
 #-----------------------------------------------------------------------------
-OS=linux  # linux or osx
-VTK=vtk-6.3.0-py27_0  # VTK library, with VTK and Python versions
+OS="linux"  #  linux or osx
+OS_XTRA="x86_64"
+VTK="vtk-6.3.0-py27_0"  # VTK library, with VTK and Python versions
 ANTS=0  # 1 to install ANTS, 0 not to install
-OS_XTRA=x86_64
 
 #-----------------------------------------------------------------------------
 # System-wide dependencies:
@@ -49,26 +49,27 @@ OS_XTRA=x86_64
 if [ $OS = "linux" ]; then
     apt-get update
     apt-get install -y g++ git make xorg
-    OS_STR=Linux
+    OS_STR="Linux"
 else
-    OS_STR=MacOSX
+    OS_STR="MacOSX"
 fi
 
 #-----------------------------------------------------------------------------
 # Anaconda's miniconda Python distribution for local installs:
 #-----------------------------------------------------------------------------
-CONDA_FILE=Miniconda-latest-${OS_STR}-${OS_XTRA}.sh
+CONDA_PATH="http://repo.continuum.io/miniconda"
+CONDA_FILE=Miniconda-latest-$OS_STR-$OS_XTRA.sh
 CONDA_DL=${DL_PREFIX}/${CONDA_FILE}
-if [ $OS == "linux" ]; then
-    wget -O $CONDA_DL http://repo.continuum.io/miniconda/${CONDA_FILE}
+if [ $OS = "linux" ]; then
+    wget -O $CONDA_DL ${CONDA_PATH}/$CONDA_FILE
 else
-    curl -o $CONDA_DL http://repo.continuum.io/miniconda/${CONDA_FILE}
+    curl -o $CONDA_DL ${CONDA_PATH}/$CONDA_FILE
 fi
 
-bash $CONDA_DL -b -p $INSTALL_PREFIX/miniconda
+bash $CONDA_DL -b -p ${INSTALL_PREFIX}/miniconda
 
 # Setup PATH
-export PATH=${INSTALL_PREFIX}/bin:$PATH
+#export PATH=${INSTALL_PREFIX}/bin:$PATH
 
 #-----------------------------------------------------------------------------
 # Additional resources for installing packages:
@@ -80,20 +81,20 @@ conda install --yes cmake pip
 #-----------------------------------------------------------------------------
 conda install --yes numpy scipy matplotlib pandas nose networkx traits vtk ipython
 pip install nibabel nipype
-VTK_DIR=${INSTALL_PREFIX}/miniconda/pkgs/${VTK}
+VTK_DIR=${INSTALL_PREFIX}/miniconda/pkgs/$VTK
 
 #-----------------------------------------------------------------------------
 # Mindboggle:
 #-----------------------------------------------------------------------------
 MB_DL=${DL_PREFIX}/mindboggle
 git clone https://github.com/nipy/mindboggle.git $MB_DL
-mv ${MB_DL} ${INSTALL_PREFIX}
+mv $MB_DL $INSTALL_PREFIX
 cd ${INSTALL_PREFIX}/mindboggle
-python setup.py install --prefix=${INSTALL_PREFIX}
+python setup.py install --prefix=$INSTALL_PREFIX
 cd ${INSTALL_PREFIX}/mindboggle/surface_cpp_tools/bin
-cmake ../ -DVTK_DIR:STRING=${VTK_DIR}
+cmake ../ -DVTK_DIR:STRING=$VTK_DIR
 make
-cd ${INSTALL_PREFIX}
+cd $INSTALL_PREFIX
 
 #-----------------------------------------------------------------------------
 # ANTs (http://brianavants.wordpress.com/2012/04/13/
@@ -102,14 +103,14 @@ cd ${INSTALL_PREFIX}
 # affine registration to standard space, and nonlinear registration for
 # whole-brain labeling, to improve and extend Mindboggle results.
 #-----------------------------------------------------------------------------
-if [ $ANTS == 1 ]; then
+if [ $ANTS -eq 1 ]; then
     ANTS_DL=${DL_PREFIX}/ants
     git clone https://github.com/stnava/ANTs.git $ANTS_DL
     cd $ANTS_DL
     git checkout tags/v2.1.0rc2
     mkdir ${INSTALL_PREFIX}/ants
     cd ${INSTALL_PREFIX}/ants
-    cmake $ANTS_DL #-DVTK_DIR:STRING=${VTK_DIR}
+    cmake $ANTS_DL #-DVTK_DIR:STRING=$VTK_DIR
     make
     cp -r ${ANTS_DL}/Scripts/* ${INSTALL_PREFIX}/ants/bin
     # Remove non-essential directories:
@@ -121,6 +122,9 @@ fi
 #-----------------------------------------------------------------------------
 # Set environment variables:
 #-----------------------------------------------------------------------------
+#MB_ENV=/etc/profile.d/mb_env.sh
+#touch $MB_ENV
+
 # -- Local install --
 echo "# Local install prefix" >> $MB_ENV
 echo "export PATH=${INSTALL_PREFIX}/bin:\$PATH" >> $MB_ENV
@@ -131,7 +135,7 @@ echo "export surface_cpp_tools=${INSTALL_PREFIX}/mindboggle/surface_cpp_tools/bi
 echo "export PATH=\$surface_cpp_tools:\$PATH" >> $MB_ENV
 
 # -- ANTs --
-if [ $ANTS == 1 ]; then
+if [ $ANTS = 1 ]; then
     echo "# ANTs" >> $MB_ENV
     echo "export ANTSPATH=${INSTALL_PREFIX}/ants/bin" >> $MB_ENV
     echo "export PATH=\$ANTSPATH:\$PATH" >> $MB_ENV
@@ -141,6 +145,6 @@ fi
 # Finally, remove non-essential directories:
 #-----------------------------------------------------------------------------
 rm_extras=0
-if [ $rm_extras == 1 ]; then
+if [ $rm_extras = 1 ]; then
     rm ${DL_PREFIX}/* -rf
 fi
