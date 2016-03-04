760dc46b0c86a940365cba52ef31ad61292db27a
= <arno@binarybottle.com>
2016-03-03 21:42:12 -0800

    Update docs

--- docs/tools/build_modref_templates.py
+++ docs/tools/build_modref_templates.py
@@ -12,7 +12,7 @@ from apigen import ApiDocWriter
 #*****************************************************************************
 if __name__ == '__main__':
     package = 'mindboggle'
-    outdir = os.path.join('api') #,'generated')
+    outdir = os.path.join('api', 'generated')
     docwriter = ApiDocWriter(package)
     docwriter.package_skip_patterns += [r'\.fixes$',
                                         r'\.externals$',
@@ -22,5 +22,7 @@ if __name__ == '__main__':
     #docwriter.module_skip_patterns += [r'\.label\.rebound',
     #                                    ]
     docwriter.write_api_docs(outdir)
-    docwriter.write_index(outdir, 'gen', relative_to='api')
-    print '%d files written' % len(docwriter.written_modules)
+#    docwriter.write_index(outdir, 'gen', relative_to='api')
+    docwriter.write_index(api, 'gen', relative_to='api')
+
+    print('{0} files written'.format(len(docwriter.written_modules))
