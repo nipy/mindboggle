8c32aec6aa403b4c1cf3bdda079060c85eda3160
= <arno@binarybottle.com>
2016-03-08 11:55:55 -0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@ needs_sphinx = '1.3'
 extensions = [
     'sphinx.ext.autodoc',
     'sphinxcontrib.napoleon',
-    'numpydoc'
+#    'numpydoc'
     ]
 #    'sphinx.ext.doctest',
 #    'sphinx.ext.coverage',
@@ -171,12 +171,12 @@ html_theme = 'alabaster'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = '_static/mindboggle_logo_small.jpg'
+#html_logo = '_static/mindboggle_logo_small.jpg'
 
 # The name of an image file (relative to this directory) to use as a favicon of
 # the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = '_static/favicon.ico'
+#html_favicon = '_static/favicon.ico'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
