ae37e225e24e3b7722d1587aacc638997bdbaf98
Satrajit S. Ghosh <satra@mit.edu>
2016-07-26 15:05:33 -0400

    Add distance correlation function from Satrajit S. Ghosh's mapalign repo.

--- mindboggle/guts/compute.py
+++ mindboggle/guts/compute.py
@@ -11,6 +11,45 @@ Copyright 2016,  Mindboggle team (http://mindboggle.info), Apache v2.0 License
 """
 
 
+def distcorr(X, Y):
+    """
+    Compute the distance correlation function.
+
+    >>> a = [1,2,3,4,5]
+    >>> b = np.array([1,2,9,4,4])
+    >>> distcorr(a, b)
+    0.762676242417
+
+    Copyright (2014-2015) MIT
+    Written by Satrajit S. Ghosh (Apache License v2.0) as part of the
+    mapalign GitHub repository (https://github.com/satra/mapalign)
+    """
+    import numpy as np
+    from scipy.spatial.distance import pdist, squareform
+
+    X = np.atleast_1d(X)
+    Y = np.atleast_1d(Y)
+    if np.prod(X.shape) == len(X):
+        X = X[:, None]
+    if np.prod(Y.shape) == len(Y):
+        Y = Y[:, None]
+    X = np.atleast_2d(X)
+    Y = np.atleast_2d(Y)
+    n = X.shape[0]
+    if Y.shape[0] != X.shape[0]:
+        raise ValueError('Number of samples must match')
+    a = squareform(pdist(X))
+    b = squareform(pdist(Y))
+    A = a - a.mean(axis=0)[None, :] - a.mean(axis=1)[:, None] + a.mean()
+    B = b - b.mean(axis=0)[None, :] - b.mean(axis=1)[:, None] + b.mean()
+
+    dcov2_xy = (A * B).sum()/float(n * n)
+    dcov2_xx = (A * A).sum()/float(n * n)
+    dcov2_yy = (B * B).sum()/float(n * n)
+    dcor = np.sqrt(dcov2_xy)/np.sqrt(np.sqrt(dcov2_xx) * np.sqrt(dcov2_yy))
+    return dcor
+
+
 def point_distance(point, points):
     """
     Compute the Euclidean distance from one point to a second (set) of points.
